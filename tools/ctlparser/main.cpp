ec8a22fbd21c7f5482876456d56fbffcca15c4c7
Gonzalo Garramuño <ggarra13@gmail.com>
2012-07-31 20:33:14 -0300

    Updated exrImage to support anaglyphs of lumma channels correctly.

--- tools/ctlparser/main.cpp
+++ tools/ctlparser/main.cpp
@@ -6,6 +6,13 @@
 
 #include <CtlSimdInterpreter.h>
 #include <CtlFunctionCall.h>
+
+
+#include <libintl.h>
+#define _(String)  gettext(String)
+#define gettext_noop(String) String
+#define N_(String) gettext_noop (String)
+
 // #include <ImathMath.h>
 // #include <IexBaseExc.h>
 
@@ -17,16 +24,21 @@ void usage(const char** argv)
   cerr << argv[0] << " - v1.0" << endl
        << endl
        << endl
-       << "A simple parser to validate ctl transforms." << endl
+       << _("A simple parser to validate ctl transforms.") << endl
        << endl
-       << "Example:" << endl
+       << _("Example:") << endl
        << endl
-       << argv[0] << " transform_RRT" << endl
+       << argv[0] << N_(" transform_RRT") << endl
        << endl;
 }
 
 int main( const int argc, const char** argv )
 {
+   
+   setlocale( LC_ALL, "" );
+   bindtextdomain( "ctlparser", "/usr/share/locale" );
+   textdomain( "ctlparser" );
+
   if ( argc != 2 )
     {
       usage(argv);
@@ -40,7 +52,7 @@ int main( const int argc, const char** argv )
     }
   catch(const std::exception &e)
     {
-      cerr << "ERROR -- caught exception: " << endl << e.what() << endl;
+       cerr << _("ERROR -- caught exception: ") << endl << e.what() << endl;
       return 1;
     }
   return 0;
