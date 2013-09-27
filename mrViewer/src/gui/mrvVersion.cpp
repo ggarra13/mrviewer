fc5d8517b250d531c7f9840f85c4ef877ddcac2a
Gonzalo Garramuño <ggarra13@gmail.com>
2013-09-27 19:49:46 -0300

    Updated version and history file.
    Former-commit-id: ee9b2b41643e4205722f7e517d24affc169dce20

--- mrViewer/src/gui/mrvVersion.cpp
+++ mrViewer/src/gui/mrvVersion.cpp
@@ -47,7 +47,7 @@ namespace mrv
 
 
 
-  static const char* kVersion = "2.4.1 - Built " __DATE__ " " __TIME__;
+  static const char* kVersion = "2.4.0 - Built " __DATE__ " " __TIME__;
 
 
   struct FormatInfo
