7b41ba02037bacaab7aa956875b432b1f348c568
Gonzalo Garramuño <ggarra13@gmail.com>
2014-02-09 16:29:47 -0300

    Updated HISTORY.txt and bumped tiny version.
    Former-commit-id: 146163face237779388c68938d86a7bf21d71c2b

--- mrViewer/src/gui/mrvVersion.cpp
+++ mrViewer/src/gui/mrvVersion.cpp
@@ -47,7 +47,7 @@ namespace mrv
 
 
 
-  static const char* kVersion = "2.5.6.2 - Built " __DATE__ " " __TIME__;
+  static const char* kVersion = "2.5.6.3 - Built " __DATE__ " " __TIME__;
 
 
   struct FormatInfo
