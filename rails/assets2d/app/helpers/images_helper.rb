8e3bcf5facc857336bc17f25430229464e6f34bb
Gonzalo Garramuño <ggarra13@gmail.com>
2015-02-22 17:09:03 -0300

    v2.7.1
    Former-commit-id: ab1f7c07a629bef6817a95f31fa729168f215859

--- rails/assets2d/app/helpers/images_helper.rb
+++ rails/assets2d/app/helpers/images_helper.rb
@@ -7,7 +7,7 @@ module ImagesHelper
   #
   # Makes the thumbnail an actual image link
   #
-  def thumbnail_column(record)
+  def thumbnail_column(record, record2)
     record.create_png
     image_tag( "dbimage-#{record.id}.png" )
   end
