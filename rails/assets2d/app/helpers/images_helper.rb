0e42ae1457a498b6e29b54ed30457d259da2e3c0
Gonzalo Garramuño <ggarra13@gmail.com>
2015-02-28 16:23:06 -0300

    Updated ruby's helpers to rails 4.2.0.
    Former-commit-id: 7b163e5c5c266a43648398172d76a05cb6359b76

--- rails/assets2d/app/helpers/images_helper.rb
+++ rails/assets2d/app/helpers/images_helper.rb
@@ -1,13 +1,13 @@
 module ImagesHelper
 
-  def pixel_format_column( record )
+  def pixel_format_column( record, column )
     h( record.pixel_format.name )
   end
     
   #
   # Makes the thumbnail an actual image link
   #
-  def thumbnail_column(record, record2)
+  def thumbnail_column( record, column )
     record.create_png
     image_tag( "dbimage-#{record.id}.png" )
   end
