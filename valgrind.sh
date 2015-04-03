7d542e58fd660507c7ff567b2f80682583f93fdb
Gonzalo Garramuño <ggarra13@gmail.com>
2015-04-03 17:02:34 -0300

    Bug fixed playback of single frames in EDL. Fixed typo in Spanish translation.

--- valgrind.sh
+++ valgrind.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/bin/bash --norc
 
-valgrind -v --trace-children=yes --track-origins=yes --show-reachable=no --leak-check=full $*
+#valgrind -v --trace-children=yes --gen-suppressions=yes $*
+
+valgrind -v --trace-children=yes --suppressions=valgrind.sup $*
+
+# valgrind -v --demangle=yes --track-fds=yes --trace-children=yes --track-origins=yes --show-reachable=yes --leak-check=full $*
 
 
