ef18af9c05951c516c3b3cb0d3cc6f517d12c2f5
Gonzalo Garramuño <ggarra13@gmail.com>
2015-06-21 09:15:26 -0300

    Seek works with all offsets.  Playback only with positive offsets.
    Former-commit-id: 1336d0c9d4e1470608638c94cc4aa98ad83fd247

--- valgrind.sh
+++ valgrind.sh
@@ -2,7 +2,7 @@
 
 #valgrind -v --trace-children=yes --gen-suppressions=yes $*
 
-valgrind -v --trace-children=yes --suppressions=valgrind.sup $*
+valgrind -v --leak-check=full --trace-children=yes --suppressions=valgrind.sup --track-origins=yes $*
 
 # valgrind -v --demangle=yes --track-fds=yes --trace-children=yes --track-origins=yes --show-reachable=yes --leak-check=full $*
 
