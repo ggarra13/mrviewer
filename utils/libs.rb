fd3ecb0823077f3e6a933d521ca0eab9301bdeb6
Gonzalo Garramuño <ggarra13@gmail.com>
2015-09-04 16:22:58 -0300

    More changes to make the viewer work in stereo. Changes to the media info window and to file chooser.

--- utils/libs.rb
+++ utils/libs.rb
@@ -5,6 +5,9 @@ require 'fileutils'
 EXCLUDE = %w(
 libGL\.so
 libm\.so
+libgcc_s.*
+libc\.so*
+libstdc\+\+.*
 .*nvidia.*
 )
 EXCLUDE_REGEX = /(?:#{EXCLUDE.join('|')}).*/
@@ -43,3 +46,15 @@ for line in files
 
 end
 
+FileUtils.rm_f( "Release/shaders" )
+FileUtils.rm_f( "Release/docs" )
+FileUtils.rm_f( "Release/ctl" )
+FileUtils.rm_f( "Release/HISTORY.txt" )
+FileUtils.rm_f( "Release/LICENSE.txt" )
+FileUtils.rm_f( "Release/Videos.txt" )
+FileUtils.ln_s( "../../../shaders", "Release/shaders" )
+FileUtils.ln_s( "../../../docs", "Release/docs" )
+FileUtils.ln_s( "../../../ctl", "Release/ctl" )
+FileUtils.ln_s( "../../../HISTORY.txt", "Release/HISTORY.txt" )
+FileUtils.ln_s( "../../../LICENSE.txt", "Release/LICENSE.txt" )
+FileUtils.ln_s( "../../../Videos.txt", "Release/Videos.txt" )
