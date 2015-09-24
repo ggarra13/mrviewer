1d58a8054f26cdebc841c0b1925c10f95f2c253a
Gonzalo Garramuño <ggarra13@gmail.com>
2015-09-24 20:13:09 -0300

    Removed gray frames from loading.

--- mrViewer/src/core/exrImage.h
+++ mrViewer/src/core/exrImage.h
@@ -156,14 +156,6 @@ namespace mrv {
 
       stringSet layers;
       int order[4];
-      int offsets[4];
-
-      size_t numChannels;
-      image_type::Format _format;
-
-      typedef std::vector< std::string > LayerList;
-      LayerList channelList;
-      Imf::PixelType imfPixelType;
 
 
       // Stereo in same image
