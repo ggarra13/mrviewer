efb70fa55961be78388481dcfeb62c0fe23d8a7e
unknown <ggarra13@gmail.com>
2016-01-30 18:37:31 -0300

    Made new pass at audio frame which works better. Updated HISTORY.txt file with better v/a sync.

--- mrViewer/src/core/CMedia_audio.cpp
+++ mrViewer/src/core/CMedia_audio.cpp
@@ -1612,7 +1612,7 @@ bool CMedia::find_audio( const boost::int64_t frame )
 
   limit_audio_store( frame );
 
-  _audio_pts = frame / _orig_fps; //av_q2d( get_audio_stream()->avg_frame_rate );
+  _audio_pts = result->frame() / _orig_fps; //av_q2d( get_audio_stream()->avg_frame_rate );
 
   _audio_clock = double(av_gettime_relative()) / 1000000.0;
   set_clock_at(&audclk, _audio_pts, 0, _audio_clock );
