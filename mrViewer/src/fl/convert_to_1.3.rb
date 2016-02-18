03c8dbdeac093bb21294ad7ff98782bf8812b5e7
Gonzalo Garramuño <ggarra13@gmail.com>
2016-02-18 15:07:41 -0300

    Added more .fl files.
    Former-commit-id: a89722c83f585d04faa2919c968c197dfcc45762

---
+++ mrViewer/src/fl/convert_to_1.3.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+if ARGV.size < 1
+  $stderr.puts "Usage: "
+  $stderr.puts 
+  $stderr.puts "$0 fltk2file.fl" 
+  exit 1
+end
+
+file  = ARGV[0]
+lines = File.readlines( file )
+
+lines.each { |x|
+  x.sub!(/\{fltk::Window\}/, 'Fl_Window' )
+  x.sub!(/\{fltk::Group\}/, 'Fl_Group' )
+  x.sub!(/\{fltk::PackedGroup\}/, 'Fl_Pack' )
+  x.sub!(/\{fltk::Browser\}/, 'Fl_Browser' )
+  x.sub!(/\{fltk::PopupMenu\}/, 'Fl_Menu_Button' )
+  x.sub!(/\{fltk::Item\}/, 'MenuItem' )
+  x.sub!(/\{fltk::ItemGroup\}/, 'Submenu' )
+  x.sub!(/\{fltk::Button\}/, 'Fl_Button' )
+  x.sub!(/\{fltk::CheckButton\}/, 'Fl_Check_Button' )
+  x.sub!(/\{fltk::RadioButton\}/, 'Fl_Radio_Button' )
+  x.sub!(/vertical\s+/, '' )
+}
+
+puts lines
