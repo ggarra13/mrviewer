ba7641b37a36d336d71daf302c6eb0ea2332968a
Gonzalo Garramuño <ggarra13@gmail.com>
2016-07-02 19:50:17 -0300

    Made mrvMedia's thumbnails unique to a cut. Made mrvPlayback's switch to another image by seeking first to first frame of sequence/movie.
    Former-commit-id: f9efe809ca05ffa9695acf432fadca8cbc87f9df

--- mrViewer/src/gui/mrvElement.h
+++ mrViewer/src/gui/mrvElement.h
@@ -24,14 +24,14 @@ namespace mrv {
 
   class Element : public fltk::Item
   {
-  public:
-    Element( mrv::media& );
+    public:
+      Element( mrv::media& );
 
-       const mrv::media& element() { return _elem; }
-       mrv::media element() const { return _elem; }
+      const mrv::media& element() { return _elem; }
+      mrv::media element() const { return _elem; }
 
-     public:
-       mrv::media _elem;
+    public:
+      mrv::media _elem;
   };
 
 }
