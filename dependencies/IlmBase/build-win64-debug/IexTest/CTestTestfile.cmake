fa1bcbf461a894aa44b257d13e02e41361edfee0
Gonzalo Garramuño <ggarra13@gmail.com>
2016-08-06 18:36:50 -0300

    Cleaned up dependencies directory a tad.

--- dependencies/IlmBase/build-win64-debug/IexTest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/code/lib/openexr-git/IlmBase/IexTest
-# Build directory: F:/code/lib/openexr-git/IlmBase/build-win64-debug/IexTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestIex "IexTest")
