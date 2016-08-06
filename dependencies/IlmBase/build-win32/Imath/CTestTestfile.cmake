b3977e2d29d6378da666b594d4bf27031a25e9ea
Gonzalo Garramuño <ggarra13@gmail.com>
2016-08-06 18:36:50 -0300

    Cleaned up dependencies directory a tad.
    Former-commit-id: 7d6ddc337b502b50d23780478b51aee6f800d241 [formerly fba0527d84f76027637044cb5e886904cb600cce]
    Former-commit-id: e58d07e510ba2b7ea5d1a4fa592c66d05ee05ac3

--- dependencies/IlmBase/build-win32/Imath/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/code/lib/openexr-git/IlmBase/Imath
-# Build directory: F:/code/lib/openexr-git/IlmBase/build-win32/Imath
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
