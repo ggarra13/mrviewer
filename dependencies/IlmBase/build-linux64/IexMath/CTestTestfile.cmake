35f753329d5e0d8c57839109e426f2910baab3ef
Gonzalo Garramuño <ggarra13@gmail.com>
2016-08-06 18:36:50 -0300

    Cleaned up dependencies directory a tad.
    Former-commit-id: 7d6ddc337b502b50d23780478b51aee6f800d241 [formerly fba0527d84f76027637044cb5e886904cb600cce]
    Former-commit-id: 2926302f6d149a7a1c710a8124d00f2d63a3fbd7

--- dependencies/IlmBase/build-linux64/IexMath/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/Linux/code/lib/openexr-git/IlmBase/IexMath
-# Build directory: /media/Linux/code/lib/openexr-git/IlmBase/build-linux64/IexMath
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
