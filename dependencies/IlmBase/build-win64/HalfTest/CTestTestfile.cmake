f34b5e0002a3ec514220ab313a52e9dce85c8110
Gonzalo Garramuño <ggarra13@gmail.com>
2016-08-06 18:36:50 -0300

    Cleaned up dependencies directory a tad.
    Former-commit-id: 7d6ddc337b502b50d23780478b51aee6f800d241 [formerly fba0527d84f76027637044cb5e886904cb600cce]
    Former-commit-id: f9d08672fe0d424e0ec2c6f854028467ced0a889

--- dependencies/IlmBase/build-win64/HalfTest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/code/lib/openexr-git/IlmBase/HalfTest
-# Build directory: F:/code/lib/openexr-git/IlmBase/build-win64/HalfTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestHalf "HalfTest")
