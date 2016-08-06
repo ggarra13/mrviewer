56f8f914425f5208338cad4caf0e9962c3c9e39e
Gonzalo Garramuño <ggarra13@gmail.com>
2016-08-06 18:36:50 -0300

    Cleaned up dependencies directory a tad.
    Former-commit-id: 7d6ddc337b502b50d23780478b51aee6f800d241 [formerly 7d6ddc337b502b50d23780478b51aee6f800d241 [formerly fba0527d84f76027637044cb5e886904cb600cce]]
    Former-commit-id: 1b4852c5fabc85d3808609b62a750c32f04cf683 [formerly e58d07e510ba2b7ea5d1a4fa592c66d05ee05ac3]
    Former-commit-id: b3977e2d29d6378da666b594d4bf27031a25e9ea

--- dependencies/IlmBase/build-linux64/ImathTest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/Linux/code/lib/openexr-git/IlmBase/ImathTest
-# Build directory: /media/Linux/code/lib/openexr-git/IlmBase/build-linux64/ImathTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestImath "ImathTest")
