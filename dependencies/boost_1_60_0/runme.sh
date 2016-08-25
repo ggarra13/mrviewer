1ff1323bff53c34bda448bb7576bdd67a132e3cd
Gonzalo Garramuño <ggarra13@gmail.com>
2016-08-24 21:19:10 -0300

    Modified gitignore to ignore .Plo files. Updated aviImage to use _fps variable instead of stream's avg_frame_rate, which was causing problems with Disney's MOANA 4K trailer.
    Former-commit-id: 125f842753b0c865aa6a67a4a2d57dab72f753de

--- dependencies/boost_1_60_0/runme.sh
+++ dependencies/boost_1_60_0/runme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash --norc
 
-./b2 --build-type=minimal --without-python variant=release link=shared threading=multi runtime-link=shared
+./b2 -j 8 toolset=msvc-12.0 --build-type=minimal --without-python variant=release link=shared threading=multi runtime-link=shared
