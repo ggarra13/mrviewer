67a69f781ab15c3ae4b19b4cf9ddc05d2b583a06
Gonzalo Garramuño <ggarra13@gmail.com>
2016-08-25 19:00:57 -0300

    Added missing dependencies for win64 compile.
    Former-commit-id: 497f038bc4fa2a4e4c571788381720c2c30fa1e9

---
+++ dependencies/freeglut-3.0.0/build-win64/CMakeFiles/Lorenz_static.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lorenz_static.dir/progs/demos/Lorenz/lorenz.c.obj"
+  "bin/Lorenz_static.pdb"
+  "bin/Lorenz_static.exe"
+  "bin/Lorenz_static.exe.manifest"
+  "lib/Lorenz_static.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lorenz_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
