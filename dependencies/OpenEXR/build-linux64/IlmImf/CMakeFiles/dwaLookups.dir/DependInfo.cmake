ab6433c162d6e95477a495b4f723eb2c18a88bdb
Gonzalo Garramuño <ggarra13@gmail.com>
2016-08-30 15:25:05 -0300

    Added missing dependencies and nsis installers.
    Former-commit-id: df38f40d39c8e343dc568cf75a126549b30f208b [formerly 0e6248a9ce781c41ba9ae19967294ff4f8f488d1]
    Former-commit-id: 252007b95e9a029a686c9fea7aa481c42a31614a

---
+++ dependencies/OpenEXR/build-linux64/IlmImf/CMakeFiles/dwaLookups.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/IlmImf/dwaLookups.cpp" "/media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/build-linux64/IlmImf/CMakeFiles/dwaLookups.dir/dwaLookups.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_CONFIG_H"
+  "ILMIMF_EXPORTS"
+  "ILM_IMF_TEST_IMAGEDIR=\"/media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/IlmImfTest/\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "IlmImf"
+  "../IlmImf"
+  "config"
+  "../IlmImfUtil"
+  "../exrmaketiled"
+  "../exrenvmap"
+  "../exrmakepreview"
+  "../exrmultiview"
+  "../IlmImfFuzzTest"
+  "/usr/local/include/OpenEXR"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
