a0eb209740160cab6b46e5296921bcaca92cef08
Gonzalo Garramuno <ggarra13@gmail.com>
2016-09-06 18:02:57 -0300

    Added missing dependencies.
    Former-commit-id: 471ab58957f14d3e9bc2678ae773d2263246b741 [formerly 9f21e65123fee703fee40818942409b706bca7fe] [formerly 7d56f77fd67a79539d9095fddbea4cb468f8fde7] [formerly 93908696bf7811ccbeb11f0aa1896bd3f4029a77] [formerly 3dd60987056c0be309af0630c077568c3396eaab [formerly 25e7103ee334f494c4f48b9165400e4e3040847e [formerly 624385cca5e8e7993d78e6252954341db1d8bbf6] [formerly 3dd60987056c0be309af0630c077568c3396eaab [formerly 3dd60987056c0be309af0630c077568c3396eaab [formerly 2553adb5d00c90c9224ce81021cfc426ba24fada]]]]] [formerly 219593072fd0187b0f173a77cbc415a9a2f2c722 [formerly 20ccfcba9b2c11b4defcfaa5831662117cce8b6f] [formerly e76d38bb0f85af9e6f527588d9036ccbadcb9fa8] [formerly eda89efd60a6bdc6d028024b8d65229e26f7e8df] [formerly 3ad2b88557dd49e7edd3604a324e751aef324846 [formerly c8cc126957cc9941daae9a32e58c920955f14375] [formerly eda89efd60a6bdc6d028024b8d65229e26f7e8df [formerly eda89efd60a6bdc6d028024b8d65229e26f7e8df [formerly d1557a40f52c89e3f0adfb2c4bcf2b786f499c7d]]]] [formerly 0dc7c48542de79415af5ca3cd67a2e802fe90ad7 [formerly 0dc7c48542de79415af5ca3cd67a2e802fe90ad7 [formerly 0dc7c48542de79415af5ca3cd67a2e802fe90ad7 [formerly 44c77dafb65243db9d442f55b6bc0d712000dec7]]]]]
    Former-commit-id: 219593072fd0187b0f173a77cbc415a9a2f2c722 [formerly 20ccfcba9b2c11b4defcfaa5831662117cce8b6f] [formerly e76d38bb0f85af9e6f527588d9036ccbadcb9fa8] [formerly eda89efd60a6bdc6d028024b8d65229e26f7e8df] [formerly 3ad2b88557dd49e7edd3604a324e751aef324846 [formerly c8cc126957cc9941daae9a32e58c920955f14375] [formerly eda89efd60a6bdc6d028024b8d65229e26f7e8df [formerly eda89efd60a6bdc6d028024b8d65229e26f7e8df [formerly d1557a40f52c89e3f0adfb2c4bcf2b786f499c7d]]]]
    Former-commit-id: cf43117af9aad47f9c72622f0e6246c834ad2714 [formerly c45995433c16680b0fbabe76b02fb5e576ef1da4 [formerly 725eab86971f369c0e4789fd9ed69600cd5e36e4] [formerly cf43117af9aad47f9c72622f0e6246c834ad2714 [formerly cf43117af9aad47f9c72622f0e6246c834ad2714 [formerly c2768ca8291f7ce1f2672deb5ed2f34cf63300d1]]]]
    Former-commit-id: 3afcbc012580ce497fa8f33a1fb1d91a2a599831
    Former-commit-id: 1bce987ad0c311b206aa60fd3cc4d1dd9821bad7
    Former-commit-id: 45ebdc43706a2da26d5e3023adb8b08f01a6c347
    Former-commit-id: ce9c33706693f0d8c38b65a3923706ca1b40e962

---
+++ dependencies/IlmBase/build-win32/Half/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/code/applications/mrViewer/dependencies/IlmBase/Half
+# Build directory: D:/code/applications/mrViewer/dependencies/IlmBase/build-win32/Half
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ dependencies/IlmBase/build-win32/IexMath/cmake_install.cmake
@@ -0,0 +1,45 @@
+# Install script for directory: D:/code/applications/mrViewer/dependencies/IlmBase/IexMath
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "D:/code/lib/Windows_32")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "D:/code/applications/mrViewer/dependencies/IlmBase/build-win32/IexMath/IexMath-2_2.lib")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE SHARED_LIBRARY FILES "D:/code/applications/mrViewer/dependencies/IlmBase/build-win32/IexMath/IexMath-2_2.dll")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/OpenEXR" TYPE FILE FILES
+    "D:/code/applications/mrViewer/dependencies/IlmBase/IexMath/IexMathFloatExc.h"
+    "D:/code/applications/mrViewer/dependencies/IlmBase/IexMath/IexMathFpu.h"
+    "D:/code/applications/mrViewer/dependencies/IlmBase/IexMath/IexMathIeeeExc.h"
+    )
+endif()
+
