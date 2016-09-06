69f5cfde6faba27b2437ff03251fa514fa301d70
Gonzalo Garramuno <ggarra13@gmail.com>
2016-09-06 18:02:57 -0300

    Added missing dependencies.
    Former-commit-id: 28375a70a98b832299733fd6cbd0bdbf5e3e16b3
    Former-commit-id: 189cc09987b4e43bb34dd2734fa77944b712f63a

---
+++ dependencies/IlmBase/build-win32/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: D:/code/applications/mrViewer/dependencies/IlmBase
+# Build directory: D:/code/applications/mrViewer/dependencies/IlmBase/build-win32
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(Half)
+subdirs(Iex)
+subdirs(IexMath)
+subdirs(Imath)
+subdirs(IlmThread)
+subdirs(HalfTest)
+subdirs(IexTest)
+subdirs(ImathTest)
