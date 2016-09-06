8cbe7cc75e22840a7c22cac3d6b6b2f5d9c4c7e4
Gonzalo Garramuno <ggarra13@gmail.com>
2016-09-06 18:02:57 -0300

    Added missing dependencies.
    Former-commit-id: 44c77dafb65243db9d442f55b6bc0d712000dec7
    Former-commit-id: 04b2d808ed7e983317549aba7c0fed570c039c19

---
+++ dependencies/IlmBase/build-linux64/IlmThread/cmake_install.cmake
@@ -0,0 +1,82 @@
+# Install script for directory: /media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/IlmThread
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  foreach(file
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libIlmThread-2_2.so.12.0.0"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libIlmThread-2_2.so.12"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libIlmThread-2_2.so"
+      )
+    if(EXISTS "${file}" AND
+       NOT IS_SYMLINK "${file}")
+      file(RPATH_CHECK
+           FILE "${file}"
+           RPATH "")
+    endif()
+  endforeach()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE SHARED_LIBRARY FILES
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/build-linux64/IlmThread/libIlmThread-2_2.so.12.0.0"
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/build-linux64/IlmThread/libIlmThread-2_2.so.12"
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/build-linux64/IlmThread/libIlmThread-2_2.so"
+    )
+  foreach(file
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libIlmThread-2_2.so.12.0.0"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libIlmThread-2_2.so.12"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libIlmThread-2_2.so"
+      )
+    if(EXISTS "${file}" AND
+       NOT IS_SYMLINK "${file}")
+      file(RPATH_CHANGE
+           FILE "${file}"
+           OLD_RPATH "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/build-linux64/Iex:"
+           NEW_RPATH "")
+      if(CMAKE_INSTALL_DO_STRIP)
+        execute_process(COMMAND "/usr/bin/strip" "${file}")
+      endif()
+    endif()
+  endforeach()
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/OpenEXR" TYPE FILE FILES
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/IlmThread/IlmThreadPool.h"
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/IlmThread/IlmThread.h"
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/IlmThread/IlmThreadSemaphore.h"
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/IlmThread/IlmThreadMutex.h"
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/IlmThread/IlmThreadNamespace.h"
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/IlmThread/IlmThreadExport.h"
+    "/media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/IlmThread/IlmThreadForward.h"
+    )
+endif()
+
