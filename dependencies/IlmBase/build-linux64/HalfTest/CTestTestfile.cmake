68d5118f14e1fb2596b3cf1bc93897b6f3d668cf
Gonzalo Garramuno <ggarra13@gmail.com>
2016-09-06 18:02:57 -0300

    Added missing dependencies.
    Former-commit-id: 3dd60987056c0be309af0630c077568c3396eaab [formerly 2553adb5d00c90c9224ce81021cfc426ba24fada] [formerly 0dc7c48542de79415af5ca3cd67a2e802fe90ad7 [formerly 44c77dafb65243db9d442f55b6bc0d712000dec7]]
    Former-commit-id: eda89efd60a6bdc6d028024b8d65229e26f7e8df [formerly d1557a40f52c89e3f0adfb2c4bcf2b786f499c7d]
    Former-commit-id: c2768ca8291f7ce1f2672deb5ed2f34cf63300d1

---
+++ dependencies/IlmBase/build-linux64/HalfTest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/HalfTest
+# Build directory: /media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/build-linux64/HalfTest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TestHalf "HalfTest")
