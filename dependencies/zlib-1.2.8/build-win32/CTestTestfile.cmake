bbc2398b7879fb21c647b6881b964c7e587360ab
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:08:38 -0300

    Removed dependencies folder from repository
    Former-commit-id: ff2ce3738658aabb88b5e3a1309bdfabcf567c4e [formerly 175888ac0220937ea8065783d19cffc732e9a36d]
    Former-commit-id: 31e3f7f81d616b141722037f882847a3e2f50fa5
    Former-commit-id: 1da11d129d4453e501fc9e9f0048e42baa76fcad
    Former-commit-id: 9bf443f94e05dbc1ed4ebd3b11712a30bab226d4
    Former-commit-id: db0b43dae8b4b6245d492cb626270517109591a7
    Former-commit-id: 64c2f8c7e94ded769902923f028d843901c68cd6
    Former-commit-id: ec04ae25c1aca93d9b10e2807c4a9bcca1f2ddaf
    Former-commit-id: 6f443cb3a22759a076c64abf5b8433c30d8183f2

--- dependencies/zlib-1.2.8/build-win32/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/code/applications/mrViewer/dependencies/zlib-1.2.8
-# Build directory: D:/code/applications/mrViewer/dependencies/zlib-1.2.8/build-win32
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example "example")
