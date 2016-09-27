2620bf4df7cf2249a3a5e6720f3f35b28d7228c4
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: c2b09d2a884f52d2cd2d634779b93d5a31cae243 [formerly 44cd5d896172f62de3c8dffe9f9d7691d001c069] [formerly be7f4413581f1e6a30e2a02ed222f199dfec4bd9] [formerly 0ad2c685f4851e3b6bdd2c025c9c0e2f3546abf9] [formerly 3ba666c6064b0c4f89be8953a96593421ad5247a [formerly 04c6e1a3f82f1ce1448ac67759e5930a0a02d942 [formerly 4cc800b0bce2891cf4eee02a947759c0992b30e3] [formerly 3ba666c6064b0c4f89be8953a96593421ad5247a [formerly 3ba666c6064b0c4f89be8953a96593421ad5247a [formerly 3ba666c6064b0c4f89be8953a96593421ad5247a [formerly ae9684b156998d95582f56d355c238ba35697ce7]]]]]] [formerly 37d2a2244b9beb7c9a1fa7ce93e9706b735360d4 [formerly 35fcf37d7646e407c39ffdc59e8506e3f1e90aa0] [formerly 86e43ee97367a810bc442754133f4ea462ab76b5] [formerly b9dbaaeeccfd271c506ae09be2bba061bc77f956] [formerly 254e02ecb85d66b0eed700575cc93d8343e6e8d2] [formerly b60ad9940a02d4e7229ec3d76cc390e139164ba9 [formerly cb4d83da5dad3557ae006033543004823e04fb11] [formerly 254e02ecb85d66b0eed700575cc93d8343e6e8d2 [formerly 254e02ecb85d66b0eed700575cc93d8343e6e8d2 [formerly 254e02ecb85d66b0eed700575cc93d8343e6e8d2 [formerly 6e630d191ef77f1905ebd5f72d6edefe4b98e9b7]]]]] [formerly 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 142efd0037f218d53fcbc6903187a71bd8b5dd32]]]]]]
    Former-commit-id: 37d2a2244b9beb7c9a1fa7ce93e9706b735360d4 [formerly 35fcf37d7646e407c39ffdc59e8506e3f1e90aa0] [formerly 86e43ee97367a810bc442754133f4ea462ab76b5] [formerly b9dbaaeeccfd271c506ae09be2bba061bc77f956] [formerly 254e02ecb85d66b0eed700575cc93d8343e6e8d2] [formerly b60ad9940a02d4e7229ec3d76cc390e139164ba9 [formerly cb4d83da5dad3557ae006033543004823e04fb11] [formerly 254e02ecb85d66b0eed700575cc93d8343e6e8d2 [formerly 254e02ecb85d66b0eed700575cc93d8343e6e8d2 [formerly 254e02ecb85d66b0eed700575cc93d8343e6e8d2 [formerly 6e630d191ef77f1905ebd5f72d6edefe4b98e9b7]]]]]
    Former-commit-id: 0a22e815a201c61346c8dddbb3e335ac52beeb1e [formerly 0a22e815a201c61346c8dddbb3e335ac52beeb1e [formerly a03cae75c0dc96e72ca36093467b7c82f97a381c]] [formerly 579a62240df209e3ec34b656f3be629d42ab2e2f [formerly e5516ca4bb49c3f3e3ba180185a93163ddfbe386] [formerly 0a22e815a201c61346c8dddbb3e335ac52beeb1e [formerly 0a22e815a201c61346c8dddbb3e335ac52beeb1e [formerly a03cae75c0dc96e72ca36093467b7c82f97a381c]] [formerly 0a22e815a201c61346c8dddbb3e335ac52beeb1e [formerly a03cae75c0dc96e72ca36093467b7c82f97a381c] [formerly a03cae75c0dc96e72ca36093467b7c82f97a381c [formerly de30394ad8eb2e6c6802552d56e2d238f160846c]]]]]
    Former-commit-id: b21697a9083133cf0dd205f7eccb690b86806a60
    Former-commit-id: f9139a6d4719baf7ec9b8ccd465327e44fcc61af
    Former-commit-id: 7468da01051a2f198f5d02ecd5110f44aae4cdf6
    Former-commit-id: b33030ed26bbc8b4f9c8c02b446056a80b912977
    Former-commit-id: 2a87de4eba31b62c22f198c81e5f0f9d2ae0da49

--- dependencies/OpenEXR/build-linux64/IlmImfTest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/IlmImfTest
-# Build directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/build-linux64/IlmImfTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestIlmImf "IlmImfTest")
