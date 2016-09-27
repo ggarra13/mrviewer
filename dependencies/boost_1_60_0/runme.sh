5888cde7ef2859ef4965c371f17e3cf9643a58ed
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 142efd0037f218d53fcbc6903187a71bd8b5dd32]]
    Former-commit-id: 3ba666c6064b0c4f89be8953a96593421ad5247a [formerly ae9684b156998d95582f56d355c238ba35697ce7]
    Former-commit-id: ae3e042e9451321265b414829351931bd1f72418

--- dependencies/boost_1_60_0/runme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash --norc
-
-# ./b2 -j 8 --build-type=minimal address-model=64 --without-python variant=release link=shared threading=multi runtime-link=shared
-
-./b2 -j 8 --without-python address-model=64 variant=release
