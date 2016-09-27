b8cc1d217bc11b2330b6bf9d30b271eba7f991db
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 142efd0037f218d53fcbc6903187a71bd8b5dd32]
    Former-commit-id: ffc64d4c63986d15696c4f4646b31857a817984b

--- dependencies/OpenEXR/build-linux64/exrenvmap/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/exrenvmap
-# Build directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/build-linux64/exrenvmap
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
