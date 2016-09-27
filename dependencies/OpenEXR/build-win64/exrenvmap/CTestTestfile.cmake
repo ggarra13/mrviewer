6e630d191ef77f1905ebd5f72d6edefe4b98e9b7
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: 142efd0037f218d53fcbc6903187a71bd8b5dd32

--- dependencies/OpenEXR/build-win64/exrenvmap/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/code/applications/mrViewer/dependencies/OpenEXR/exrenvmap
-# Build directory: D:/code/applications/mrViewer/dependencies/OpenEXR/build-win64/exrenvmap
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
