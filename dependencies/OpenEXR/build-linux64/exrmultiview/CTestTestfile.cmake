4d07353b98c95ddacae8ae7b339e2fd898cbd664
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository

--- dependencies/OpenEXR/build-linux64/exrmultiview/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/exrmultiview
-# Build directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/build-linux64/exrmultiview
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
