46d505d1643a2e8e46d3af075f065fc789a074e3
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: 3ba666c6064b0c4f89be8953a96593421ad5247a [formerly 3ba666c6064b0c4f89be8953a96593421ad5247a [formerly ae9684b156998d95582f56d355c238ba35697ce7]] [formerly 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 142efd0037f218d53fcbc6903187a71bd8b5dd32]]]
    Former-commit-id: 254e02ecb85d66b0eed700575cc93d8343e6e8d2 [formerly 254e02ecb85d66b0eed700575cc93d8343e6e8d2 [formerly 6e630d191ef77f1905ebd5f72d6edefe4b98e9b7]]
    Former-commit-id: a03cae75c0dc96e72ca36093467b7c82f97a381c [formerly de30394ad8eb2e6c6802552d56e2d238f160846c]
    Former-commit-id: 815d95fd3014109029948c4a7eeb13d3e4b0bd5b

--- dependencies/IlmBase/build-linux64/IlmThread/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/IlmThread
-# Build directory: /media/gga/Datos/code/applications/mrViewer/dependencies/IlmBase/build-linux64/IlmThread
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- dependencies/ImageMagick-6.9.3-0-win64/openjpeg/src/lib/openjp2/opj_config_private.h
+++
@@ -1,2 +0,0 @@
-/* create opj_config_private.h for CMake */
-#define OPJ_PACKAGE_VERSION "2.1.0"
