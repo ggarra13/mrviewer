ffc64d4c63986d15696c4f4646b31857a817984b
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: 142efd0037f218d53fcbc6903187a71bd8b5dd32

--- dependencies/OpenEXR/build-win64/IlmImfUtil/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/code/applications/mrViewer/dependencies/OpenEXR/IlmImfUtil
-# Build directory: D:/code/applications/mrViewer/dependencies/OpenEXR/build-win64/IlmImfUtil
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- dependencies/OpenEXR/build-win64/exrmakepreview/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/code/applications/mrViewer/dependencies/OpenEXR/exrmakepreview
-# Build directory: D:/code/applications/mrViewer/dependencies/OpenEXR/build-win64/exrmakepreview
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
