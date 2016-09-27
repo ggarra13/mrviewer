18987d927e49b93474f8c523f1290fbc97a21900
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: 9e2b6f3586b914602d2f4d4ca267830853786618 [formerly 751ebcddcb0f7f112c8bbe13bcaf1c63e4f6db12]
    Former-commit-id: 2bda28c053a545c524e43883ab1cd13df9d163b9 [formerly 17354f94497dc990a47e97bca5868fc0c57cc1a8]
    Former-commit-id: 209279c69c726c8277358cffeb439b29c1a1e9f6

--- dependencies/OpenEXR/build-linux64/IlmImfUtil/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/IlmImfUtil
-# Build directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/build-linux64/IlmImfUtil
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
