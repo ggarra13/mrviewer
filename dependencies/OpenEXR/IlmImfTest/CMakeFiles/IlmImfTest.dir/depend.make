e00730b8489f555931d661da3d161735aa9f30f4
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: 64be158dbe109f2f6e6bf9a4ac6b63f7527864a6 [formerly 142efd0037f218d53fcbc6903187a71bd8b5dd32]
    Former-commit-id: 6e630d191ef77f1905ebd5f72d6edefe4b98e9b7

--- dependencies/OpenEXR/IlmImfTest/CMakeFiles/IlmImfTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for IlmImfTest.
-# This may be replaced when dependencies are built.
--- dependencies/OpenEXR/exrstdattr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/code/lib/openexr-git/OpenEXR/exrstdattr
-# Build directory: F:/code/lib/openexr-git/OpenEXR/exrstdattr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
