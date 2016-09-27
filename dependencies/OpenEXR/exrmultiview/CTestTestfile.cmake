209279c69c726c8277358cffeb439b29c1a1e9f6
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: 9e2b6f3586b914602d2f4d4ca267830853786618 [formerly 751ebcddcb0f7f112c8bbe13bcaf1c63e4f6db12]
    Former-commit-id: 17354f94497dc990a47e97bca5868fc0c57cc1a8

--- dependencies/OpenEXR/exrmultiview/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/code/lib/openexr-git/OpenEXR/exrmultiview
-# Build directory: F:/code/lib/openexr-git/OpenEXR/exrmultiview
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- dependencies/freeglut-3.0.0/build-win64/CMakeFiles/freeglut_static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "lib/freeglut_static.lib"
-)
