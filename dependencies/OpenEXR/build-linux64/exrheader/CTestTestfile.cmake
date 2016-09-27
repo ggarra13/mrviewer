17354f94497dc990a47e97bca5868fc0c57cc1a8
Gonzalo Garramuño <ggarra13@gmail.com>
2016-09-26 21:14:39 -0300

    Removed dependencies folder from repository
    Former-commit-id: 751ebcddcb0f7f112c8bbe13bcaf1c63e4f6db12

--- dependencies/OpenEXR/build-linux64/exrheader/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/exrheader
-# Build directory: /media/gga/Datos/code/applications/mrViewer/dependencies/OpenEXR/build-linux64/exrheader
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
