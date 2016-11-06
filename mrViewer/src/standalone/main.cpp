6457f2cce147a2b48ff873cdf466b6e52a334b6b
Gonzalo Garramuño <ggarra13@gmail.com>
2016-11-06 13:33:19 -0300

    Removed debugging info.
    Former-commit-id: 7803f9a0edd5f5d9c68b66cfa186c2a318c4833a [formerly 044c4fe6445ec4dda166e6294ec154315e84d2db]
    Former-commit-id: 0e9e0c315932ba6a993a038cae2a7d5cb332e6c4 [formerly 3fadc4ef242c5528ff5b32fd76a207cdf3078ad3]
    Former-commit-id: d62e60e63364a20370518066b7f45edf05e5bf39

--- mrViewer/src/standalone/main.cpp
+++ mrViewer/src/standalone/main.cpp
@@ -215,7 +215,7 @@ int main( int argc, char** argv )
   mrv::set_root_path( argc, argv );
 
 
-  fltk::lock(); // start FLTK's lock mechanism
+  //fltk::lock(); // start FLTK's lock mechanism
 
   // Adjust ui based on preferences
   for (;;) {
