70a33b98f35f6e85360972e0a4ad92e6dd6efb12
Gonzalo Garramuño <ggarra13@gmail.com>
2016-11-08 08:43:19 -0300

    Improved start of movie files in EDL.
    Former-commit-id: 0d460969d014878792e7fce6d3f8982c88adccd0 [formerly 0d460969d014878792e7fce6d3f8982c88adccd0 [formerly 0d07b254bdf2d616cc17fa003f56c1828d646afd]] [formerly a0dc9a31d2c4555a7ad4caec53cc907b4ffeb614 [formerly a0dc9a31d2c4555a7ad4caec53cc907b4ffeb614 [formerly c26f823c835397119715be6d4605ee65eca76015]]]
    Former-commit-id: 6f3247450f7400d6e1d9467c8e8a82fc1abdafc0 [formerly 6f3247450f7400d6e1d9467c8e8a82fc1abdafc0 [formerly 76744ee51ee161215c1d2ff7542703366f3da43a]]
    Former-commit-id: 3935f9610daf02ad4e90b1186c1873a782a28918 [formerly 04c2ff81b70a6e5b4a970c21a38edeafd8d9ba13]
    Former-commit-id: 43fd9d8dac0b38793f9c553b67f2080d29ab0609

--- mrViewer/src/gui/mrvImageBrowser.cpp
+++ mrViewer/src/gui/mrvImageBrowser.cpp
@@ -1343,7 +1343,7 @@ void ImageBrowser::load_stereo( mrv::media& fg,
 
     if ( img->has_video() || img->has_audio() )
     {
-        img->fetch( img->first_frame() );
+        img->seek( first );
     }
     else
     {
