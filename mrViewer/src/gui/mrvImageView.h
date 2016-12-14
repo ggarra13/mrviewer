2779da62a9c87f09aad6d378549b97e877042bda
Gonzalo Garramuño <ggarra13@gmail.com>
2016-12-14 18:40:52 -0300

    Fixed left-right stereo input being wrong on display window disappearing.  It would not show the new values of the elements beyond the edge.
    Former-commit-id: 35295b5dbb414f25191a8effea65bad9109676c3 [formerly c05badb913c42f3fbd559e82cbc15ce4349946fe] [formerly ffef37f1a45ae06f2a638cf43d5325822e6f0c38 [formerly 353fb8a676133621adff87b160a0b6b6a9b5b39b]]
    Former-commit-id: a21172bf8247f910e63ef1d40ff84d11d2e0bd47 [formerly 8b8876cd106ff8ff6982b87adec58bf7abeb1353]
    Former-commit-id: cdc2d696f603583ad620224ed899c0c4a4612bf2

--- mrViewer/src/gui/mrvImageView.h
+++ mrViewer/src/gui/mrvImageView.h
@@ -567,7 +567,8 @@ class server;
 
       void separate_layers( const CMedia* const img,
                             mrv::image_type_ptr& pic, int& xp, int& yp,
-                            short& idx, int w, int h ) const;
+                            short& idx, int w, int h,
+                            const mrv::Recti& dpw ) const;
       void top_bottom( const CMedia* const img,
                        mrv::image_type_ptr& pic, int& xp, int& yp,
                        short& idx, int w, int h ) const;
