b0154e595a6d3056347381e2cee4834143fbf07b
Gonzalo Garramuño <ggarra13@gmail.com>
2016-12-18 12:20:14 -0300

    Fixed a _video_packet.empty() due to lack of mutex.
    Former-commit-id: 11acb4eef7669a3dd0238b2eae0d609d5234910d [formerly 11acb4eef7669a3dd0238b2eae0d609d5234910d [formerly d63676e29a3d00a5a1ce355746c0dfd088802936]]
    Former-commit-id: a51f142e87370cbe244c24b81d6570a26d468163 [formerly 93e230d79b5a5fc71c2757aab6412654ed2e80f8]
    Former-commit-id: 038893b772c1461badec094a6bf3886b79f8bfba

--- upload_docs.sh
+++ upload_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -avP -e ssh docs ggarra13@web.sourceforge.net:/home/project-web/mrviewer/htdocs
+rsync -avP --exclude '*~' -e ssh docs ggarra13@web.sourceforge.net:/home/project-web/mrviewer/htdocs
 
