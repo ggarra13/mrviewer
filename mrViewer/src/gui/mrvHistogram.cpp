1f2772bd7ce616ca500c1daa049ebc6318f03ddb
Gonzalo Garramuño <ggarra13@gmail.com>
2016-12-19 20:58:17 -0300

    Fixed normalize not updating histogram, color area and vectorscope.
    Former-commit-id: 805fa8aa6e3c339d119e3a8ce653f50cc5f8a769
    Former-commit-id: 862e3842c160bb99e474b2c464eaebf1cda232cc

--- mrViewer/src/gui/mrvHistogram.cpp
+++ mrViewer/src/gui/mrvHistogram.cpp
@@ -197,6 +197,11 @@ namespace mrv
 	  { 
               CMedia::Pixel op = pic->pixel( x, y );
 
+              if ( uiMain->uiView->normalize() )
+              {
+                  uiMain->uiView->normalize( op );
+              }
+               
               if ( uiMain->uiView->use_lut() && v == ImageView::kRGBA_Full )
               {
                   engine->evaluate( img, 
