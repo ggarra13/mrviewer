8672d69ef4d2ac36bce879359982270c27afcc98
Gonzalo Garramuño <ggarra13@gmail.com>
2017-03-14 12:59:01 -0300

    Made GLSL shader receive floats instead of ints except for channel variable. This works around bug in GLSL compiler.
    Former-commit-id: bf64e76aa4748d703ae3ae43b1a345d3b2f41e2d [formerly 13f5a9d492a51993c4fbac05e8d2f8aa6ebdc547]
    Former-commit-id: 67083455dae33a8e5ac5ad28b0ce7751fdbe39d0

--- mrViewer/src/video/mrvGLEngine.h
+++ mrViewer/src/video/mrvGLEngine.h
@@ -35,7 +35,7 @@
 #include "mrvDrawEngine.h"
 
 #if 0
-#  define CHECK_GL GLEngine::handle_gl_errors( __FUNCTION__, __LINE__ )
+#  define CHECK_GL GLEngine::handle_gl_errors( __FUNCTION__, __LINE__, true )
 #else
 #  define CHECK_GL
 #endif
@@ -130,7 +130,8 @@ namespace mrv {
 
 
     /// Auxiliary function used to check for openGL errors
-      static void handle_gl_errors(const char* function, const unsigned line);
+      static void handle_gl_errors(const char* function, const unsigned line,
+                                   const bool print = false);
 
   protected:
       void set_matrix( const ImageView::FlipDirection flip = 
