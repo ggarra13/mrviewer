fdff9791e44630f17dc0c3d652af5871ea062b83
Gonzalo Garramuño <ggarra13@gmail.com>
2017-04-25 18:28:34 -0300

    Made # character be the last in checks for frame spec.  First, it checks %, then @ and finally #.
    Former-commit-id: 7b0820e1bbb5a1256be085a7a49e32dbd943985c [formerly 49086b5757f8db9f4eb5529de8d5de3b0e92497d] [formerly 3a427550d0f78072b3d472daefaa70e3dddd11b5] [formerly b9a2b204c4d220849d6117512e690dadafde42b3] [formerly 076f575069b102b011423a46be73ee5804929cc1 [formerly 2dce447606fc173a1fa8d01a8ecee28653bd6c10 [formerly d45ef14322ac246365ca2392e9ec61015552d513] [formerly 076f575069b102b011423a46be73ee5804929cc1 [formerly 076f575069b102b011423a46be73ee5804929cc1 [formerly b6cd3fb635d720cd3dc73f71e4b265cbdf042d6b]]]]] [formerly 1f874d2b1be20f0ead3e90e67cd22fbbedfbccb0 [formerly d5a588fde0b478ebf66e9a142958fd047f4018ae] [formerly 1e03869251588d2399256b702ab26ccaf21ac61a] [formerly 5768a707e7a60276807aaca16870f04bec51b97e] [formerly 62e284931a62c64b16dab5eb2284e9804d552064 [formerly 9b5be28055801b5776f97d49732501d89d2b16ba] [formerly 5768a707e7a60276807aaca16870f04bec51b97e [formerly 5768a707e7a60276807aaca16870f04bec51b97e [formerly 50a5688602f666f54c31093c1dd64b8f54e14bd7]]]] [formerly fe6ce898762405efbf6f97d32778a599a0a5915e [formerly fe6ce898762405efbf6f97d32778a599a0a5915e [formerly fe6ce898762405efbf6f97d32778a599a0a5915e [formerly 01ab3c5a2775489ccb7b99d3ac0b8cd7126a5e25]]]]]
    Former-commit-id: 1f874d2b1be20f0ead3e90e67cd22fbbedfbccb0 [formerly d5a588fde0b478ebf66e9a142958fd047f4018ae] [formerly 1e03869251588d2399256b702ab26ccaf21ac61a] [formerly 5768a707e7a60276807aaca16870f04bec51b97e] [formerly 62e284931a62c64b16dab5eb2284e9804d552064 [formerly 9b5be28055801b5776f97d49732501d89d2b16ba] [formerly 5768a707e7a60276807aaca16870f04bec51b97e [formerly 5768a707e7a60276807aaca16870f04bec51b97e [formerly 50a5688602f666f54c31093c1dd64b8f54e14bd7]]]]
    Former-commit-id: c0927c77054cdc074ee0f1f6055f4c0cb5868588 [formerly 101a6a69aea8f5379939824175a2764f0c648dfd] [formerly 0e4ef5748b5fc55e46fa81a158eef630408fee2a [formerly 2e4737e5544067d8445b65b5b081c6e87e35f54a] [formerly c0927c77054cdc074ee0f1f6055f4c0cb5868588 [formerly 101a6a69aea8f5379939824175a2764f0c648dfd] [formerly 101a6a69aea8f5379939824175a2764f0c648dfd [formerly 29921ede9c56842f95bd751831dc143c82be56b1]]]]
    Former-commit-id: 69d1f46bb5f024d414060c5fdfadfc419cfb39f7
    Former-commit-id: 768b23cd4c153df795a4a827ff0b2354132284f9
    Former-commit-id: ac262797680dc9f6fa0e577aef370d5d6d6eb5e6
    Former-commit-id: 72ff76fa66c468fde1f006f596181157dcab2f25

--- mrViewer/src/core/Sequence.cpp
+++ mrViewer/src/core/Sequence.cpp
@@ -483,8 +483,8 @@ bool is_valid_view( std::string view )
             size_t pos;
             std::string fspec;
             if ( ( pos = root.find('%') ) != std::string::npos ||
-                 ( pos = root.find('#') ) != std::string::npos ||
-                 ( pos = root.find('@') ) != std::string::npos )
+                 ( pos = root.find('@') ) != std::string::npos ||
+                 ( pos = root.find('#') ) != std::string::npos )
             {
                 fspec = root.substr( pos, root.size() - pos - 1 );
                 if ( is_valid_frame_spec( fspec ) )
