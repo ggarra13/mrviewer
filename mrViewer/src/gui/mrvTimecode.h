630db67d848fb2a1eab602fbf36d939a5c9178ec
Gonzalo Garramuño <ggarra13@gmail.com>
2017-06-05 21:14:52 -0300

    bug fixed timecode but it is still wrong on some movies by 1 frame.
    Former-commit-id: 6abdfbabaef6d606c01da843cde288b8ee4219d5 [formerly 107b6e5c419cd73b12138de2d0925f575a12672a]
    Former-commit-id: 9f61d2d922f5df6278baaea1055f96d7fc719913

--- mrViewer/src/gui/mrvTimecode.h
+++ mrViewer/src/gui/mrvTimecode.h
@@ -37,17 +37,17 @@
 namespace mrv
 {
 
-  class Timecode : public fltk::FloatInput
-  {
+class Timecode : public fltk::FloatInput
+{
   public:
     enum Display
-      {
-	kFrames,
-	kSeconds,
-	kTime,
-	kTimecodeNonDrop,
-	kTimecodeDropFrame
-      };
+    {
+    kFrames,
+    kSeconds,
+    kTime,
+    kTimecodeNonDrop,
+    kTimecodeDropFrame
+    };
 
   public:
     Timecode( int x, int y, int w, int h, const char* l = 0 );
@@ -69,7 +69,10 @@ namespace mrv
     void step( const int64_t x)    { _step = x; }
 
     int64_t frame() const { return _frame; }
-    void frame( const int64_t f ) { value(f); }
+    void frame( const int64_t& f ) { value(f); }
+
+    void timecode( const int64_t& f ) { _tc_frame = f; }
+    int64_t timecode() const          { return _tc_frame; }
 
     int64_t value() const;
     void value( const int64_t f );
@@ -77,8 +80,8 @@ namespace mrv
 		const int frames );
 
     static int format( char* buf, const Display display, 
-		       const int64_t v, const double fps,
-		       const bool withFrames = false );
+		       const int64_t& frame, const int64_t& tc,
+                       const double fps, const bool withFrames = false );
 
     virtual bool replace(int, int, const char*, int);
 
@@ -86,12 +89,12 @@ namespace mrv
     void update();
 
     static bool valid_drop_frame( int hours, int mins, int secs, int frames,
-				  double fps );
+                                  double fps );
 
     Display      _display;
     double       _fps;
-    int64_t      _frame, _minimum, _maximum, _step;
-  };
+    int64_t      _frame, _tc_frame, _minimum, _maximum, _step;
+};
 
 } // namespace mrv
 
