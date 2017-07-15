7f3ad31cdac0b608001b7192eb18297af88a5f35
Gonzalo Garramuno <ggarra13@gmail.com>
2017-07-15 19:11:43 -0300

    Added missing YCbCrA and YByRyA shaders.
    Former-commit-id: 03d321eb98fbc09eda531537295043704e74ddf4

---
+++ shaders/YByRyA.glsl
@@ -0,0 +1,150 @@
+/**
+ * @file   rgba.glsl
+ * @author gga
+ * @date   Thu Jul  5 22:50:08 2007
+ * 
+ * @brief    simple YByRy texture with 3D lut shader
+ * 
+ */
+
+
+// Images
+uniform sampler2D YImage;
+uniform sampler2D UImage;
+uniform sampler2D VImage;
+uniform sampler3D lut;
+
+// Interlaced/Checkerboard controls
+uniform int mask;
+uniform int mask_value;
+uniform int height;
+uniform int width;
+
+// Standard controls
+uniform float gain;
+uniform float gamma;
+uniform int   channel;
+
+// Normalization variables
+uniform bool  premult;
+uniform bool  unpremult;
+uniform bool  enableNormalization;
+uniform float normMin;
+uniform float normSpan;
+
+// Y B-Y R-Y variables
+uniform vec3  yw;
+
+// Lut variables
+uniform bool  enableLut;
+uniform bool  lutF;
+uniform float lutMin;
+uniform float lutMax;
+uniform float lutM;
+uniform float lutT;
+
+
+void main()
+{ 
+  //
+  // Sample luminance and chroma, convert to RGB. 
+  //
+  vec3 pre;
+  pre.r = texture2D(YImage, gl_TexCoord[0].st).r;  // Y
+  pre.g = texture2D(UImage, gl_TexCoord[0].st).r;  // U
+  pre.b = texture2D(VImage, gl_TexCoord[0].st).r;  // V
+
+  vec4 c;
+  c.r = (pre.g + 1.0) * pre.r;
+  c.b = (pre.b + 1.0) * pre.r;
+  c.g = (pre.r - c.r * yw.x - c.b * yw.z) / yw.y;
+  c.a = 1.0;
+
+  //
+  // Apply normalization
+  //
+  if (enableNormalization)
+    {
+      c.rgb = (c.rgb - normMin) / normSpan;
+    }
+
+  //
+  // Apply gain 
+  //
+  c.rgb *= gain;
+
+  //
+  // Apply 3D color lookup table (in log space).
+  //
+  if (enableLut)
+    {
+      c.rgb = lutT + lutM * log( clamp(c.rgb, lutMin, lutMax) );
+      c.rgb = exp( texture3D(lut, c.rgb).rgb ); 
+    }
+
+  //
+  // Apply video gamma correction.
+  // 
+  c.r = pow( c.r, gamma );
+  c.g = pow( c.g, gamma );
+  c.b = pow( c.b, gamma );
+
+  //
+  // Apply channel selection
+  // 
+  if ( channel == 1 )
+    {
+      c.rgb = c.rrr;
+    }
+  else if ( channel == 2 )
+    {
+      c.rgb = c.ggg;
+    }
+  else if ( channel == 3 )
+    {
+      c.rgb = c.bbb;
+    }
+  else if ( channel == 4 )
+    {
+      c.rgb = c.aaa;
+    }
+  else if ( channel == 5 )
+    {
+      c.r *= 0.5;
+      c.r += c.a * 0.5;
+    }
+  else if ( channel == 6 )
+    {
+      c.rgb = vec3( (c.r + c.g + c.b) / 3.0 );
+    }
+
+
+  vec2 tc = gl_TexCoord[0].st;
+  c.rgb = clamp( c.rgb, 0.0, 1.0 );
+  int x = 1000;
+  
+  if ( mask == 1 )  // even odd rows
+  {
+      float f = tc.y * height;
+      x = int( mod( f, 2 ) );
+  }
+  else if ( mask == 2 ) // even odd columns
+  {
+      float f2 = tc.x * width;
+      x = int( mod( f2, 2 ) );
+  }
+  else if ( mask == 3 ) // checkerboard
+  {
+      float f = tc.y * height;
+      float f2 = tc.x * width;
+      x = int( mod( floor( f2 ) + floor( f ), 2 ) < 1 );
+  }
+
+  if ( x == mask_value )
+  {
+      c.r = c.g = c.b = c.a = 0.0;
+  }
+
+
+  gl_FragColor = c;
+} 
