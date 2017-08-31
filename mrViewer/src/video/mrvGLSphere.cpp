1f8a91e8058b350e3ea8ba3ae78831ca4773ed16
Gonzalo Garramuño <ggarra13@gmail.com>
2017-08-31 11:16:26 -0300

    Clean up of PreferenceUI's tabs.  Documented changes in video/audio. Removed Video->uiPrefsStereoRightEye.x
    Former-commit-id: f857b89f86c7648220bf64b3f4de140103b1b074 [formerly 2974c108d5817690c0c860d6b0b859b3f6490486] [formerly 7104a191a4be1746dbd039697993237a2fb06921] [formerly 89b49b23c1ad88f6478f14a66a823e0225fd1318] [formerly d028596c508597d9322561f4255e3cdf45eb35e9 [formerly 798f806fad4a971fb554ccf4c57027c7e5e65b8c [formerly 86851395d3a4d7a61f1f7437dec9c51842d27558] [formerly d028596c508597d9322561f4255e3cdf45eb35e9 [formerly d028596c508597d9322561f4255e3cdf45eb35e9 [formerly d028596c508597d9322561f4255e3cdf45eb35e9 [formerly d028596c508597d9322561f4255e3cdf45eb35e9 [formerly 38e28f4addfba6d9a42e77e9b9138cc11b99d2e3]]]]]]] [formerly c88b40d764ee82d118e2b8bd4902766f8ed96ad3 [formerly dbc1b92ec76df1c2a96ec76f28ba6856a9f61fd3] [formerly ec252d9987ec5fcaf98a842752925865c7af2fca] [formerly e7162637f4743d2c4a8d15cd08ed5e4cd1fda394] [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b] [formerly f7541d50d988fe32c872c5e0b51061d0d729fd16 [formerly b8f4a661da4d25e1c7045f2a413e922fe8e0ac13] [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b [formerly b3c9d0a4732b45cb0398aadd5aebcd9fb38fc936]]]]]] [formerly b9d5b91e617ae3cdc8a1d4636d285413bb06f097 [formerly b9d5b91e617ae3cdc8a1d4636d285413bb06f097 [formerly b9d5b91e617ae3cdc8a1d4636d285413bb06f097 [formerly b9d5b91e617ae3cdc8a1d4636d285413bb06f097 [formerly b9d5b91e617ae3cdc8a1d4636d285413bb06f097 [formerly 824f2d7f6d057f0c585cdc9d3c80c75900349432]]]]]]]
    Former-commit-id: c88b40d764ee82d118e2b8bd4902766f8ed96ad3 [formerly dbc1b92ec76df1c2a96ec76f28ba6856a9f61fd3] [formerly ec252d9987ec5fcaf98a842752925865c7af2fca] [formerly e7162637f4743d2c4a8d15cd08ed5e4cd1fda394] [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b] [formerly f7541d50d988fe32c872c5e0b51061d0d729fd16 [formerly b8f4a661da4d25e1c7045f2a413e922fe8e0ac13] [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b [formerly 177d7e40e2c109910b445353c0f6bc27e2f5ee5b [formerly b3c9d0a4732b45cb0398aadd5aebcd9fb38fc936]]]]]]
    Former-commit-id: 4013bd0dcb954e765bfcfb81768765160b55bcbf [formerly 032f7aaf8672c15805ca60ae788a9b13387160b4 [formerly 7c368bdc517576e7e4b08518ef85c50c2be38162] [formerly 4013bd0dcb954e765bfcfb81768765160b55bcbf [formerly 4013bd0dcb954e765bfcfb81768765160b55bcbf [formerly 4013bd0dcb954e765bfcfb81768765160b55bcbf [formerly 4013bd0dcb954e765bfcfb81768765160b55bcbf [formerly 09e3747a3aba1d4b89c54d5e99c9d26b9110020b]]]]]]
    Former-commit-id: 499592f16e776bf8e582c012d42d1fcdf3d48e52
    Former-commit-id: a09ebd988b6b988d4a26f5a26ff0315cf61c8650
    Former-commit-id: bfd5091e0b48ca448669f5aeb7b9f26125871223
    Former-commit-id: 41371364fdc94bf39665e61be0e74e01d48b313e
    Former-commit-id: 7b1b1e1d31bb99e1f9ff35a6ed15931d62231135 [formerly afbb0b3dcb17d54c73baf3dc5603e98cd9cf3cae]
    Former-commit-id: 3faa5d97ea845d5d313ef242f17e69be05692153

--- mrViewer/src/video/mrvGLSphere.cpp
+++ mrViewer/src/video/mrvGLSphere.cpp
@@ -203,7 +203,7 @@ GLSphere::~GLSphere()
 	    _shader->setUniform( "lutMax", _lut->lutMax );
 	    _shader->setUniform( "lutM", _lut->lutM );
 	    _shader->setUniform( "lutT", _lut->lutT );
-	    // 	  _shader->setUniform( "lutF", _lut->lutF );
+	    _shader->setUniform( "lutF", false );
 	  }
 	else
 	  {
@@ -216,17 +216,17 @@ GLSphere::~GLSphere()
         int premult = 0, unpremult = 0;
 
         switch ( v->uiPrefs->uiPrefsBlendMode->value() )
-        {
-            case ImageView::kBlendPremultNonGamma:
-                unpremult = premult = 1;
-                break;
-            case ImageView::kBlendTraditionalNonGamma:
-                unpremult = premult = 1;
-                break;
-            default:
-                unpremult = premult = 0;
-                break;
-        }
+	  {
+	  case ImageView::kBlendPremultNonGamma:
+	    unpremult = premult = 1;
+	    break;
+	  case ImageView::kBlendTraditionalNonGamma:
+	    unpremult = premult = 1;
+	    break;
+	  default:
+	    unpremult = premult = 0;
+	    break;
+	  }
 
         _shader->setUniform( "premult", premult );
         if ( _shader == GLEngine::rgbaShader() )
