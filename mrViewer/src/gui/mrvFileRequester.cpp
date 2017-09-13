bec395c94c8b4e0b2ee1274681abe9b18ceb64a0
Gonzalo Garramuño <ggarra13@gmail.com>
2017-09-13 10:34:41 -0300

    Made EDL Window's timeline follow the main viewer timeline always. Made changing of pattern in file requester reload it to avoid wrong files being displayed.
    Former-commit-id: 0e4417b17f6663f424213f5e98c5dcfe38e49973

--- mrViewer/src/gui/mrvFileRequester.cpp
+++ mrViewer/src/gui/mrvFileRequester.cpp
@@ -76,7 +76,7 @@ static const char* kModule = "file";
   // File extension patterns
   static const std::string kReelPattern = "reel";
 
-  static const std::string kMoviePattern = "avi,AVI,divx,DIVX,dv,DV,flv,FLV,mkv,MKV,m4v,M4V,mp4,MP4,mpg,MPG,mpeg,MPEG,mov,MOV,mxf,MXF,qt,QT,vob,VOB,vp9,VP9,webm,WEBM,wmv,WMV,y4m,Y4M";
+  static const std::string kMoviePattern = "3gp,3GP,asf,ASF,avc,AVC,avchd,AVCHD,avi,AVI,divx,DIVX,dv,DV,flv,FLV,m2ts,M2TS,m2t,M2T,mkv,MKV,m4v,M4V,mp4,MP4,mpg,MPG,mpeg,MPEG,mov,MOV,mxf,MXF,ogm,OGM,ogv,OGV,qt,QT,ts,TS,vob,VOB,vp9,VP9,webm,WEBM,wmv,WMV,y4m,Y4M";
 
   static const std::string kImagePattern =
     "bmp,bit,cin,CIN,ct,dng,DNG,dpx,DPX,exr,EXR,gif,GIF,hdr,HDR,hdri,HDRI,iff,IFF,jpg,JPG,jpeg,JPEG,map,nt,miff,MIFF,mt,pic,PIC,png,PNG,ppm,pnm,pgm,pbm,psd,PSD,ras,RAS,rgb,RGB,rla,RLA,rpf,RPF,shmap,sgi,st,sun,SUN,sxr,SXR,tga,TGA,tif,tiff,TIF,TIFF,zt";
