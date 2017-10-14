516ae765e2eddb623487cdeb99cbfd68766f9fea
Gonzalo Garramuño <ggarra13@gmail.com>
2017-10-14 18:02:10 -0300

    Added missing mrvActionWindow.cpp/h.
    Former-commit-id: d316298dc417a9f80031585d442dfa9a11e577f8
    Former-commit-id: 2f3c9ff87ece88c33f3311fcde0c5c789519a78d

---
+++ mrViewer/src/gui/mrvActionWindow.cpp
@@ -0,0 +1,22 @@
+
+
+#include <fltk/events.h>
+
+#include "gui/mrvActionWindow.h"
+#include "gui/mrvImageView.h"
+#include "mrViewer.h"
+
+namespace mrv {
+
+ImageView* ActionWindow::view() const { return uiMain->uiView; }
+
+int ActionWindow::handle( int event )
+{
+    int ok = 0;
+    if ( event == fltk::KEY )
+        ok = view()->handle( event );
+    if ( !ok ) ok = fltk::Window::handle( event );
+    return ok;
+}
+
+}
