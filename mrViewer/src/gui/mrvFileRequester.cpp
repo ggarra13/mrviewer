2109bf1e0aed0142f06eabe607b9633272b32eb1
Gonzalo Garramuño <ggarra13@gmail.com>
2017-10-19 10:16:32 -0300

    Added support for RAW (CR2,DNG,etc) images through libraw library. Fixed shaders' scale and offset that were missing. Fixed documentation of OCIO's library entry.
    Former-commit-id: ffce32868fa6952d6fefa4c8ee72fe3b62e46a12 [formerly c5e6bd3b5fbf85fd5667c4f526d655effcb7e046] [formerly 1ef80c0533b30dae9ee13649c29a3228256076d0] [formerly ea468c223b047bc5ee7346b73c0ac6a61f432a3c] [formerly 9642951b60349b89f6033c3dc6e72cd99f9c934d [formerly 8c87f437e59888908d104f36a82180e34e96aa1a [formerly 728145c7c53376d28aad35c64a94dd3711ce2681] [formerly 9642951b60349b89f6033c3dc6e72cd99f9c934d [formerly 9642951b60349b89f6033c3dc6e72cd99f9c934d [formerly 9642951b60349b89f6033c3dc6e72cd99f9c934d [formerly 9642951b60349b89f6033c3dc6e72cd99f9c934d [formerly 3513434e8477d22bf9a080de36b956200f442abf]]]]]]] [formerly f7a1c98e48d8ca05e4a4503f5857cffbcd43b213 [formerly 80e9cc16c6c7945a58b39fdc3fa4450beab98d3c] [formerly 6f387e969a1e403fb1c82e5e42bc5bc6cfa46863] [formerly 4dd7095cce59df3622d7319f90e61f5324ff5f44] [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564] [formerly 733362092663f71e2eeaf37b8415b2faa1871244 [formerly e89a7e0f2632345dfa317e75fdbe59493c4ad10b] [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564 [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564 [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564 [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564 [formerly 95f07a241aa73b07819de1a6f19c6dff88a0ea20]]]]]] [formerly e6fbe2173a0c4a2b5ce7db324c69f2c26fd7e285 [formerly e6fbe2173a0c4a2b5ce7db324c69f2c26fd7e285 [formerly e6fbe2173a0c4a2b5ce7db324c69f2c26fd7e285 [formerly e6fbe2173a0c4a2b5ce7db324c69f2c26fd7e285 [formerly e6fbe2173a0c4a2b5ce7db324c69f2c26fd7e285 [formerly fb03c3e28e6a3869f2ec0a1f9f71d196df3ccfeb]]]]]]]
    Former-commit-id: f7a1c98e48d8ca05e4a4503f5857cffbcd43b213 [formerly 80e9cc16c6c7945a58b39fdc3fa4450beab98d3c] [formerly 6f387e969a1e403fb1c82e5e42bc5bc6cfa46863] [formerly 4dd7095cce59df3622d7319f90e61f5324ff5f44] [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564] [formerly 733362092663f71e2eeaf37b8415b2faa1871244 [formerly e89a7e0f2632345dfa317e75fdbe59493c4ad10b] [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564 [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564 [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564 [formerly 98a5ba533f0b67de5cad63bfff1422a9de0b1564 [formerly 95f07a241aa73b07819de1a6f19c6dff88a0ea20]]]]]]
    Former-commit-id: d534980d72c89ae7be976d28b8f7a8663f522588 [formerly ce72067bdf001b63266f508268f7c4b8bdde44c6 [formerly 70751ff064706a1e79f458c9e1539f417beb3bc7] [formerly d534980d72c89ae7be976d28b8f7a8663f522588 [formerly d534980d72c89ae7be976d28b8f7a8663f522588 [formerly d534980d72c89ae7be976d28b8f7a8663f522588 [formerly d534980d72c89ae7be976d28b8f7a8663f522588 [formerly 6b48ab8dcff608c77f07a06216a6387d13652d84]]]]]]
    Former-commit-id: 27c34bcf8295ea02ac9371fd404d622f7afd7a32
    Former-commit-id: 6f4d52f4ace7192dd165476bb47712e1706b56e4
    Former-commit-id: f71452ab97012850724d13a5844858323d8be129
    Former-commit-id: 6c791b9a0ee6a07c745c0ef2a5dc17ed5d0bcfd9
    Former-commit-id: 6a9008090436a98f1e3ab167759082a817a32be2 [formerly 1a76823eeadf6508c16f62a182110f868effd42b]
    Former-commit-id: 95d34da45f2b4e5e5420cd8827b424933ea3f7f7

--- mrViewer/src/gui/mrvFileRequester.cpp
+++ mrViewer/src/gui/mrvFileRequester.cpp
@@ -80,7 +80,7 @@ static const char* kModule = "file";
   static const std::string kMoviePattern = "3gp,3GP,asf,ASF,avc,AVC,avchd,AVCHD,avi,AVI,divx,DIVX,dv,DV,flv,FLV,m2ts,M2TS,m2t,M2T,mkv,MKV,m4v,M4V,mp4,MP4,mpg,MPG,mpeg,MPEG,mov,MOV,mxf,MXF,ogm,OGM,ogv,OGV,qt,QT,ts,TS,vob,VOB,vp9,VP9,webm,WEBM,wmv,WMV,y4m,Y4M";
 
   static const std::string kImagePattern =
-    "bmp,bit,cin,CIN,ct,dng,DNG,dpx,DPX,exr,EXR,gif,GIF,hdr,HDR,hdri,HDRI,iff,IFF,jpg,JPG,jpeg,JPEG,map,nt,miff,MIFF,mt,pic,PIC,png,PNG,ppm,pnm,pgm,pbm,psd,PSD,ras,RAS,rgb,RGB,rla,RLA,rpf,RPF,shmap,sgi,st,sun,SUN,sxr,SXR,tga,TGA,tif,tiff,TIF,TIFF,zt";
+    "bmp,bit,cin,CIN,cr2,CR2,ct,dng,DNG,dpx,DPX,exr,EXR,gif,GIF,hdr,HDR,hdri,HDRI,iff,IFF,jpg,JPG,jpeg,JPEG,map,nt,miff,MIFF,mt,pic,PIC,png,PNG,ppm,pnm,pgm,pbm,psd,PSD,ras,RAS,rgb,RGB,rla,RLA,rpf,RPF,shmap,sgi,st,sun,SUN,sxr,SXR,tga,TGA,tif,tiff,TIF,TIFF,zt";
 
   static const std::string kProfilePattern = "icc,icm,ICC,ICM";
 
