2fa027f0ea3743cd65e802460d1cf54569304e9c
Gonzalo Garramuño <ggarra13@gmail.com>
2017-10-24 10:44:31 -0300

    Added missing oiioImage files.
    Former-commit-id: 6c35067503f3f39a9856da5ae53555c3f9643db1 [formerly c2901d2572e69b9c14b720ab85e074d002e53d23]
    Former-commit-id: be35077a74f829a3ae99460db83231f77dcf1b71
    Former-commit-id: 1bd3edc6789532f75d2370f1b2786a1375c85710
    Former-commit-id: e7761d85f3e4317b62cd41af7fba7e7cc245fa45
    Former-commit-id: 387815007c97bdc174485c3ad7fb8ed2623fb88e
    Former-commit-id: d9ad05ed50c3b5454a9d6f23598081dbcbff40a4
    Former-commit-id: 5e21e5904a271280eb0fc2b4ff1e733a63b7f3b1
    Former-commit-id: e58596e01e451a440f956f945a063110045a62d9

---
+++ mrViewer/src/core/oiioImage.h
@@ -0,0 +1,78 @@
+/*
+    mrViewer - the professional movie and flipbook playback
+    Copyright (C) 2007-2014  Gonzalo Garramuño
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file   oiioImage.h
+ * @author gga
+ * @date   Fri Nov 03 15:38:30 2006
+ * 
+ * @brief  A simple wrapper class to read all of ImageMagick's image formats
+ * 
+ * 
+ */
+
+#ifndef oiioImage_h
+#define oiioImage_h
+
+#include <CMedia.h>
+
+#include <OpenImageIO/imageio.h>
+
+namespace mrv {
+
+  class oiioImage : public CMedia
+  {
+    oiioImage();
+
+    static CMedia* create() { return new oiioImage(); }
+
+
+  public:
+    static bool test(const char* file);
+    static CMedia* get(const char* name, const boost::uint8_t* datas = 0) {
+      return CMedia::get(create, name, datas);
+    }
+
+    virtual ~oiioImage();
+
+    virtual const char* const format() const { return _format; }
+
+    /// Returns the image compression (if any)
+    virtual const char* const compression() const { 
+       return ""; 
+    }
+
+    virtual bool initialize();
+    virtual bool release();
+
+      void level( int x ) { _level = x; refresh(); }
+      int level() const { return _level; }
+      
+      unsigned mipmap_levels() const { return _mipmaps; }
+
+    bool fetch( const boost::int64_t frame );
+
+  protected:
+      char* _format;
+      int _level;  // current mipmap level
+      unsigned _mipmaps;
+  };
+
+} // namespace mrv
+
+
+#endif // oiioImage_h
