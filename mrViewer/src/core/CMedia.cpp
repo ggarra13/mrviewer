7d6c6c04c09032813ee80db854bb90f6c05b8fdd
Gonzalo Garramuño <ggarra13@gmail.com>
2017-11-25 20:45:37 -0300

    Made ocio tooltips report useful information.  Updated docs with OCIO changes.
    Former-commit-id: e1d0412889e615bf22eb7bb69ac7592c580b38ed [formerly 6a8f143a569d0a53455d5237e8e06ac54d35854b]
    Former-commit-id: 2905164ce586860fbd70e28a5f7847225054c32b

--- mrViewer/src/core/CMedia.cpp
+++ mrViewer/src/core/CMedia.cpp
@@ -3627,8 +3627,6 @@ void CMedia::ocio_input_color_space( const std::string& n )
 {
     if ( _input_color_space == n || is_thumbnail() ) return;
 
-    mrvLOG_INFO( "ocio", name() << _(" : Setting OCIO ICS to ")
-                 << n << std::endl );
     _input_color_space = n;
     image_damage( image_damage() | kDamageData | kDamageContents | kDamageLut );
 }
