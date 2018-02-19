3a79ef17e4de661ec2e51dfbf1b700742e5e815b
Gonzalo Garramuño <ggarra13@gmail.com>
2018-02-19 08:28:56 -0300

    First commit.
    Former-commit-id: 6d0d23e4e47294b5fb013e08bff227376c195d38

---
+++ win64_deps/fltk-2.0-vc14-win64/src/AlignGroup.cxx
@@ -0,0 +1,108 @@
+// Align layout manager for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998-2006 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems on the following page:
+//
+//    http://www.fltk.org/str.php
+//
+
+// The AlignGroup overrides all group's children's label alignments to
+// it's own align() value, tiles and resize()s the children to fit in
+// the space not required by the (outsize) labels.
+
+#include <stdio.h>
+#include <string.h>
+#include <fltk/draw.h>
+#include <fltk/Flags.h>
+#include <fltk/AlignGroup.h>
+
+using namespace fltk;
+
+void AlignGroup::layout() {
+  Widget::layout();
+  int i;
+
+  int n_lines = n_to_break() ? 
+                (children() / n_to_break() + (children() % n_to_break()?1:0)):
+		1;
+  bool variable_is_y = (align() & (ALIGN_TOP|ALIGN_BOTTOM)) != 0;
+  int nx = vertical() ? n_lines : n_to_break() ? n_to_break() : children(); 
+  int ny = vertical() ? n_to_break() ? n_to_break() : children() : n_lines;
+  int n_variable = variable_is_y ? ny : nx;
+  uchar label_space[256];
+  memset(label_space,0,n_variable*sizeof(uchar));
+
+  int u=0, v=0;
+  int total_label_space=0;
+  int numchildren = children();
+  if (align()) {
+    for (i = 0; i < numchildren; i++) {
+      Widget* o = child(i);
+      setfont(o->labelfont(),o->labelsize());
+      setdrawflags(o->flags());
+      int w = this->w()-o->w(),h = this->h()-o->h();
+      measure(o->label(), w, h);
+      if (variable_is_y) w = h;
+      int which = (variable_is_y == vertical()) ? u : v; 
+      if (label_space[which] < w) label_space[which] = w;
+      if (++u == n_to_break()) {u = 0; v++;}
+    }
+    for (i = 0; i<n_variable; i++) total_label_space+=label_space[i];
+  }
+
+  int W = (w() - (variable_is_y ? 0:total_label_space) - (nx + 1)*dw()) / nx;
+  int H = (h() - (variable_is_y ? total_label_space:0) - (ny + 1)*dh()) / ny; 
+  int cx = dw(), cy = dh();
+  int icx = cx, icy = cy;
+
+  u = v = 0;
+  for (i = 0; i < numchildren; i++) {
+    Widget* o = child(i);
+    int which = (variable_is_y == vertical()) ? u : v; 
+    int X = cx, Y = cy;
+    if (align() & ALIGN_TOP) Y += label_space[which];
+    else if (!variable_is_y && align() & ALIGN_LEFT) X += label_space[which];
+
+    o->resize(X,Y,W,H);
+    o->align(align());
+    o->layout();
+
+    if (++u == n_to_break()) {
+      u = 0; v++;
+      if (vertical()) {
+	cx += W+dw(); cy = icy;
+        if (!variable_is_y) cx += label_space[which];
+      } else {
+	cy += H+dh(); cx = icx;
+        if (variable_is_y) cy += label_space[which];
+      }
+    } else {
+      if (vertical()) {
+	cy += H+dh();
+        if (variable_is_y) cy += label_space[which];
+      } else {
+	cx += W+dw();
+        if (!variable_is_y) cx += label_space[which];
+      }
+    }
+  }
+  init_sizes();
+}
+
+
---
+++ win64_deps/fltk-2.0-vc14-win64/themes/conf.c
@@ -0,0 +1,69 @@
+/*
+   "$Id: conf.c 8500 2011-03-03 09:20:46Z bgbnbigben $"
+
+    Configuration file routines for the Fast Light Tool Kit (FLTK).
+
+    Carl Thompson's config file routines version 0.5
+    Copyright 1995-2000 Carl Everard Thompson (clip@home.net)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+    USA.
+*/
+/*
+        file:           conf.c
+        author:         Carl Thompson (clip@home.net)
+        start date:     16 Dec 1995
+
+        Typical config file format:
+
+        ------- test.conf --------------------------------------------------------------------------------------------------------
+		
+		key1 = value1							# this key is not in a section
+		key2								# this key has no value associated
+		
+                [section1]
+		# Three standard key/value pairs
+            	    key1 = value1
+            	    key2 = value2
+            	    key3 = value3
+
+                    [section1/subsection]					# this is how a subsection is defined
+                	key1 = value1
+                	key2 = value2
+
+                [section2]
+        	    key1 = value1
+            	    key2 = value2
+
+        --------------------------------------------------------------------------------------------------------------------------
+
+
+        Notes:
+        - comments may be on a line by themselves or at the end of a line
+	  - for comments at the end of the line, the comment separator
+	    character must followed by whitespace
+        - indentation/formatting is handled automatically (sorry!)
+*/
+
+
+/* global variables */
+char        conf_sep = '=';                                                     /* this seperates keys from values */
+char        conf_level_sep = '/';                                               /* this denotes nested sections */
+char	    conf_comment_sep = '#';						/* this denotes comments */
+int         conf_comment_column = 51;						/* what column comments start in */
+
+/*
+    End of "$Id: conf.c 8500 2011-03-03 09:20:46Z bgbnbigben $".
+*/
