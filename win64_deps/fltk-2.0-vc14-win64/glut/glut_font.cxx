90175d3a74bd98dc474e2528b43d7aac6790103d
Gonzalo Garramuño <ggarra13@gmail.com>
2018-02-19 08:28:56 -0300

    First commit.
    Former-commit-id: 4939447aa4f8e6d466768cf359a3c76682069f7e [formerly 4939447aa4f8e6d466768cf359a3c76682069f7e [formerly 2f4e803e47ea1175fad6b9c4e474bdfa3d924bf3]]
    Former-commit-id: 3420ca1c910cef283ca90194b822712b58ff023e [formerly fd0d036ebd023820767a1ab40bcaeab06eaa8d2e]
    Former-commit-id: 6fee06a2b4fe273325a6443e32f69b74e6e7e603

---
+++ win64_deps/fltk-2.0-vc14-win64/glut/glut_font.cxx
@@ -0,0 +1,67 @@
+//
+// "$Id: glut_font.cxx 8500 2011-03-03 09:20:46Z bgbnbigben $"
+//
+// GLUT bitmap font routines for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998-2006 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems on the following page:
+//
+//    http://www.fltk.org/str.php
+//
+
+// (sort of) emulation of Glut's bitmap drawing functions, using FL's
+// font stuff.  Not all the fonts match!
+
+#include <config.h>
+#if HAVE_GL
+
+#include <fltk/glut.h>
+#include <fltk/gl.h>
+using namespace fltk;
+
+Glut_Bitmap_Font glutBitmap9By15 = {SCREEN_FONT, 15};
+Glut_Bitmap_Font glutBitmap8By13 = {SCREEN_FONT, 13};
+Glut_Bitmap_Font glutBitmapTimesRoman10 = {TIMES, 10};
+Glut_Bitmap_Font glutBitmapTimesRoman24 = {TIMES, 24};
+Glut_Bitmap_Font glutBitmapHelvetica10 = {HELVETICA, 10};
+Glut_Bitmap_Font glutBitmapHelvetica12 = {HELVETICA, 12};
+Glut_Bitmap_Font glutBitmapHelvetica18 = {HELVETICA, 18};
+
+void glutBitmapCharacter(void* font, int character) {
+  glsetfont(((Glut_Bitmap_Font *)font)->font,((Glut_Bitmap_Font *)font)->size);
+  char a[1]; a[0] = character;
+  gldrawtext(a,1);
+}
+
+int glutBitmapWidth(void* font, int character) {
+  glsetfont(((Glut_Bitmap_Font *)font)->font,((Glut_Bitmap_Font *)font)->size);
+  char a[1]; a[0] = character;
+  return int(glgetwidth(a,1)+.5);
+}
+
+int glutBitmapLength(void* font, const unsigned char* string) {
+  glsetfont(((Glut_Bitmap_Font *)font)->font,((Glut_Bitmap_Font *)font)->size);
+  return int(glgetwidth((const char*)string)+.5);
+}
+
+#endif
+
+//
+// End of "$Id: glut_font.cxx 8500 2011-03-03 09:20:46Z bgbnbigben $".
+//
