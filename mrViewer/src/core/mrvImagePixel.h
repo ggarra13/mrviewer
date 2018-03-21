66b24c13374f228d5b9f201a96f393b445774237
Gonzalo Garramuño <ggarra13@gmail.com>
2018-03-21 12:58:58 -0300

    Bug fixed baking of ocio and gamma change.  Made chocolatey install work for all users.
    Former-commit-id: d708fc24bf0d306ad0933b245f715d16f31688c3 [formerly c738027755749ee88bf25c5bcd67d23c42f85b24]
    Former-commit-id: f74eb7658b48e3591bb87de1a853600bfda1695d

--- mrViewer/src/core/mrvImagePixel.h
+++ mrViewer/src/core/mrvImagePixel.h
@@ -50,7 +50,17 @@ namespace mrv {
        {
        };
 
-       void clamp()
+       inline void clamp()
+       {
+	  if ( r < 0.0f ) r = 0.0f;
+	  else if ( r > 1.0f ) r = 1.0f;
+	  if ( g < 0.0f ) g = 0.0f;
+	  else if ( g > 1.0f ) g = 1.0f;
+	  if ( b < 0.0f ) b = 0.0f;
+	  else if ( b > 1.0f ) b = 1.0f;
+       }
+      
+       inline void clampRGBA()
        {
 	  if ( r < 0.0f ) r = 0.0f;
 	  else if ( r > 1.0f ) r = 1.0f;
