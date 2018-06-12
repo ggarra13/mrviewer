dc1c91b89b38771ca420b1e91690f79a11090e3b
Gonzalo Garramuño <ggarra13@gmail.com>
2018-06-12 17:25:07 -0300

    Removed conflicts after merge:
    	mrViewer/src/core/CMedia.cpp
    	mrViewer/src/core/aviImage.cpp
    	mrViewer/src/gui/mrvPreferences.cpp
    
    
    Former-commit-id: 540ab186e510544a72d73db647b52c4a5006172c [formerly 540ab186e510544a72d73db647b52c4a5006172c [formerly f439669c617820db89c747f8e9cc5e4a683059c2]] [formerly 37eb4d35aa2241f542b230a65127aa2e188091e7 [formerly 37eb4d35aa2241f542b230a65127aa2e188091e7 [formerly 9d96cd2d681b8dbe5da129d86c3768f94e3ac262]]]
    Former-commit-id: 52a39b203f50d6bad9d847e4a919973719583a74 [formerly 52a39b203f50d6bad9d847e4a919973719583a74 [formerly c26a0cff6234409c511f6acd7d8e5fc5412eedc7]]
    Former-commit-id: 16a355196934d617bc2452580944ee3212f498c2 [formerly c676e6a9bc1c487db62cfcd1018df7a95331065c]
    Former-commit-id: 4b2e1208712fcd22ae2febf8cdfd4e0be8cfa4e5

--- mrViewer/src/gui/mrvPreferences.cpp
+++ mrViewer/src/gui/mrvPreferences.cpp
@@ -301,7 +301,6 @@ fltk::StyleSet*     newscheme = NULL;
   int   Preferences::selectiontextcolor;
 
 
-
   Preferences::Preferences( mrv::PreferencesUI* uiPrefs )
   {
     bool ok;
@@ -561,8 +560,6 @@ fltk::StyleSet*     newscheme = NULL;
     playback.get( "fps", tmpF, 24.0 );
     uiPrefs->uiPrefsFPS->value(tmpF);
     CMedia::default_fps = tmpF;
-
-
 
     playback.get( "loop_mode", tmp, 1 );
     uiPrefs->uiPrefsLoopMode->value(tmp);
@@ -849,10 +846,6 @@ fltk::StyleSet*     newscheme = NULL;
 
 
     fltk::Preferences loading( base, "loading" );
-    loading.get( "oiio_readers", tmp, 0 );
-    uiPrefs->uiPrefsOIIOReaders->value(tmp);
-    CMedia::oiio_readers = (bool)tmp;
-
     loading.get( "drag_load_seq", tmp, 1 );
     uiPrefs->uiPrefsLoadSequence->value( (bool) tmp );
 
@@ -1126,28 +1119,28 @@ static const char* kCLocale = "C";
     const char* var = environmentSetting( "OCIO",
                                           uiPrefs->uiPrefsOCIOConfig->text(),
                                           true);
-
-    static std::string old_ocio;
-    std::string tmp = root + "/ocio/nuke-default/config.ocio";
-
-
-    if (  ( !var || strlen(var) == 0 || tmp == var ) && use_ocio )
+    if (  ( !var || strlen(var) == 0 ) && use_ocio )
     {
         mrvLOG_INFO( "ocio",
-                     _("Setting OCIO environment variable to nuke-default." )
+                     _("Setting OCIO environment variable to nuke-default" )
                      << std::endl );
-        old_ocio = tmp;
-        var = old_ocio.c_str();
+        std::string tmp = root + "/ocio/nuke-default/config.ocio";
+        DBG( __FUNCTION__ << " " << __LINE__ );
+        var = strdup( tmp.c_str() );
+        DBG( __FUNCTION__ << " " << __LINE__ );
     }
     if ( var && use_ocio && strlen(var) > 0 )
     {
+        static std::string old_ocio;
 
+        DBG( __FUNCTION__ << " " << __LINE__ );
         if ( old_ocio != var )
         {
-            old_ocio = var;
+            DBG( __FUNCTION__ << " " << __LINE__ );
             mrvLOG_INFO( "ocio", _("Setting OCIO environment variable to:")
                          << std::endl );
             mrvLOG_INFO( "ocio", var << std::endl );
+            old_ocio = var;
         }
 
         DBG( __FUNCTION__ << " " << __LINE__ );
@@ -1157,7 +1150,6 @@ static const char* kCLocale = "C";
         putenv( strdup(buf) );
 
         uiPrefs->uiPrefsOCIOConfig->text( var );
-
 
 #ifdef __linux__
         char tmpS[256];
@@ -1333,8 +1325,6 @@ static const char* kCLocale = "C";
 
     int scale = CMedia::cache_scale();
     CMedia::cache_scale( uiPrefs->uiPrefsCacheScale->value() );
-
-    CMedia::oiio_readers = (bool) uiPrefs->uiPrefsOIIOReaders->value();
 
     DBG( __FUNCTION__ << " " << __LINE__ );
     if ( uiPrefs->uiPrefsCacheFPS->value() == 0 )
@@ -1784,7 +1774,6 @@ static const char* kCLocale = "C";
     caches.set( "image_size", uiPrefs->uiPrefsImageCacheSize->value() );
 
     fltk::Preferences loading( base, "loading" );
-    loading.set( "oiio_readers", (int) uiPrefs->uiPrefsOIIOReaders->value() );
     loading.set( "drag_load_seq", (int) uiPrefs->uiPrefsLoadSequence->value() );
     loading.set( "file_assoc_load_seq",
                  (int) uiPrefs->uiPrefsLoadSequenceOnAssoc->value() );
