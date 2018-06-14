03e472122f0a1b34856cce7aed7ff25a59400318
Gonzalo Garramuño <ggarra13@gmail.com>
2018-06-13 23:01:52 -0300

    Some cleanup and error catching.
    Former-commit-id: 4e2d7fde06c5256b308fea6ed05c293df309f7fc
    Former-commit-id: 216dae0762b5099e9c6d9ed68152e710d48d98ef

--- mrViewer/src/gui/mrvMediaTrack.cpp
+++ mrViewer/src/gui/mrvMediaTrack.cpp
@@ -215,6 +215,7 @@ bool media_track::remove( const int idx )
 bool media_track::remove( const mrv::media m )
 {  
    if ( _reel_idx < 0 ) return false;
+   std::cerr << "_reel_idx " << _reel_idx << std::endl;
    browser()->reel( _reel_idx );
    browser()->remove( m );
    browser()->parent()->redraw();
