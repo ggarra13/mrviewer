19f515fc490fcadf671875b4c6766a9f728e3439
Gonzalo Garramuño <ggarra13@gmail.com>
2018-09-07 12:51:21 -0300

    Fix of mrViewer's git repository crash.
    Former-commit-id: b571fa614d98dc72479328441f7cd8a111eb4876 [formerly f128ab6452cf82f984699e95225e2073be865e84]
    Former-commit-id: fc6d6f0587343a337b1647387d02215f932db1b2

--- mrViewer/src/core/mrvPacketQueue.cpp
+++ mrViewer/src/core/mrvPacketQueue.cpp
@@ -35,6 +35,7 @@ bool      PacketQueue::inited = false;
 AVPacket  PacketQueue::_flush;
 AVPacket  PacketQueue::_seek;
 AVPacket  PacketQueue::_seek_end;
+AVPacket  PacketQueue::_jump;
 AVPacket  PacketQueue::_preroll;
 AVPacket  PacketQueue::_loop_start;
 AVPacket  PacketQueue::_loop_end;
