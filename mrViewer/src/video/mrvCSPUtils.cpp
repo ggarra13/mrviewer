103c03189195b3d4cfbbfec5bac5ba45d571b304
Gonzalo Garramuño <ggarra13@gmail.com>
2018-09-23 18:56:51 -0300

    Added beginning comment to files.
    Former-commit-id: d9eecff87c3c2f7198cb2697b19c86bbf8cb3d15 [formerly 00c5bb1cd10537897440522f7054feebdce7caee]
    Former-commit-id: ce339c886c2cb129ba2ee48ab859a58770ff8524 [formerly 16135c8917636cedca221fa503b44968bcd7d1ad]
    Former-commit-id: c18a6b8ad8a339cf1c0accef47ab225d038a969b

--- mrViewer/src/video/mrvCSPUtils.cpp
+++ mrViewer/src/video/mrvCSPUtils.cpp
@@ -1,3 +1,29 @@
+/*
+    mrViewer - the professional movie and flipbook playback
+    Copyright (C) 2007-2014  Gonzalo Garramuño
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file   mrvCSPUtils.cpp
+ * @author gga
+ * @date   Fri Feb  8 10:14:11 2008
+ *
+ * @brief  Color space utils as in mpv
+ *         This file is mainly a copy of mpv's csputils.c file.
+ *
+ */
 
 extern "C" {
 
