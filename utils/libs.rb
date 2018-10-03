2ed666cca2ef119106d3e0eb422ecc1222da1d89
Gonzalo Garramuño <ggarra13@gmail.com>
2018-10-03 20:12:56 -0300

    Fixed util/libs.rb to not overwrite bin/mrViewer incorrectly.
    Former-commit-id: 653f79ccb4dca3203c18465f67cd55c228a4388e

--- utils/libs.rb
+++ utils/libs.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'optparse'
 
 EXCLUDE = %w(
 libGL\.so
@@ -17,7 +18,22 @@ libdl.*
 libxcb.*
 libasound.*
 libfontconfig.*
+libACESclip.*
 )
+
+@options = { :verbose => true }
+OptionParser.new do |opts|
+  opts.banner = "Usage: utils/libs.rb [@options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    @options[:verbose] = v
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end.parse!
 
 EXCLUDE_REGEX = /(?:#{EXCLUDE.join('|')}).*/
 
@@ -28,29 +44,32 @@ def parse( files )
 
     next if not loc or not lib
 
-    loc.sub!(/\s*\(.*\)$/, '')
+    loc.sub!(/\s*$/, '')
     lib.sub!(/^\s*/, '')
 
     if loc.empty?
-      puts "#{lib} empty loc"
       next
     end
 
     if lib =~ EXCLUDE_REGEX
-      puts "Exclude #{lib}"
+      puts "Exclude #{lib}" if @options[:verbose]
       next
     end
+
 
     begin
       FileUtils.rm("#{@debug}/lib/#{lib}")
     rescue
     end
-
-    puts "loc: #{loc} -> lib: #{lib}"
+    if @options[:verbose]
+      puts "loc: #{loc} -> lib: #{lib}"
+    else
+      print "."
+    end
     orig = lib
     if File.symlink?( loc )
       lib = File.readlink( loc )
-      puts "#{lib} ->->-> #{orig}"
+      puts "#{lib} ->->-> #{orig}" if @options[:verbose]
       FileUtils.cp(loc, "#{@debug}/lib/#{lib}" )
       FileUtils.ln_s( "#{lib}", "#{@debug}/lib/#{orig}" )
     else
@@ -68,8 +87,6 @@ release = `uname -r`.chop!
 
 build = "BUILD/Linux-#{release}-64/"
 
-Dir.chdir( build )
-
 home=ENV['HOME']+"/bin/mrViewer"
 FileUtils.rm_f( home )
 FileUtils.ln_s( ENV['PWD']+'/'+build+"/Release/bin/mrViewer.sh", ENV['HOME']+"/bin/mrViewer" )
@@ -85,10 +102,8 @@ files = []
 
 for exe in exes
   output=`ldd #{exe}`
-  puts "\t#{exe}"
-  puts output
+  output.gsub!( /\(0x.*\)/, '' )
   files += output.split("\n")
-
 end
 
 files.sort!
@@ -99,8 +114,8 @@ parse( files )
 FileUtils.rm_f( "#{@debug}/shaders" )
 FileUtils.rm_f( "#{@debug}/docs" )
 FileUtils.rm_f( "#{@debug}/ctl" )
-FileUtils.cp_r( "../../shaders", "#{@debug}/" )
-FileUtils.cp_r( "../../docs", "#{@debug}/" )
-FileUtils.cp_r( "../../ctl", "#{@debug}/" )
+FileUtils.cp_r( "shaders", "#{@debug}/" )
+FileUtils.cp_r( "docs", "#{@debug}/" )
+FileUtils.cp_r( "ctl", "#{@debug}/" )
 
-`find . -name '*fuse*' -exec rm {} \\;`
+#`find . -name '*fuse*' -exec rm {} \\;`
