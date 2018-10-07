86416bb3d7e81ec5fc8214edb75966521cc9c4f9
Gonzalo Garramuno <ggarra13@gmail.com>
2018-10-07 17:01:31 -0300

    Updated files to openexr2.3.0
    Former-commit-id: 3ae555c2092800a689ce7c5e563975d5d4c33e1d [formerly 4f2987cedc487d0effbaab7bdcd147787d3ec58e]
    Former-commit-id: 84b8e59aeb2cb143409f00e23ffeeeb3b7f1d5b1

--- modules/FindOpenEXR.cmake
+++ modules/FindOpenEXR.cmake
@@ -78,7 +78,7 @@ FIND_LIBRARY( IexMath
 )
 
 FIND_LIBRARY( Half
-  NAMES Half-2_4_dll Half-2_3_dll Half-2_3 Half_dll Half_dll_d Half Halfd
+  NAMES Half-2_3_dll Half-2_3 Half_dll Half_dll_d Half Halfd
   PATHS ${SEARCH_DIRS}
   NO_DEFAULT_PATH
   DOC   "OpenEXR Half library"
