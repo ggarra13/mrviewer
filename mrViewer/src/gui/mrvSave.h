fde9996ffb79780eda1baec854b7fef3ff1add2a
Gonzalo Garramuño <ggarra13@gmail.com>
2018-11-07 13:35:05 -0300

    Compilation fixes.
    Former-commit-id: f6b0c4668aedf9ce71f17c297859b30e68533698 [formerly f6b0c4668aedf9ce71f17c297859b30e68533698 [formerly f6b0c4668aedf9ce71f17c297859b30e68533698 [formerly f6b0c4668aedf9ce71f17c297859b30e68533698 [formerly f6b0c4668aedf9ce71f17c297859b30e68533698 [formerly f6b0c4668aedf9ce71f17c297859b30e68533698 [formerly f3e30dd61b6d9fe199142572f003008548cbe91f]]]]]]
    Former-commit-id: cd044c69d57e515fa3c63df84cc3d30eeee3f949 [formerly 62108ce0cfddd7de7183adc147657f6772c78505] [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly 04c8c3a9e5be70ba654b705cb8c5d65388dfd6af]]]] [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly 04c8c3a9e5be70ba654b705cb8c5d65388dfd6af]]] [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly 04c8c3a9e5be70ba654b705cb8c5d65388dfd6af]] [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly 04c8c3a9e5be70ba654b705cb8c5d65388dfd6af] [formerly 04c8c3a9e5be70ba654b705cb8c5d65388dfd6af [formerly 92ec5115b0ef6f658cb95b4d240896b3c3bf5774]]]]]]
    Former-commit-id: f394c731c449127fb1afd06f1f64cff1a09be550 [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly f394c731c449127fb1afd06f1f64cff1a09be550 [formerly 04c8c3a9e5be70ba654b705cb8c5d65388dfd6af]]]]
    Former-commit-id: 6d2e4ce0fe79ddbdb7a2c1bf896aa6fae22f4ee5
    Former-commit-id: a38abd60c2c82337a13daeee8541618e9fd2790b
    Former-commit-id: 485add0df2544f39a4d675d61c5a21a1c3e4e294
    Former-commit-id: 351563ebdc850861e3d77f18fc2c8c70990dfa7c
    Former-commit-id: 67a71a955064fa342454c78cf3cb82509214ed38

--- mrViewer/src/gui/mrvSave.h
+++ mrViewer/src/gui/mrvSave.h
@@ -9,6 +9,8 @@ class ViewerUI;
 void save_movie_or_sequence( const char* file, const mrv::ViewerUI* uiMain,
 			     const bool opengl );
 
+bool save_xml( const CMedia* img, ImageOpts* iopts, const char* file );
+
 } // namespace mrv
 
 #endif
