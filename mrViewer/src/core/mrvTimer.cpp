2465bd3d3183325f91a252cc472c65acc17c19ad
Gonzalo Garramuño <ggarra13@gmail.com>
2018-11-14 15:19:36 -0300

    Fixed playback for BG images.  Made FG startup be slow when comping with BG images, but super fast when no comp is needed.
    Former-commit-id: aaa71a7a44badc1456a040ce80f1154aea8daba9 [formerly f6a2cb7c1f84abfb9be6d65d6f46c786218d0034]
    Former-commit-id: edc3cc2094e6c5a41096a9b4a1a79ede964d72c9

--- mrViewer/src/core/mrvTimer.cpp
+++ mrViewer/src/core/mrvTimer.cpp
@@ -64,16 +64,22 @@
   int
   gettimeofday (struct timeval *tv, void *tz)
   {
-    union
-    {
-      ULONGLONG ns100;  // time since 1 Jan 1601 in 100ns units
-      FILETIME ft;
-	} now;
-    
-    GetSystemTimeAsFileTime (&now.ft);
-    tv->tv_usec = long ((now.ns100 / 10LL) % 1000000LL);
-    tv->tv_sec = long ((now.ns100 - 116444736000000000LL) / 10000000LL);
-    
+    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
+    // until 00:00:00 January 1, 1970 
+    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+    SYSTEMTIME  system_time;
+    FILETIME    file_time;
+    uint64_t    time;
+
+    GetSystemTime( &system_time );
+    SystemTimeToFileTime( &system_time, &file_time );
+    time =  ((uint64_t)file_time.dwLowDateTime )      ;
+    time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+    tv->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+    tv->tv_usec = (long) (system_time.wMilliseconds * 1000);
     return 0;
   } 
 
