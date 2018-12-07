6d0e54b818f70ae7755bc3a2c608c469f4075b74
Gonzalo Garramuno <ggarra13@gmail.com>
2018-12-07 14:41:41 -0300

    Bug fixed cache starting at frame other than 1.
    Former-commit-id: 88816fd65742abebb56c0d63f4a6c58bc1c87445 [formerly 88816fd65742abebb56c0d63f4a6c58bc1c87445 [formerly 78e6b6c2bd7bf7b96c60079aafc6330a70733bd5]] [formerly cd3ba7b025636a8d3e6512f96c9e7adbfe0e4524 [formerly cd3ba7b025636a8d3e6512f96c9e7adbfe0e4524 [formerly a9cc0383beb0ad6053ecce27ca3d6287d4530aa1]]]
    Former-commit-id: 7440bae8d52c454ef71f386ccdf947e7ddd8f1a4 [formerly 5b5799d21b5a7cab65c9b46c0c9f2958d927e3e3] [formerly 7440bae8d52c454ef71f386ccdf947e7ddd8f1a4 [formerly 5b5799d21b5a7cab65c9b46c0c9f2958d927e3e3] [formerly 45b16d8ee049614fb43634ba35a2b5d4099670c0 [formerly ff1fa4b7c75911c819dadf61efb4e7628105087c]]]
    Former-commit-id: 9e196ad3f249bcc9faeb293c91fe1668cc53fb32 [formerly 94770b51b0bb5e91cc282991d6ed26748eb05735]
    Former-commit-id: 9d777459a2f95f0cd9b613e69ba702cbfe740a8a [formerly 11362d1aa97a51faacea12e50abdc48b99109d89]
    Former-commit-id: 881b8b66b5fcd238eaf796d0f83e15840a32e919

--- mrViewer/src/gui/mrvImageView.h
+++ mrViewer/src/gui/mrvImageView.h
@@ -544,7 +544,7 @@ class server;
        bool ghost_next()     const { return _ghost_next; }
 
       // Make pre-loading start from scratch
-      inline void reset_caches() { _reel = 0; _preframe = 1; }
+      void reset_caches();
 
       /// Clear image sequence caches from reel idx
       void clear_reel_cache( size_t idx );
