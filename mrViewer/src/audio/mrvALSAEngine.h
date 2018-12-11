3587aad00b15bcb082966674c2ef505b1813c251
Gonzalo Garramuño <ggarra13@gmail.com>
2018-12-11 20:35:58 -0300

    Saving files before refreshing line endings
    Former-commit-id: 8dac12562fafd6256d782f36b2ad4a33c1e533f2 [formerly 73d5719565db40e3abfb56c5fea2732fa09c6177] [formerly 4e5ae0478fde4c392294bceba3b5c31fc0708ffb] [formerly 1a14756edf017037a33568f508f109c301bebd9b] [formerly 1082192a13700cf9ab1c390b9b8429a67e9c63e2 [formerly 33256abd3ed13b308e4b0782713d594f8e2d2c75 [formerly 6efd142ca5ee4da8efd3f5ccae1a578a284c7236] [formerly 1082192a13700cf9ab1c390b9b8429a67e9c63e2 [formerly 1082192a13700cf9ab1c390b9b8429a67e9c63e2 [formerly 1082192a13700cf9ab1c390b9b8429a67e9c63e2 [formerly b0e7ff8ef4de6b7f788a22a983bc04d9c9caed29]]]]]] [formerly eb74c503257be95ae024893d46455a2cb1503012 [formerly 86b2fec9bc5fcc52d12bebcb0ed14e9fe2b14dc2] [formerly 6fc015fb48d4e30dfd877c6e6dabfda5f9f725db] [formerly cc74120b38fbb42c81febfb0b3c4e36d2241e0cf] [formerly de6c11409f6d452b88a56ffc2a290b872036c235 [formerly 6a39636919ae21532b824c37fc3587e33ea6d25d] [formerly cc74120b38fbb42c81febfb0b3c4e36d2241e0cf [formerly cc74120b38fbb42c81febfb0b3c4e36d2241e0cf [formerly cc74120b38fbb42c81febfb0b3c4e36d2241e0cf [formerly 13034430e6bc8ac61f1898e8a6e50f6dbfb5e510]]]]] [formerly 488c853525fc280f4fd37fc1211f283562bb817b [formerly 488c853525fc280f4fd37fc1211f283562bb817b [formerly 488c853525fc280f4fd37fc1211f283562bb817b [formerly 488c853525fc280f4fd37fc1211f283562bb817b [formerly 40b2eb3bad0d45d63c8b491d964789a082c638d5]]]]]]
    Former-commit-id: eb74c503257be95ae024893d46455a2cb1503012 [formerly 86b2fec9bc5fcc52d12bebcb0ed14e9fe2b14dc2] [formerly 6fc015fb48d4e30dfd877c6e6dabfda5f9f725db] [formerly cc74120b38fbb42c81febfb0b3c4e36d2241e0cf] [formerly de6c11409f6d452b88a56ffc2a290b872036c235 [formerly 6a39636919ae21532b824c37fc3587e33ea6d25d] [formerly cc74120b38fbb42c81febfb0b3c4e36d2241e0cf [formerly cc74120b38fbb42c81febfb0b3c4e36d2241e0cf [formerly cc74120b38fbb42c81febfb0b3c4e36d2241e0cf [formerly 13034430e6bc8ac61f1898e8a6e50f6dbfb5e510]]]]]
    Former-commit-id: 8ad8b605895915c87442c35d8ba5b52480034fbb [formerly f1bd22d1bde87e9f14227c51b9bb869b3a2c61a2] [formerly 1ebe861d33501cda5b78d78c4082311cc9840db3 [formerly 7b45e515bc9c2e4aa34e0deba9dee81380476284] [formerly 8ad8b605895915c87442c35d8ba5b52480034fbb [formerly f1bd22d1bde87e9f14227c51b9bb869b3a2c61a2] [formerly f1bd22d1bde87e9f14227c51b9bb869b3a2c61a2 [formerly f1bd22d1bde87e9f14227c51b9bb869b3a2c61a2 [formerly 36fe63fc62e1988b294a21c675f40c4915e52c27]]]]]
    Former-commit-id: 0faba595cb60b00dea68ce1c96e9eae26013e944
    Former-commit-id: f1e89f78c086e263a3246c81ac9bba2430134534
    Former-commit-id: d0737ba362572d2541e0deb8561587a92ce00157
    Former-commit-id: d677324e2ba1b13cc5cc9a04b25caee67f71656e
    Former-commit-id: ac54af7024540553687c056840313c7edfd58d2e

--- mrViewer/src/audio/mrvALSAEngine.h
+++ mrViewer/src/audio/mrvALSAEngine.h
@@ -84,9 +84,9 @@ protected:
   snd_pcm_t*   _pcm_handle;
 
 protected:
-  static unsigned int _instances;
-  static snd_mixer_t* _mixer;
-  snd_pcm_format_t    _pcm_format;
+    static unsigned int _instances;
+    static snd_mixer_t* _mixer;
+    snd_pcm_format_t    _pcm_format;
     snd_pcm_hw_params_t* hwparams;
     snd_pcm_sw_params_t* swparams;
 };
