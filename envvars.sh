b531c12e9c196a2fd559aab68560838c4eae0dcb
Gonzalo Garramuño <ggarra13@gmail.com>
2019-01-17 08:23:48 -0300

    Commited envvars.sh/envvars_sanitize.sh to set environment to release or debug builds on linux (sanitize).
    Former-commit-id: ba9ea7bcf95047a8d79c95e4863c10a54ad360d5

---
+++ envvars.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export FFMPEG_ROOT=/usr/local/
+export CTL_ROOT=/usr/local/
+export OPENEXR_ROOT=/usr/local/
---
+++ envvars_sanitize.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export FFMPEG_ROOT=/usr/local/sanitize/
+export CTL_ROOT=/usr/local/sanitize/
+export OPENEXR_ROOT=/usr/local/sanitize/
