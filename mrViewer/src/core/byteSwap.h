90501567a505c1a4aa88f4759d8e7d6b52ec6c24
Gonzalo Garramuno <ggarra13@gmail.com>
2019-03-01 16:19:54 -0300

    fltk1.4 changes.  Compiles and links, but crashes on running.
    Former-commit-id: 7ea26399283127972f83543e69f9e767b77e1281 [formerly 945935a375cfcfd45c8696b8311f3948636281a2 [formerly 2c542b41fddfec14947354d61484f1d66ca34809]]
    Former-commit-id: 5df497bcf7ba337617a1734ecfd498da24e2c472 [formerly 17f91917d15866af2c0da1b1c1021777db7d7613] [formerly cef623b681f781dddf0840ada068383250335f4f] [formerly 611ae68cca8b72e847f418009d13ca75d62f0ea4 [formerly 7d5c5ee041f76520547d01fae4b55fea3868a776 [formerly 6b7dcbe56dfc7423689eb239961ce25fd510cfd2]] [formerly 4a0342fe531012e07d9fb4c7d8d14b7f1b8b9d14 [formerly 871ea2fbf96792882eef2d4870e3c71186fd956e [formerly 2d97ae1e197dc11db831a03aeca67d1bf39e0506]]]]
    Former-commit-id: aab4770413a81185116742a4e3fe01c14c52c66f [formerly a4c0e63ac4cbfd80d9cc546f21f6d2178e4da4c0] [formerly 82264879332e9885597a67f8db93c5a4fc6a88ba]
    Former-commit-id: 1bd7a9db2989dc213d3f84e371ca88e1d42e8684
    Former-commit-id: 8390f9c19ee6ec35bdab2732fee4f6722f2feaf2
    Former-commit-id: a371af0aaff4e893d6f01d4faeb72cc2f2d1182f

--- mrViewer/src/core/byteSwap.h
+++ mrViewer/src/core/byteSwap.h
@@ -1,6 +1,6 @@
 /*
     mrViewer - the professional movie and flipbook playback
-    Copyright (C) 2007-2014  Gonzalo Garramuño
+    Copyright (C) 2007-2014  Gonzalo GarramuÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ±o
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -22,18 +22,18 @@
 //! Swap a float between network and host byte-order
 inline float ByteSwap( float f )
 {
-    union
-    {
-        float f;
-        unsigned char b[4];
-    } dat1, dat2;
+  union
+  {
+    float f;
+    unsigned char b[4];
+  } dat1, dat2;
 
-    dat1.f = f;
-    dat2.b[0] = dat1.b[3];
-    dat2.b[1] = dat1.b[2];
-    dat2.b[2] = dat1.b[1];
-    dat2.b[3] = dat1.b[0];
-    return dat2.f;
+  dat1.f = f;
+  dat2.b[0] = dat1.b[3];
+  dat2.b[1] = dat1.b[2];
+  dat2.b[2] = dat1.b[1];
+  dat2.b[3] = dat1.b[0];
+  return dat2.f;
 }
 
 #ifdef MRV_BIG_ENDIAN
