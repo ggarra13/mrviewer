7957c4c9f7cbe51c95b220353e91d4227aa6d5f9
Gonzalo Garramuno <ggarra13@gmail.com>
2019-03-01 16:19:54 -0300

    fltk1.4 changes.  Compiles and links, but crashes on running.
    Former-commit-id: 7ea26399283127972f83543e69f9e767b77e1281 [formerly 2c542b41fddfec14947354d61484f1d66ca34809]
    Former-commit-id: 440def8e568afbb15d0dd06dd10cbd4bc0d7e9b7 [formerly 611ae68cca8b72e847f418009d13ca75d62f0ea4] [formerly 7d5c5ee041f76520547d01fae4b55fea3868a776 [formerly 6b7dcbe56dfc7423689eb239961ce25fd510cfd2]] [formerly 4a0342fe531012e07d9fb4c7d8d14b7f1b8b9d14 [formerly 7d5c5ee041f76520547d01fae4b55fea3868a776 [formerly 6b7dcbe56dfc7423689eb239961ce25fd510cfd2] [formerly 871ea2fbf96792882eef2d4870e3c71186fd956e [formerly 2d97ae1e197dc11db831a03aeca67d1bf39e0506]]]]
    Former-commit-id: a4c0e63ac4cbfd80d9cc546f21f6d2178e4da4c0 [formerly 82264879332e9885597a67f8db93c5a4fc6a88ba]
    Former-commit-id: e088d76041c6eda9651612730fb50ffca4dd6e37 [formerly a4c0e63ac4cbfd80d9cc546f21f6d2178e4da4c0]
    Former-commit-id: aab4770413a81185116742a4e3fe01c14c52c66f
    Former-commit-id: c6e0b4838740e8a697c499c180360caa90f25ef0

--- mrViewer/src/core/mrvLicensing.cpp
+++ mrViewer/src/core/mrvLicensing.cpp
@@ -1,6 +1,6 @@
 /*
     mrViewer - the professional movie and flipbook playback
-    Copyright (C) 2007-2014  Gonzalo Garramuño
+    Copyright (C) 2007-2014  Gonzalo GarramuÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ±o
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,9 +19,9 @@
  * @file   mrvProtection.cpp
  * @author gga
  * @date   Mon Oct 30 04:55:39 2006
- *
+ * 
  * @brief  Functions used to read and check license file
- *
+ * 
  */
 
 #include <iostream>
@@ -38,50 +38,50 @@ using namespace std;
 
 namespace mrv
 {
-// namespace {
+  // namespace {
 
-//   //
-//   // 128 SHA-1 encode of 'gga_mrViewer_key'
-//   //
-//   // Key to PC1 algorithm
-//   //
-//   const char* kPC1_key = "fd0629758a5ead396c130a550185b865edc0163c";
+  //   //
+  //   // 128 SHA-1 encode of 'gga_mrViewer_key'
+  //   //
+  //   // Key to PC1 algorithm
+  //   //
+  //   const char* kPC1_key = "fd0629758a5ead396c130a550185b865edc0163c";
+  
+  //   // "2764@127.0.0.1"
+  //   const char* kDefaultServer = "hkldnbkjfnbjnidnpbmpmfheadpm";
 
-//   // "2764@127.0.0.1"
-//   const char* kDefaultServer = "hkldnbkjfnbjnidnpbmpmfheadpm";
+  //   // "Error initializing license system:\n"
+  //   const char* kLicenseInitError = 
+  //     "anannemampdfkdbjdlokmcjdjkljlhiibmmencnhnomkodkffieimcomobbehomabicoll";
 
-//   // "Error initializing license system:\n"
-//   const char* kLicenseInitError =
-//     "anannemampdfkdbjdlokmcjdjkljlhiibmmencnhnomkodkffieimcomobbehomabicoll";
+  //   // "License Error:\n"
+  //   const char* kLicenseError = "aefaigpbgjbbejondkamhmdmmjbcgf";
 
-//   // "License Error:\n"
-//   const char* kLicenseError = "aefaigpbgjbbejondkamhmdmmjbcgf";
+  //   // "mrViewer"
+  //   const char* kProduct = "cfmjmjmonjcdcpkh";
 
-//   // "mrViewer"
-//   const char* kProduct = "cfmjmjmonjcdcpkh";
-
-//   // "2007.01"
-//   const char* kVersion = "hkledbdicaoigp";
-
-
-//   RLM_HANDLE   rh;
-//   RLM_LICENSE lic;
-
-//   mrv::cipher::PC1 cypher;
-
-// }
+  //   // "2007.01"
+  //   const char* kVersion = "hkledbdicaoigp";
 
 
-// const char* decrypt( const char* text )
-// {
-//   cypher.ascipherlen = strlen(text);
-//   cypher.plainlen    = strlen(text);
-//   cypher.ascii_decrypt128( text, kPC1_key );
-//   return cypher.plainText;
-// }
+  //   RLM_HANDLE   rh;
+  //   RLM_LICENSE lic;
 
-bool open_license(const char* prog)
-{
+  //   mrv::cipher::PC1 cypher;
+
+  // }
+
+
+  // const char* decrypt( const char* text )
+  // {
+  //   cypher.ascipherlen = strlen(text);
+  //   cypher.plainlen    = strlen(text);
+  //   cypher.ascii_decrypt128( text, kPC1_key );
+  //   return cypher.plainText;
+  // }
+
+  bool open_license(const char* prog)
+  {
     // rh = rlm_init( ".", prog, decrypt( kDefaultServer ) );
     // int stat = rlm_stat(rh);
     // if ( stat )
@@ -89,30 +89,30 @@ bool open_license(const char* prog)
     // 	char errstring[RLM_ERRSTRING_MAX];
 
     // 	fprintf(stderr, decrypt( kLicenseInitError ) );
-    // 	fprintf(stderr, "%s\n", rlm_errstring((RLM_LICENSE) NULL, rh,
+    // 	fprintf(stderr, "%s\n", rlm_errstring((RLM_LICENSE) NULL, rh, 
     // 					      errstring));
     // 	close_license();
     // 	exit(1);
     //   }
 
     return true;
-}
+  }
 
-// void printstat( RLM_HANDLE rh, RLM_LICENSE lic )
-// {
-//   char errstring[RLM_ERRSTRING_MAX];
-//   fprintf(stderr, decrypt( kLicenseError ) );
-//   fprintf(stderr, "%s\n", rlm_errstring(lic, rh, errstring));
+  // void printstat( RLM_HANDLE rh, RLM_LICENSE lic )
+  // {
+  //   char errstring[RLM_ERRSTRING_MAX];
+  //   fprintf(stderr, decrypt( kLicenseError ) );
+  //   fprintf(stderr, "%s\n", rlm_errstring(lic, rh, errstring));
 
-//   checkin_license();
-//   close_license();
+  //   checkin_license();
+  //   close_license();
 
-//   exit(1);
-// }
+  //   exit(1);
+  // }
 
-bool checkout_license()
-{
-    return true;
+  bool checkout_license()
+  {
+     return true;
     // char* product = strdup( decrypt( kProduct ) );
     // char* version = strdup( decrypt( kVersion ) );
 
@@ -122,36 +122,36 @@ bool checkout_license()
     // free( version );
 
     // return (lic != NULL);
-}
+  }
 
-bool check_license_status()
-{
+  bool check_license_status()
+  {
     // if (!lic) {
     //   if ( !checkout_license() ) return false;
     // }
 
     // int stat = rlm_license_stat(lic);
     // if ( stat == 0 || stat == RLM_EL_INQUEUE || stat == RLM_EL_OVERSOFT)
-
-    return true;
+       
+      return true;
 
     // printstat( rh, lic );
     // return false;
-}
+  }
 
-bool checkin_license()
-{
+  bool checkin_license()
+  {
     // if (lic)  rlm_checkin(lic);
     // lic = NULL;
     return true;
-}
+  }
 
 
-bool close_license()
-{
+  bool close_license()
+  {
     // rlm_close(rh);
     // rh = NULL;
     return true;
-}
+  }
 
 }
