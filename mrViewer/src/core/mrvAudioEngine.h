cdcd85b8265d1bd05a5174ec541bc1de458a4ba6
Gonzalo Garramuno <ggarra13@gmail.com>
2019-03-01 16:19:54 -0300

    fltk1.4 changes.  Compiles and links, but crashes on running.
    Former-commit-id: 2c542b41fddfec14947354d61484f1d66ca34809
    Former-commit-id: 7d5c5ee041f76520547d01fae4b55fea3868a776 [formerly 6b7dcbe56dfc7423689eb239961ce25fd510cfd2] [formerly 871ea2fbf96792882eef2d4870e3c71186fd956e [formerly 2d97ae1e197dc11db831a03aeca67d1bf39e0506]]
    Former-commit-id: fbfdcddd7b8204e6d492c2c8f60ac0382f421a1b [formerly c89169f05bf410c2091fdd25a11cf873bd9364d9]
    Former-commit-id: 4469ae83f8ac57c97c2c243f1e86788426276c7d

--- mrViewer/src/core/mrvAudioEngine.h
+++ mrViewer/src/core/mrvAudioEngine.h
@@ -1,6 +1,6 @@
 /*
     mrViewer - the professional movie and flipbook playback
-    Copyright (C) 2007-2014  Gonzalo Garramuño
+    Copyright (C) 2007-2014  Gonzalo GarramuÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ±o
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -36,9 +36,9 @@ extern "C" {
 #include <atomic>
 
 
-namespace mrv {
-
 class ViewerUI;
+
+namespace mrv {
 
 class AudioEngine
 {
