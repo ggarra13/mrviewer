2b466d910d6dadbdd6ed201eecc67445671c5269
Gonzalo Garramuño <ggarra13@gmail.com>
2019-03-03 14:04:55 -0300

    Plays back movies and sequences command-line.
    Former-commit-id: 0a32e295185ec39318aee556e0bb347517edfa1e [formerly 8ca10f530211925658d110821dd72ea799713928]
    Former-commit-id: 470f89f06da54c18d9c72690defe61e1c7295098 [formerly 29789b1913f303e151b3bc0567818e66af73b7c2] [formerly 419fc8498c7f60736206040ae1172d2c5d56e69d [formerly 7b4b9b0fdef2b76c36345241822f955d8f6164c3] [formerly 26c6389b402bc1e0114134c2d4fd11efa4f950af [formerly 3054026657d112059d4a370d5e8af926b9f61376]]]
    Former-commit-id: 4c973ce2418fe1bb78090d75cf01f1ad435661fb [formerly 9640d2197b994dc806aa9f6f287f5a0681cf8a7d]
    Former-commit-id: 0ad4fdaf023e5b243dac91934a00ab6bc772272c [formerly 4090cd9ad370cd47c9152f659c7bb74f59287901]
    Former-commit-id: c3ba01a13daf368dfa72587330dc5fe9db1cb3dc

--- mrViewer/src/gui/mrvElement.h
+++ mrViewer/src/gui/mrvElement.h
@@ -16,96 +16,38 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifndef mrvElement_h
+#define mrvELement_h
+
 #define __STDC_LIMIT_MACROS
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>  // for PRId64
 
+#include <FL/Fl_Group.H>
 #include <FL/Fl_Menu_Item.H>
 #include <FL/Fl_Box.H>
 
 #include "core/clonedImage.h"
 #include "gui/mrvMedia.h"
 
+
 namespace mrv {
 
-static const int VMARGIN = 2;
 
 class Element : public Fl_Group {
     Fl_Box *image;		// image part of widget
     Fl_Box *label;		// label part of widget
 public:
-Element(mrv::media& m) : Fl_Group(0,0,1,80+VMARGIN*2),
-                         _elem( m )
-    {		// VMARGIN makes group slightly larger than items
-        // Create widget to hold image
-        //     Assign different image to each instance of us for demo purposes..
-        //
+    Element(mrv::media& m);
 
-        CMedia* img = m->image();
-        m->create_thumbnail();
-        image = new Fl_Box(0,VMARGIN,image->w(),image->h());
-        image->image( m->thumbnail() );
-        // Create label
-        label = new Fl_Box(image->w(), VMARGIN, 800, 80);
-        char info[2048];
-        if ( dynamic_cast< clonedImage* >( img ) != NULL )
-        {
-            sprintf( info,
-                     _("Cloned Image\n"
-                       "Name: %s\n"
-                       "Date: %s\n"
-                       "Resolution: %dx%d"),
-                     img->name().c_str(),
-                     img->creation_date().c_str(),
-                     img->width(),
-                     img->height()
-                     );
-        }
-        else
-        {
-            sprintf( info,
-                     _("Directory: %s\n"
-                       "Name: %s\n"
-                       "Resolution: %dx%d\n"
-                       "Frames: %" PRId64 "-%" PRId64 " FPS %g"),
-                     img->directory().c_str(),
-                     img->name().c_str(),
-                     img->width(),
-                     img->height(),
-                     img->start_frame(),
-                     img->end_frame(),
-                     img->fps()
-                     );
-        }
-        label->copy_label( info );
-        label->color(0xddddff00);
-        label->align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE);
-        label->box(FL_FLAT_BOX);
-        end();
-    }
-    void Label(const char *s) {
-        if ( label ) label->copy_label(s);
-    }
+    void Label(const char *s);
+
     // Draw ourself at a specific X,Y position
-    void DrawAt(int X, int Y) {
-        int xsave = x(), ysave = y();
-        // Temporarily move widget to mouse position
-        position(X,Y);
-        init_sizes();
-        redraw();
-        // Draw widget at mouse position
-        draw();				// draw widget's group
-        // ..now move widget back to where it was
-        position(xsave,ysave);
-        init_sizes();
-        redraw();
-    }
+    void DrawAt(int X, int Y);
 
-    const mrv::media& element() const { return _elem; }
+    const mrv::media& element() const;
 
-    void draw() {
-        Fl_Group::draw();
-    }
+    void draw();
 
   protected:
     mrv::media _elem;
@@ -114,3 +56,6 @@ Element(mrv::media& m) : Fl_Group(0,0,1,80+VMARGIN*2),
 
 
 }
+
+
+#endif //
