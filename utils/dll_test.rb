50088aa12a30cca72fcfbf08e8ac47829e7ef076
Gonzalo Garramuño <ggarra13@gmail.com>
2019-03-07 12:36:30 -0300

    Fixed FindImageMagick to only find _RE_ (release) builds. Changed CMakeLists.txt to list all sort of warnings (but none appear -- not sure why).
    Former-commit-id: 1b76adaebc6218e67ae7df830994654045205a4d [formerly 2226a879ed60814413fcde5bf3f070fbd1e7d5bd] [formerly 5b6f67a80f914dd2a57c3a2d5f4c62c5f81dea90] [formerly eaee51ac336b7ae3fadef55f4cba473377426d67] [formerly a18b87ad7db638c9260a04d2c1a75c08ce1f3cbd [formerly 4c05c4c7473c45c3757fbedb3af1e9ff4c540ce0 [formerly 18769e0b759b8b9d9548c818a9913d921def2ed2] [formerly a18b87ad7db638c9260a04d2c1a75c08ce1f3cbd [formerly a18b87ad7db638c9260a04d2c1a75c08ce1f3cbd [formerly 1eee83e23ebe0a82335daec0ac9351dee2131963]]]]]
    Former-commit-id: a82065b779404cc68ffd64a8bc59379c76b34acf [formerly a408dbb224f32148cc730b2d3f42d415190c3207 [formerly f45c728cc3350bfb936464b2a7733447e0eff898] [formerly a82065b779404cc68ffd64a8bc59379c76b34acf [formerly a82065b779404cc68ffd64a8bc59379c76b34acf [formerly b04e5bdb0023b004a5cf3b1ecc6d0d79358f2036]]]]
    Former-commit-id: e9a37c3767535d0338f7971ff91c23865711a60b
    Former-commit-id: 9eaf9effe8aea7cab32f2077d50c93f0018a2bbc
    Former-commit-id: 92ce0b1c339ff6d5f26ca92135977a4e8505a91b
    Former-commit-id: 8c0079d50c24e84fd42727b0a7fc76857664aa80

--- utils/dll_test.rb
+++ utils/dll_test.rb
@@ -1,26 +1,40 @@
 #!/usr/bin/env ruby
 
+if ARGV[0]
+  dir = ARGV[0]
+  arch = 'x64'
+else
+  dir = "BUILD/Windows*32/Release/bin"
+  arch = 'x86'
+end
 
-puts "Check BUILD/Windows*32/Release/bin"
+puts "Check #{dir}"
 
-files = Dir.glob("BUILD/Windows-6.3.9600-32/Release/bin/*")
-
-for f in files
-  h = `dumpbin -headers #{f}`
-  if h =~ /machine.*x64/
-    puts "ERROR: #{f} is x64 when it should be x86"
+def check_files( files, arch )
+  for f in files
+    h = `dumpbin -headers #{f}`
+    if h =~ /machine.*^#{arch}/
+      puts "ERROR: #{f} is wrong architecture when it should be #{arch}"
+      if h =~ /debug/i
+        puts "ERROR: #{f} is set incorrectly wrong #{arch} debug"
+      end
+    else
+      if h =~ /debug/i
+        puts "ERROR: #{f} is set to #{arch} debug"
+      end
+    end
   end
 end
+
+
+files = Dir.glob(dir + "/*")
+
+check_files( files, arch )
 
 puts "Check BUILD/Windows*64/Release/bin"
 files = Dir.glob("BUILD/Windows-6.3.9600-64/Release/bin/*")
 
-for f in files
-  h = `dumpbin -headers #{f}`
-  if h =~ /machine.*x86/
-    puts "ERROR: #{f} is x86 when it should be x64"
-  end
-end
+check_files( files, 'x64' )
 
 puts "Finished checking BUILD area.  Will check vc14_Windows_* area."
 
@@ -31,21 +45,12 @@ puts "Finished checking BUILD area.  Will check vc14_Windows_* area."
 puts "Check lib/vc14_Windows_64/bin"
 files = Dir.glob("../../lib/vc14_Windows_64/bin/*")
 
-for f in files
-  h = `dumpbin -headers #{f}`
-  if h =~ /machine.*x86/
-    puts "ERROR: #{f} is x86 when it should be x64"
-  end
-end
+check_files( files, 'x64' )
+
 
 
 
 puts "Check lib/vc14_Windows_32/bin"
 files = Dir.glob("../../lib/vc14_Windows_32/bin/*")
 
-for f in files
-  h = `dumpbin -headers #{f}`
-  if h =~ /machine.*x64/
-    puts "ERROR: #{f} is x64 when it should be x86"
-  end
-end
+check_files( files, 'x86' )
