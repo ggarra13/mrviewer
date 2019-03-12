dea936d0d0376b037bffa80103198426584cd353
Gonzalo Garramuño <ggarra13@gmail.com>
2019-03-12 07:47:18 -0300

    Saved ->hires to ->left changes.
    Former-commit-id: ac5e52d7c07a623313112ddb709d9501c60cf7a8 [formerly 6b9fd74a22ee268dc45e218bf6e7d557074a5fb7 [formerly 15ae0dca4068410fd67113a975c8fb5a090981a7]]
    Former-commit-id: 45db0313694d859592c7292a00897b5e70e8723f [formerly 45db0313694d859592c7292a00897b5e70e8723f [formerly 7ebe136f53c5cd7b154eea9d071fb5e52c9bbe93]] [formerly ebdcc6d37004ea72c2b323a5234575aa0ee25d6a [formerly ebdcc6d37004ea72c2b323a5234575aa0ee25d6a [formerly b1212ec305e38df970c710905a1c7925dfa5b408]]] [formerly 92707860eeab70750a9e4075455abc719b9198ce [formerly 92707860eeab70750a9e4075455abc719b9198ce [formerly e0e519c6e31ed522dfb8990d0dfb5aab3b8f878b]] [formerly 785ab963d8c8ee25147bb5eec92f40497af5b537 [formerly 785ab963d8c8ee25147bb5eec92f40497af5b537 [formerly 1b47c3ce7f1ae7bac905144f241f78ff01264f83]]]]
    Former-commit-id: 41ea56d3234f5492b5222a40cd19eb60ac2c7113 [formerly 41ea56d3234f5492b5222a40cd19eb60ac2c7113 [formerly 23131104ad76ce3e6f3a6ddffcc6e8a84d52e549]] [formerly 85a1281e16dce1ef3d8463be4cd228109b56ddf7 [formerly 85a1281e16dce1ef3d8463be4cd228109b56ddf7 [formerly 27a0b50a05578414df99d42178b8336a3dba9597]]]
    Former-commit-id: c1c4f82075050f7905d66a7c7726bdf7f9965c13 [formerly c1c4f82075050f7905d66a7c7726bdf7f9965c13 [formerly 0eb6e8af89a33742639106ad35458772a804f582]]
    Former-commit-id: 0f6ad19adf047532325f96f297b9576f813afbb4 [formerly d4ca59f685a9bc01994ba98b41c079b44cb50b54]
    Former-commit-id: e3213681340f1664741d45a303f557480df36849

--- mrViewer/src/gui/mrvVectorscope.cpp
+++ mrViewer/src/gui/mrvVectorscope.cpp
@@ -163,7 +163,7 @@ void Vectorscope::draw_pixels( const mrv::Recti& r )
         return;
     }
     CMedia* img = m->image();
-    mrv::image_type_ptr pic = img->hires();
+    mrv::image_type_ptr pic = img->left();
     if ( !pic ) return;
 
     tooltip( NULL );
