74255da6a41de0e70fc09a79de46f0a0fef756bf
Gonzalo Garramuño <ggarra13@gmail.com>
2019-03-19 15:15:00 -0300

    mrv::Table fixed, better image info window.  Still some problems remain.
    Former-commit-id: 5ce198d5867b1084b2bcc40d7b9cfd6e5c8c6f2f [formerly 14510961f7ad707f11258c3540dc08142bd1c800 [formerly 58b2818ea26bdddebf4cab2712ef843a0355ec9c] [formerly 5d5c85b19fa96b57584cdabe10493539fd32453b [formerly 9be3694b39fd98002b44d5875c3b91e4fa93e46b]]]
    Former-commit-id: 3736e89049a411d6d8bea57d15b71ef7e207ca21 [formerly 23dc145a4f22ceef4e5239aab33c9610693d81a3] [formerly d776b284b520fa30e36e49d6e6abcfb071f1d4e5] [formerly f2e25aa02f9a559ef36ea4939ce110a71ddb46e8] [formerly 74e97ee64f28a5c97a70853f2a2936134ef98266 [formerly dbe97a6500dd427c1d9713b496e885ac32a5541a [formerly b56cec2447e4728c87110a0197971924e4d9c619] [formerly 74e97ee64f28a5c97a70853f2a2936134ef98266 [formerly b8346a679ce1ec4f45005c728844e54f1fd2b9a4]]]] [formerly f1d3492b6efe790cdf89fb1c942c805d0f357792 [formerly 4174aff184b5e9d59111bd60483f9bd4db1e56e0] [formerly a28a85a9c71767f9200529b0f6e4b757019fbbfb] [formerly bee49150614b8cd6555eff2611445bf14b773f55] [formerly 7a7277967b6c6224847715149c9c7b9f6640b7cf [formerly 52c30a7cb2edef09d4182fc6a864d2970d1a9069 [formerly 9da18b2a17199d0515989442605f4a22de229d65] [formerly db9a7def2b15d3fa062a9290291bda5d04271500 [formerly 3451aea703e6a363cd0eb5669f0d33f976d33f47] [formerly 7a7277967b6c6224847715149c9c7b9f6640b7cf [formerly 05ae99e563ff4fee480a68c9ab8b50dbd7b5c37b]]]]]]
    Former-commit-id: 0b10875ee59e31836ce3e27727587bf1b7354e45 [formerly e9307c37c49e709dd7a124d05b57d867f848dd37] [formerly b3521b8dd82f55df377183fa07c51273225dc427] [formerly ffc6af8e99599413455c66cefe6020be50f1104b] [formerly 3f164087e00941d38beb5c74fc03d9f25cd8d783 [formerly fd108146e3e4953c06693de0eaead0f28d2c709c [formerly f3b4682df145a78a870b12f3b8d9a4fcbb8491bf] [formerly 5d81ae80bbb77c82ca38b882c7caaf176d1a3350 [formerly f8f2e45f7f65fe36910fd0402f9a729f947a4ef0] [formerly 3f164087e00941d38beb5c74fc03d9f25cd8d783 [formerly c522e909768be44549fdd447fd6c7e81f28d4945]]]]]
    Former-commit-id: 2fea8385d5dcf1f4e38b049d2131c1f888b6050c [formerly 077ee808174745837d2e9106175496667ca76e85 [formerly 3cab8f0c3d60dbd1b543bcdaa5354b5fc71c324b] [formerly 2fea8385d5dcf1f4e38b049d2131c1f888b6050c [formerly 95660c9c9aa3c417a9cf2c16eed763de6ecf299c]]]
    Former-commit-id: e397e69951d5904d1fcdc3f0b0b80c3add05bb1b
    Former-commit-id: 2bd819f53897bd06d723039427bc4d0d7a597a16
    Former-commit-id: 8b6f9c14f3c1eadf9a41a98127a4738704b5035c
    Former-commit-id: 9d51de0d375e215cc68b141c4c223ab0537c3ddb

--- mrViewer/src/gui/mrvTable.cpp
+++ mrViewer/src/gui/mrvTable.cpp
@@ -25,23 +25,105 @@
  *
  */
 
+#include <iostream>
+
+#include <FL/fl_draw.H>
+
 #include <gui/mrvTable.h>
+
+#define MAX_COLS 2
 
 namespace mrv {
 
 Table::Table( int x, int y, int w, int h, const char* l ) :
 Fl_Table( x, y, w, h, l )
 {
+    rows(0);                   // start with no rows, grow later
+    row_header(0);              // enable row headers (along left)
+    row_height_all(24);         // default height of rows
+    row_resize(0);              // enable row resizing
+    // Cols
+    cols(MAX_COLS);             // how many columns
+    col_header(1);              // enable column headers (along top)
+    col_width_all(w/2);         // default width of columns
+    col_resize(1);              // enable column resizing
+    end();
 }
+
 
 Table::~Table()
 {
 }
 
-void Table::draw_cell(TableContext context, int R, int C, 
+// Draw the row/col headings
+//    Make this a dark thin upbox with the text inside.
+//
+void Table::DrawHeader(const char *s, int X, int Y, int W, int H) {
+    fl_push_clip(X,Y,W,H);
+    fl_draw_box(FL_THIN_UP_BOX, X,Y,W,H, row_header_color());
+    fl_color(FL_BLACK);
+    fl_draw(s, X,Y,W,H, FL_ALIGN_CENTER);
+    fl_pop_clip();
+} 
+
+
+void Table::add( Fl_Widget* w )
+{
+    int c = children() % 2;
+    if ( c == 0 )
+    {
+	rows( rows() + 1 );
+    }
+    int r = rows() - 1;
+    
+    int X, Y, W, H;
+    find_cell( CONTEXT_TABLE, r, c, X, Y, W, H );
+    w->resize( X, Y, W, H );
+    Fl_Table::add( w );
+}
+
+void Table::draw_cell(TableContext context, int ROW, int COL, 
 		      int X, int Y, int W, int H)
 {
-    // @TODO: fltk1.4
+    char s[40];
+    switch ( context ) {
+	case CONTEXT_STARTPAGE:         // before page is drawn..
+	    fl_font(FL_HELVETICA, 16);  // the font for our drawing operations
+	    return; 
+	case CONTEXT_COL_HEADER:                  // Draw column headers
+	    if ( COL >= 2 ) return;
+	    sprintf(s,"%s",headers[COL]);                // "A", "B", "C", etc.
+	    DrawHeader(s,X,Y,W,H);
+	    return; 
+	case CONTEXT_CELL: 
+	    return;
+	case CONTEXT_RC_RESIZE:
+	    {
+		// Change the table's column_width() for Value column to
+		// reach table's edge
+		// width of table minus Attribute's current col_width(),
+		// which may have been changed interactively by user.
+		// Note: use of Fl_Table:tiw for table's "inner width"
+		col_width(1, tiw - col_width(0));
+		
+	  
+		int X, Y, W, H;
+		int index = 0;
+		int R = rows();
+		int C = cols();
+		for ( int r = 0; r < R; ++r ) {
+		    for ( int c = 0; c < C; ++c ) {
+			if ( index >= children() ) break;
+			find_cell(CONTEXT_TABLE, r, c, X, Y, W, H);
+			child(index++)->resize(X,Y,W,H);
+		    }
+		}
+		init_sizes();			// tell group children resized
+		return;
+	    }
+	default:
+	    return;
+    }
 }
     
 } // namespace mrv
