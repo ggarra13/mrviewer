b1169bb10a6b1228fdc93744b605ad8c961c0cbb
Gonzalo Garramuno <ggarra13@gmail.com>
2019-04-04 22:10:15 -0300

    Fixes for Windows build of fltk1.4 version of mrViewer.
    Former-commit-id: 4821c46e0aa4170470523053fe7deda967c0028e [formerly e6ec234dd97e3b0e45fd7ec28ed7bb7519f203e1]
    Former-commit-id: 2e65e2646836562a3f7f3933d4cafbfe419bc489 [formerly b686fbfe03d65ed0e997704742cbd7a234e22d55] [formerly 693452809470a58f6d2ecf308af221ed0c87c863 [formerly dee093cae12a034686361a38d450443fc9ca5616] [formerly 019d16cf538b41847864d48fb6189ea2e50688ca [formerly 35ac54b4b5c1bdd70089d3cb7ae2ddc4241b4819]]]
    Former-commit-id: b943e4233736f01dd6c5ab2291f99defa69d68b8 [formerly 50183b46df25905cb92e023323036a1a7f783b87]
    Former-commit-id: f9f246173bfa1e867ee54d8ee7adde969d71558a [formerly eac94b0a36c7109eceedfccbd756bdf6b42240b2]
    Former-commit-id: 2bb8295601d76d0fedc2f04d6df4ed6bd4c6fe9e

--- mrViewer/src/standalone/mrvCommandLine.h
+++ mrViewer/src/standalone/mrvCommandLine.h
@@ -1,6 +1,6 @@
 /*
     mrViewer - the professional movie and flipbook playback
-    Copyright (C) 2007-2014  Gonzalo GarramuÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ±o
+    Copyright (C) 2007-2014  Gonzalo Garramuño
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -62,7 +62,7 @@ typedef std::vector< std::string > stringArray;
   //
   // Command-line parser
   //
-  void parse_command_line( const int argc, const char** argv,
+  void parse_command_line( const int argc, char** argv,
 			   ViewerUI* ui, 
                            mrv::Options& opts);
 }
