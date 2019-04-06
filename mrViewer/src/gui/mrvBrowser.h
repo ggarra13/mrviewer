6643893999a6b6539007a2bc0a627ed319c00fae
Gonzalo Garramuño <ggarra13@gmail.com>
2019-04-06 10:58:07 -0300

    Updated files for fltk1.4.  Removed useless mrvPreferencesBrowser.cpp/h.
    Former-commit-id: 3c79b573e74028e8e3d2a0149d63b8e93834ee47 [formerly 01d1f4e4bc8b7f4ac94e47561dfdc4e2c1c69036 [formerly 95c9d54d6722018ebd0be707417e12f3d158eb92]]
    Former-commit-id: ca8872edcbeda0a7457f7fc4cf25ce69ecf93ef1 [formerly ca8872edcbeda0a7457f7fc4cf25ce69ecf93ef1 [formerly 9eeb27931f05dccb0063d9377d077abec371bd5d]] [formerly 4428aab82e76ca0293fe0b55af080babce1db447 [formerly 4428aab82e76ca0293fe0b55af080babce1db447 [formerly 1e2d820b7684a350ce1f4a4d5bbef97bd8683ee0]]] [formerly 5fa3bab498bd3e4777bcd86f94a2dca78e5a9586 [formerly 5fa3bab498bd3e4777bcd86f94a2dca78e5a9586 [formerly 11bbae3a29578f6d081f7e2285aedcf747b5a4fc]] [formerly ca0ccd1d5d0ab8ae9474c018e8c134280655c2f3 [formerly ca0ccd1d5d0ab8ae9474c018e8c134280655c2f3 [formerly a767b31ea402da5675d0e6605d8a9c5eab4c8b87]]]]
    Former-commit-id: a45e93de6d25f157e0481e26203c3df42901eed5 [formerly a45e93de6d25f157e0481e26203c3df42901eed5 [formerly 33e90914a349868c09cdaa74233f98df25bc41e8]] [formerly 3beaa776e920b4745d32b21280f58eba0b7b67d3 [formerly 3beaa776e920b4745d32b21280f58eba0b7b67d3 [formerly edc23fb58a043f0ea9dc10cfc1ef0ccd9be7578f]]]
    Former-commit-id: 243335842948c77d98cfd0d46e0f50c4db5b933b [formerly 243335842948c77d98cfd0d46e0f50c4db5b933b [formerly 62557d48d18717cbe7a9aec0e92b578222771e73]]
    Former-commit-id: 9a12848bcbaa5719e9db28f9079d6288b5ec15cd [formerly e0abd385282ee6d48cf903edab81831e1bec2a8d]
    Former-commit-id: f73178e7cd3f966794edf8412810821baf55107f

--- mrViewer/src/gui/mrvBrowser.h
+++ mrViewer/src/gui/mrvBrowser.h
@@ -1,6 +1,6 @@
 /*
     mrViewer - the professional movie and flipbook playback
-    Copyright (C) 2007-2014  Gonzalo GarramuÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ±o
+    Copyright (C) 2007-2014  Gonzalo Garramuño
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -28,6 +28,7 @@
 #ifndef mrvBrowser_h
 #define mrvBrowser_h
 
+#include <FL/Fl_Window.H>
 #include <FL/Fl_Browser.H>
 #include <FL/fl_draw.H>
 
@@ -37,66 +38,174 @@ class ViewerUI;
 namespace mrv
 {
 
-class Browser : public Fl_Browser
-{
-public:
-    Browser( int x, int y, int w, int h, const char* l = 0 );
-    virtual ~Browser() {};
-
-    virtual int handle( int e );
-    virtual void layout();
-    virtual void draw();
-
-    bool column_separator() const    {
-        return _column_separator;
+class Browser : public Fl_Browser {
+    Fl_Color  _colsepcolor;     // color of column separator lines 
+    int       _showcolsep;      // flag to enable drawing column separators
+    Fl_Cursor _last_cursor;     // saved cursor state info
+    int       _drag_col;        // col# user is dragging (-1 = not dragging)
+    int      *_widths;          // pointer to user's width[] array
+    int       _nowidths[1];     // default width array (non-const)
+    // CHANGE CURSOR
+    //     Does nothing if cursor already set to value specified.
+    //
+    void change_cursor(Fl_Cursor newcursor) {
+        if ( newcursor == _last_cursor ) return;
+        window()->cursor(newcursor);
+        _last_cursor = newcursor;
     }
-    void column_separator(bool t) {
-        _column_separator = t;
+    // RETURN THE COLUMN MOUSE IS 'NEAR'
+    //     Returns -1 if none.
+    //
+    int which_col_near_mouse() {
+        int X,Y,W,H;
+        Fl_Browser::bbox(X,Y,W,H);            // area inside browser's box()
+        // EVENT NOT INSIDE BROWSER AREA? (eg. on a scrollbar)
+        if ( ! Fl::event_inside(X,Y,W,H) ) {
+            return(-1);
+        }
+        int mousex = Fl::event_x() + hposition();
+        int colx = this->x();
+        for ( int t=0; _widths[t]; t++ ) {
+            colx += _widths[t];
+            int diff = mousex - colx;
+            // MOUSE 'NEAR' A COLUMN?
+            //     Return column #
+            //
+            if ( diff >= -4 && diff <= 4 ) {
+                return(t);
+            }
+        }
+        return(-1);
+    }
+    // FORCE SCROLLBAR RECALC
+    //    Prevents scrollbar from getting out of sync during column drags
+    void recalc_hscroll() {
+      int size = textsize();
+      textsize(size+1);     // XXX: changing textsize() briefly triggers
+      textsize(size);       // XXX: recalc Fl_Browser's scrollbars
+      redraw();
+    }
+protected:
+    // MANAGE EVENTS TO HANDLE COLUMN RESIZING
+    int handle(int e) {
+        // Not showing column separators? Use default Fl_Browser::handle() logic
+        if ( !showcolsep() ) return(Fl_Browser::handle(e));
+        // Handle column resizing
+        int ret = 0;
+        switch ( e ) {
+            case FL_ENTER: {
+                ret = 1;
+                break;
+            }
+            case FL_MOVE: {
+                change_cursor( (which_col_near_mouse() >= 0) ? FL_CURSOR_WE
+                                                             : FL_CURSOR_DEFAULT);
+                ret = 1;
+                break;
+            }
+            case FL_PUSH: {
+                int whichcol = which_col_near_mouse();
+                if ( whichcol >= 0 ) {
+                    // CLICKED ON RESIZER? START DRAGGING
+                    _drag_col = whichcol;
+                    change_cursor(FL_CURSOR_DEFAULT);
+                    return 1;   // eclipse event from Fl_Browser's handle()
+                }               // (prevents FL_PUSH from selecting item)
+                break;
+            }
+            case FL_DRAG: {
+                if ( _drag_col != -1 ) {
+                    // Sum up column widths to determine position
+                    int mousex = Fl::event_x() + hposition();
+                    int newwidth = mousex - x();
+                    for ( int t=0; _widths[t] && t<_drag_col; t++ ) {
+                        newwidth -= _widths[t];
+                    }
+                    if ( newwidth > 0 ) {
+                        // Apply new width, redraw interface
+                        _widths[_drag_col] = newwidth;
+                        if ( _widths[_drag_col] < 2 ) {
+                            _widths[_drag_col] = 2;
+                        }
+                        recalc_hscroll();
+                        redraw();
+                    }
+                    return 1;   // eclipse event from Fl_Browser's handle()
+                }
+                break;
+            }
+            case FL_LEAVE:
+            case FL_RELEASE: {
+                _drag_col = -1;                         // disable drag mode
+                change_cursor(FL_CURSOR_DEFAULT);       // ensure normal cursor
+                if ( e == FL_RELEASE ) return 1;        // eclipse event
+                ret = 1;
+                break;
+            }
+        }
+        return(Fl_Browser::handle(e) ? 1 : ret);
+    }
+    void draw() {
+        // DRAW BROWSER
+        Fl_Browser::draw();
+        if ( _showcolsep ) {
+            // DRAW COLUMN SEPARATORS
+            int colx = this->x() - hposition();
+            int X,Y,W,H;
+            Fl_Browser::bbox(X,Y,W,H);
+            fl_color(_colsepcolor);
+            for ( int t=0; _widths[t]; t++ ) {
+                colx += _widths[t];
+                if ( colx > X && colx < (X+W) ) {
+                    fl_line(colx, Y, colx, Y+H-1);
+                }
+            }
+        }
+    }
+public:
+    // CTOR
+    Browser(int X,int Y,int W,int H,const char*L=0) : Fl_Browser(X,Y,W,H,L) {
+        _colsepcolor = Fl_Color(FL_GRAY);
+        _last_cursor = FL_CURSOR_DEFAULT;
+        _showcolsep  = 0;
+        _drag_col    = -1;
+        _nowidths[0] = 0;
+        _widths      = _nowidths;
+    }
+
+    int absolute_item_index();
+
+    // GET/SET COLUMN SEPARATOR LINE COLOR
+    Fl_Color colsepcolor() const {
+        return(_colsepcolor);
+    }
+    void colsepcolor(Fl_Color val) {
+        _colsepcolor = val;
+    }
+    // GET/SET DISPLAY OF COLUMN SEPARATOR LINES
+    //     1: show lines, 0: don't show lines
+    //
+    int showcolsep() const {
+        return(_showcolsep);
+    }
+    void showcolsep(int val) {
+        _showcolsep = val;
+    }
+    // GET/SET COLUMN WIDTHS ARRAY
+    //    Just like fltk method, but array is non-const.
+    //
+    int *column_widths() const {
+        return(_widths);
     }
 
     void column_labels( const char** labels )
     {
     }
     
-    void column_separator_color( Fl_Color c ) {
-        _column_separator_color = c;
+    void column_widths(const int *val) {
+        _widths = (int*)val;
+        Fl_Browser::column_widths(val);
     }
-    Fl_Color column_separator_color() const {
-        return _column_separator_color;
-    }
-
-    //
-    // Returns the absolute (selected) item index by adding all children.
-    //
-    int absolute_item_index();
-
-    void auto_resize( bool t ) {
-        _auto_resize = t;
-    }
-
-    inline int xposition() const { return _xposition; }
-    inline void xposition( int x ) { _xposition = x; }
-
-protected:
-
-    void set_column_start( int col, int width );
-
-    int absolute_item_index( Fl_Group* g );
-    int absolute_item_index( bool& found,
-                             Fl_Widget* item,
-                             Fl_Widget* child );
-
-    void change_cursor( Fl_Cursor cursor );
-    int which_col_near_mouse();
-
-protected:
-    Fl_Color  _column_separator_color;	// color of column separator lines
-    Fl_Cursor _last_cursor;	// saved cursor state info
-    int       _xposition;
-    int       _dragcol;		// col# user is currently dragging
-    bool      _column_separator;	// flag to enable drawing column separators
-    bool      _dragging;	// true if user dragging a column
-    bool      _auto_resize;
 };
 
 } // namespace mrv
