1a035d8d7eafde3c43b1f91fc7cf4b3f23fd0b96
Gonzalo Garramuño <ggarra13@gmail.com>
2019-04-06 11:27:13 -0300

    Added mrv::Tree to handle images of closing and opening tree.
    Former-commit-id: db8ae536a5b0e80d3c95753a1a2774e9b63da87c

---
+++ mrViewer/src/gui/mrvTree.h
@@ -0,0 +1,14 @@
+
+#include <FL/Fl_Tree.H>
+
+namespace mrv
+{
+
+class PreferencesTree : public Fl_Tree
+{
+  public:
+    PreferencesTree( int X, int Y, int W, int H, const char* l = 0 );
+};
+
+
+} // namespace mrv
