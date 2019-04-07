a578098859bb15d04d8a2296609667e6517e11d9
Gonzalo Garramuño <ggarra13@gmail.com>
2019-04-06 22:19:51 -0300

    Made color info copy the values of the display. Reversed open/closed icons in mrv::PreferencesTree.
    Former-commit-id: b3a703bc08b8f32587158daefe728c4ffd5c7aef [formerly 6b7ccfb26e7fae6bc3b2fccc55689545df823e6c]
    Former-commit-id: fda237349497aec6e12a5e274a45d544d6c7c92d [formerly 580ce6a815e42a360d61c1fc6a69971147961bff] [formerly af28da18d10691ec996b6380f955836fad2c1353 [formerly 5a0c6c80210ff392170ebe0a2148bfc8c2131bda]]
    Former-commit-id: d3ab058d28449ab018f4dc25be2dee6f7a428069 [formerly 8fed66cb7d99e11db882c6a7be1becd1b36b5c89]
    Former-commit-id: e616af6d94a1190cffe6bda636b6dae658ffe52d

--- mrViewer/src/gui/mrvTree.cpp
+++ mrViewer/src/gui/mrvTree.cpp
@@ -1,5 +1,6 @@
 
-#include "icons/icons.c"
+#include <FL/Fl_XPM_Image.H>
+#include "FLU/flu_pixmaps.h"
 #include "gui/mrvTree.h"
 
 namespace mrv
@@ -8,7 +9,20 @@ namespace mrv
 PreferencesTree::PreferencesTree( int x, int y, int w, int h, const char* l ) :
 Fl_Tree( x, y, w, h, l )
 {
-    closeicon( closed_icon );
+    {
+	Fl_Pixmap closed_icon( folder_closed_xpm );
+	Fl_RGB_Image* img = new Fl_RGB_Image( &closed_icon );
+	openicon( img );
+    }
+
+    
+    {
+	Fl_Pixmap opened_icon( folder_open_xpm );
+	Fl_RGB_Image* img = new Fl_RGB_Image( &opened_icon );
+	closeicon( img );
+    }
+
+    
 }
 
 }
