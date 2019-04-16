0225c2ae37cbf3868043032b200b049e772ee6be
Gonzalo Garramuño <ggarra13@gmail.com>
2019-04-16 00:06:14 -0300

    RC3 changes.
    Former-commit-id: 6f262a1f7549862a3226772ce2492bd151d1abd6 [formerly afce403edf1fc2b24ccddc856f1af80ab2ec4eaf [formerly cd0f189b396229b07f5f9cdae64af7fe33f4ee13]]
    Former-commit-id: 7b3cf94d857e3af12acde3a7e8686c68fb23ac80 [formerly 7ba4dbb729f9df961ac789615b07e750a228c5b8] [formerly ae3e129a34fa915d4618ac2ff1e8554e32174027 [formerly 289b97f68221f82e3af2a8a5ae0085d99018a3a4]] [formerly 405268bc33e2dee6525f87fb16482136c8b77d53 [formerly af48add9fa99567707e1eb98a4c6ee7e8b525dd6] [formerly 7b56a9f81bf6e46868890256f4c71a44fd1587fc [formerly a5dfc15340eaf40a5700b4e142e562d2fd7914df]]]
    Former-commit-id: a30629709db3219e249b5df0202d9c69f2fb408a [formerly 196b29fc869296926a2d1eb456184d1503299971] [formerly b0f0494e96cda51456bb4e0e10e5035a1162ae3a [formerly deb0f20fef3603ce395c609fbeaa6b80af4d0f38]]
    Former-commit-id: f583a4ad316f8ee709bef01b27e1d574defdb6ad [formerly 80323e4ad9f259ea8dbc7c428e683fab90406ae5]
    Former-commit-id: d272e9138e716e36dd20e53432c003d197219d19

--- mrViewer/src/gui/mrvImageBrowser.h
+++ mrViewer/src/gui/mrvImageBrowser.h
@@ -61,121 +61,170 @@ void end_button_cb(Fl_Button* o, ViewerUI* v);
 class ImageBrowser : public Fl_Tree
 {
 public:
-    typedef std::vector< boost::thread* > thread_pool_t;
-
-    struct LThreadData
-    {
-        mrv::ImageView* view;
-
-        LThreadData( mrv::ImageView* v ) :
-            view( v )
-        {
-        }
-    };
-
-public:
     ImageBrowser(int x, int y, int w, int h);
     ~ImageBrowser();
 
+    //! Clone all channels of a media
     void clone_all_current();
+
+    //! Clone current channels of a media
     void clone_current();
+
+    //! Ask for a directory and open all files in it
     void open_directory();
+
+    //! Ask for filename and open sequence or movie file
     void open();
+
+    //! Ask for filename and open it as a stereo image for current
+    //! selected image
     void open_stereo();
+    
+    //! Ask for filename and open it as a single image
     void open_single();
+
+    //! Ask for filename and save image under it
     void save();
+
+    //! Ask for filename and save sequence under it
     void save_sequence();
+
+    //! Ask for filename and save reeel under it
     void save_reel();
-    void load_reel( const char* name );
+
+    //! Load a reel from filename
+    void load_reel( const char* filename );
+
+    //! Removes the current reel from reel list
     void remove_reel();
 
+    //! Add the media to tree.  Returns true on success, false on failure
+    bool add_to_tree( mrv::media m );
+
+    //! Returns the number of reels
     size_t number_of_reels() const {
         return _reels.size();
     }
+    //! Creates a new reel with name (or name #x if name already exists)
     mrv::Reel new_reel( const char* name = "reel" );
+    //! Returns current reel
     mrv::Reel current_reel();
+    //! Changes current reel to the one matching name
     mrv::Reel reel( const char* name );
+    //! Changes current reel to reel index
     mrv::Reel reel( unsigned int idx );
+    //! Returns the reel at a certain index, without changing the current reel
     mrv::Reel reel_at( unsigned int idx );
+    //! Returns the current selected reel index
     unsigned reel_index() { return _reel; }
 
+    //! Returns the current image being shown on gui
     mrv::media current_image();
 
+    //! Sets the Fl_Choice for the reel
     void reel_choice( mrv::Choice* c ) {
         _reel_choice = c;
     }
 
-    void attach_profile();
 
+    //! Remove current reel
     void remove_current();
 
+    //! Sets the last image as current
     void last_image();
 
+    //! Changes image to image index # i
     void change_image( int i );
 
+    //! Changes image version up or down by 1 or more.
     void image_version( int sum );
+
+    //! Change image version to previous one (if found on disk )
     void previous_image_version();
+
+    //! Change image version to next one ( if found on disk )
     void next_image_version();
 
+    //! Changes to previous image on reel
     void previous_image();
+
+    //! Changes to next image on reel
     void next_image();
 
+    //! Clears reel->images array and rebuilds it from the tree
     void match_tree_order();
 
+    //! Inserts a media at a certain index in tree
     void insert( int idx, mrv::media m );
 
-    // @todo: these should be factored to a database helper class
-#if 0
-    void add_image( const mrv::media& m );
-    void add_video( const mrv::media& m );
-    void add_audio( const mrv::media& m );
-#endif
-
+    //! Adds media to tree
     mrv::media add( const mrv::media m );
+
+    //! Adds image to tree
     mrv::media add( CMedia* img );
+    
+    //! Addes filename as media to tree
     mrv::media add( const char* filename,
                     const int64_t start = -999999,
                     const int64_t end = -999999 );
 
     void debug_images() const;
 
+    //! Loads files as media to tree
     void load( const LoadList& files, const bool stereo = false,
                std::string bgfile = "",
-	       const bool edl = false,
+               const bool edl = false,
                const bool progressBar = false );
+    
+    //! Loads files as media to tree
     void load( const stringArray& files, const bool seqs = true,
                const bool stereo = false,
                const std::string bgfile = "",
-	       const bool edl = false,
+               const bool edl = false,
                const bool progressBar = false );
 
-    
+    //! Replaces media at index idx in tree with media m.
     void replace( int idx, mrv::media m );
 
+    //! Remove media m from reel image list and tree
     void remove( mrv::media m );
+
+    //! Remove media at index idx from reel image list and tree
     void remove( int idx );
 
+    //! Refresh media's icon
     void refresh( mrv::media img );
 
+    //! Seek to a certain frame in reel EDL
     void seek( const int64_t f );
 
+    //! Sets frame to a certain frame in reel EDL
     void frame( const int64_t f );
-    
+
+    //! Handles clearing, setting and toggling EDL
     void clear_edl();
     void set_edl();
     void toggle_edl();
 
+    //! Clear bg image
     void clear_bg();
+
+    //! Set bg image to media bg
     void set_bg( mrv::media bg );
+
+    //! Change background to nothing
     void change_background();
 
+    //! Attach icc profile to current image
     void attach_icc_profile();
 
+    //! Attach ctl script to current image
     void attach_ctl_script();
 
-
+    //! Handle Drag and Drop of images into this tree's reel
     void handle_dnd();
 
+    //! Sets and returns the current index of the selected image
     void value( int idx ) { _value = idx; }
     int value() const { return _value; }
 
@@ -191,42 +240,59 @@ public:
 
     void send_image( int idx );
 
+    //! Exchange image in oldsel with image in sel
     void exchange( int oldsel, int sel );
 
 public:
+    //! Create a tree element from a media
     static mrv::Element* new_item(mrv::media img);
 
 protected:
 
+    //! Set pathname from a media
     std::string media_to_pathname( const mrv::media m );
-    
+
+    //! These functions send reel, images o current image to network
     void send_reel( const mrv::Reel& r );
     void send_images( const mrv::Reel& r);
     void send_current_image( const mrv::media& m );
 
+    //! Change reel to new one selected
     void change_reel();
+
+    //! Change image to new one selected
     void change_image();
+
+    //! Adjust timeline of media clips when in EDL mode
     void adjust_timeline();
+
+    //! Load a stereo image for fg
     void load_stereo( mrv::media& fg,
                       const char* name,
                       const int64_t first, const int64_t last,
                       const int64_t start, const int64_t end );
+    
+    //! Load an image
     mrv::media load_image( const char* name,
                            const int64_t first, const int64_t last,
                            const int64_t start, const int64_t end,
                            const bool avoid_seq = false );
 
-
+    //! Handle mouse drag
     int mouseDrag( int x, int y );
+    //! Handle mouse push
     int mousePush( int x, int y );
+    //! Handle mouse release
     int mouseRelease( int x, int y );
 
+    //! Returns GUI timeline
     mrv::Timeline* timeline();
+    
+    //! Returns GUI EDL group
     mrv::EDLGroup* edl_group() const;
 
+    //! Returns GUI view window
     mrv::ImageView* view() const;
-
-    thread_pool_t  _load_threads;    //!< loading threads if any
 
     unsigned       _reel;
     mrv::ReelList  _reels;
@@ -245,4 +311,3 @@ protected:
 
 
 #endif  // mrvImageBrowser_h
-
