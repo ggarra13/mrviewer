a0f1cf1180f235b0866cee4602436ed4e8615ea7
Gonzalo Garramuño <ggarra13@gmail.com>
2019-04-16 00:06:14 -0300

    RC3 changes.
    Former-commit-id: 6f262a1f7549862a3226772ce2492bd151d1abd6 [formerly afce403edf1fc2b24ccddc856f1af80ab2ec4eaf [formerly cd0f189b396229b07f5f9cdae64af7fe33f4ee13]]
    Former-commit-id: 3102e3ec325048f0cea3d5b2e863a6d3a159db24 [formerly 54b734932b01dae74996e0674ee93f91e3d735db] [formerly 7b3cf94d857e3af12acde3a7e8686c68fb23ac80] [formerly 7b3cf94d857e3af12acde3a7e8686c68fb23ac80 [formerly 7ba4dbb729f9df961ac789615b07e750a228c5b8]] [formerly 89f4d50ab3b13cb2784293f6f13124da6672f83c [formerly 34f756bd240540721d02eb158c7799cd894d7c9b] [formerly ae3e129a34fa915d4618ac2ff1e8554e32174027] [formerly ae3e129a34fa915d4618ac2ff1e8554e32174027 [formerly 289b97f68221f82e3af2a8a5ae0085d99018a3a4]]] [formerly c5d2b947c9a7881efb73af0d0c3431d36b65676c [formerly a1d34a8d55f000818db5d5dee67bbc5c55a7c29d] [formerly 405268bc33e2dee6525f87fb16482136c8b77d53] [formerly 405268bc33e2dee6525f87fb16482136c8b77d53 [formerly af48add9fa99567707e1eb98a4c6ee7e8b525dd6]] [formerly bfb92e40112d8bd0ba9ccc43ceae9bdd2407c60a [formerly 768298f834875320833c52828248f6f73424b007] [formerly 7b56a9f81bf6e46868890256f4c71a44fd1587fc] [formerly 868d84ef231f295e6a8a43e26e5b5c7f48c75dcb [formerly 7b56a9f81bf6e46868890256f4c71a44fd1587fc [formerly a5dfc15340eaf40a5700b4e142e562d2fd7914df]]]]]
    Former-commit-id: 7e30a59242b306cb1be21d959865c96a0c649ec1 [formerly c727fc9cbe07634a43c53305608651a6cdb2c3b8] [formerly a30629709db3219e249b5df0202d9c69f2fb408a] [formerly a30629709db3219e249b5df0202d9c69f2fb408a [formerly 196b29fc869296926a2d1eb456184d1503299971]] [formerly a475e682d614c12eaac0b803c5d572cb850c97f5 [formerly 82641b4cc64e7d74c2d1d110f51e987a10a7b2fa] [formerly b0f0494e96cda51456bb4e0e10e5035a1162ae3a] [formerly b0f0494e96cda51456bb4e0e10e5035a1162ae3a [formerly deb0f20fef3603ce395c609fbeaa6b80af4d0f38]]]
    Former-commit-id: 18b8f2151534db38f1ae883f52b334b17ed52108 [formerly 0f9922a40377fffcbbd9fc7c0da510cd89a1222d] [formerly f583a4ad316f8ee709bef01b27e1d574defdb6ad] [formerly f583a4ad316f8ee709bef01b27e1d574defdb6ad [formerly 80323e4ad9f259ea8dbc7c428e683fab90406ae5]]
    Former-commit-id: 0225c2ae37cbf3868043032b200b049e772ee6be [formerly d272e9138e716e36dd20e53432c003d197219d19]
    Former-commit-id: 0225c2ae37cbf3868043032b200b049e772ee6be
    Former-commit-id: 68e4c53736b198c89b8847521a6f324756c3d76e

--- mrViewer/src/gui/mrvDoubleWindow.cpp
+++ mrViewer/src/gui/mrvDoubleWindow.cpp
@@ -1,4 +1,7 @@
 
+#include <iostream>
+
+#include <FL/Fl.H>
 #include "gui/mrvDoubleWindow.h"
 
 mrvDoubleWindow::mrvDoubleWindow( int x, int y, int w, int h, 
@@ -14,5 +17,8 @@ Fl_Double_Window( w, h, l )
 
 bool mrvDoubleWindow::exec() 
 { 
-    set_modal(); show(); return _exec;
+    set_modal(); show();
+    while ( visible() )
+	Fl::check();
+    return _exec;
 };
