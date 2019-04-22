0be2626feb7f0e85d3d9793acc7b482a0f17b8ee
Gonzalo Garramuño <ggarra13@gmail.com>
2019-04-22 14:38:36 -0300

    Added missing files.
    Former-commit-id: 2c46f424384b83fb08fc894d60b31367039f6c77 [formerly d6b4e1fc8534ea14970fdd383afa4b4ccefc4b62 [formerly 3bea3024afbd692f3b542f4bdd4949ba2b5d3356]]
    Former-commit-id: 93b27a84125a1d5bf9fea0e551ac78c71455d034 [formerly e35850977c13b1320d14e6b6fa43e9ff1bf71eb5] [formerly da66fcdf924a12d000ea2b060fad6935d6bf93b6 [formerly 896aaebc4272dc299fa7a4ee5c1c1dbcb392b078]] [formerly 2f24e126e01e4eede0b58abbed19f8891eca2296] [formerly 2f24e126e01e4eede0b58abbed19f8891eca2296 [formerly 2f24e126e01e4eede0b58abbed19f8891eca2296 [formerly e612f83ab64fc709be550534ab4612fb69a49d56]]] [formerly 48d4abc20104016e34368b566d90b3de5efa44e3 [formerly 48d4abc20104016e34368b566d90b3de5efa44e3 [formerly 3c28127bd0bfb5f5915111ef99297fa5678e347b]] [formerly 3d1f107a7d1197b34354f5c5efc4af93ce1be68d [formerly 2fec93b095a98aba4a84f2561672b7fd052bd8c9]] [formerly f8492c6b3dd97082137bb744d6a346ad28ef170c [formerly 3d1f107a7d1197b34354f5c5efc4af93ce1be68d [formerly 2fec93b095a98aba4a84f2561672b7fd052bd8c9] [formerly 3d1f107a7d1197b34354f5c5efc4af93ce1be68d [formerly 2fec93b095a98aba4a84f2561672b7fd052bd8c9] [formerly 8d2b9d61adcf4e7c4ba8e23a0095603931ea41ee [formerly 202ccc66356ed1c914f4e34d0e4a3c21d07ba32f]]]]]]
    Former-commit-id: fc7de5242177f808afc618bedc472bc08dbd8261 [formerly 2fda567ef102eac0aeb30e2ff53086144f559e7c] [formerly 587037c26b304bb915865cb6141d89e2e5bbcc64 [formerly d33ebe41f5a58e3716eef0520bd17fbba0c13d30]] [formerly b46b5098454051f958e791d8c7c564c690434566] [formerly b46b5098454051f958e791d8c7c564c690434566 [formerly b46b5098454051f958e791d8c7c564c690434566 [formerly 4f06d87204dd8a990b0227baa5f482117ba442ae]]]
    Former-commit-id: 64ba9b88e6a773fba65444ab801f4b8985a50445 [formerly 64ba9b88e6a773fba65444ab801f4b8985a50445 [formerly e968c93829e97f53e46377cdb9c659111ab66468]]
    Former-commit-id: 64ba9b88e6a773fba65444ab801f4b8985a50445
    Former-commit-id: 4f5f8abed5113f70f9ce47385475159d9ce0fbe6 [formerly d023bc8a667554075ba8bbb0e2896937fce3bea4]
    Former-commit-id: 383d521bc6b50f022aa8f9552ea7ed3fce390729 [formerly 17c48d9643b142e94cc2527b5d58c50268b1b66c]
    Former-commit-id: 8899527922466ec4b93c134554193ba357874a65

---
+++ mrViewer/src/gui/mrvAsk.h
@@ -0,0 +1,49 @@
+
+#ifndef mrvAsk_h
+#define mrvAsk_h
+
+#include "FL/Enumerations.H"
+
+namespace mrv {
+
+
+class Fl_Widget;
+
+
+#  ifdef __GNUC__
+/* the GNUC-specific attribute appearing below in prototypes with a variable list of arguments
+ helps detection of mismatches between format string and argument list at compilation time */
+#    define __fl_attr(x) __attribute__ (x)
+#  else
+#    define __fl_attr(x)
+#  endif // __GNUC__
+
+// fl_ask() is deprecated since it uses "Yes" and "No" for the buttons,
+// which does not conform to the current FLTK Human Interface Guidelines.
+// Use fl_choice() instead with the appropriate verbs instead.
+FL_EXPORT int fl_choice(const char *q,const char *b0,const char *b1,const char *b2,...) __fl_attr((__format__ (__printf__, 1, 5)));
+
+FL_EXPORT Fl_Widget *fl_message_icon();
+extern FL_EXPORT Fl_Font fl_message_font_;
+extern FL_EXPORT Fl_Fontsize fl_message_size_;
+inline void fl_message_font(Fl_Font f, Fl_Fontsize s) {
+  fl_message_font_ = f; fl_message_size_ = s;}
+
+FL_EXPORT void fl_message_hotspot(int enable);
+FL_EXPORT int fl_message_hotspot(void);
+
+FL_EXPORT void fl_message_title(const char *title);
+FL_EXPORT void fl_message_title_default(const char *title);
+
+// pointers you can use to change FLTK to another language:
+extern FL_EXPORT const char* fl_no;
+extern FL_EXPORT const char* fl_yes;
+extern FL_EXPORT const char* fl_ok;
+extern FL_EXPORT const char* fl_cancel;
+extern FL_EXPORT const char* fl_close;
+
+}  // namespace mrv
+
+#endif
+
+
