9ff6159d749b2140e4f18dea451dc37d2266a388
Gonzalo Garramuño <ggarra13@gmail.com>
2019-04-24 18:09:49 -0300

    Bug fixed combo_tree browser and list of favorites in pulldown menu. Made retname return the path to latest file loaded (even if from DND).
    Former-commit-id: 58181856776267eadfc79197ad308b5ba03f9fe1 [formerly 1947ca233fb0d646f98ce1657c537a6963af2d42]
    Former-commit-id: b8463eedb8051c5b30412e912fc68a59a2f772e9 [formerly b8463eedb8051c5b30412e912fc68a59a2f772e9 [formerly f4d5fa0cba18469ba7a6029c19fb03dcb33478b3]] [formerly 28a8717c7f9cc22332e0aae17264f428804ef897 [formerly bd9750034c55d6ac71edcb0725021c7dc5ba4a5a]] [formerly d7ee12ff30c111cbf7a2b97b55a6af0a5e89cbcd [formerly 28a8717c7f9cc22332e0aae17264f428804ef897 [formerly bd9750034c55d6ac71edcb0725021c7dc5ba4a5a] [formerly 28a8717c7f9cc22332e0aae17264f428804ef897 [formerly bd9750034c55d6ac71edcb0725021c7dc5ba4a5a] [formerly caa7bf75f409f03b3256ccc800db78eaadbf3bd5 [formerly 9f4cf90ee4efcae8acec6e2f571abed01e472331]]]]]
    Former-commit-id: 39999701209bb3f4ee236a0bef6af61b742cdbe3 [formerly 39999701209bb3f4ee236a0bef6af61b742cdbe3 [formerly 6c1ca6856fc4d40169116daa1e131366bbae23ea]]
    Former-commit-id: 39999701209bb3f4ee236a0bef6af61b742cdbe3
    Former-commit-id: 4b13fc4e0dade00227cea6131e3009bfea2c595d [formerly d30bc8abd820996d31a781486f62a6f76c043e77]
    Former-commit-id: e9f600693089798242c55d2f8ad14ad4f9f25133 [formerly dd2367cfdb2700f0b17042308e80d70d338ef724]
    Former-commit-id: 3feb8422beae7e93a37bef4b212877cb3c5ea92e

--- mrViewer/src/gui/FLU/Flu_Combo_Tree.h
+++ mrViewer/src/gui/FLU/Flu_Combo_Tree.h
@@ -1,14 +1,14 @@
 // $Id: Flu_Combo_Tree.h,v 1.6 2004/03/24 02:49:00 jbryan Exp $
 
 /***************************************************************
- *                FLU - FLTK Utility Widgets 
+ *                FLU - FLTK Utility Widgets
  *  Copyright (C) 2002 Ohio Supercomputer Center, Ohio State University
  *
  * This file and its content is protected by a software license.
  * You should have received a copy of this license with this file.
  * If not, please contact the Ohio Supercomputer Center immediately:
  * Attn: Jason Bryan Re: FLU 1224 Kinnear Rd, Columbus, Ohio 43212
- * 
+ *
  ***************************************************************/
 
 
@@ -17,7 +17,7 @@
 #define _FLU_COMBO_TREE_H
 
 #include "FLU/Flu_Combo_Box.h"
-#include "FLU/Flu_Tree_Browser.h"
+#include "FL/Fl_Tree.H"
 
 //! Just like the Fl_Choice widget except the input area is editable and it can display a tree instead of a list (using Flu_Tree_Browser)
 class FLU_EXPORT Flu_Combo_Tree : public Flu_Combo_Box
@@ -32,14 +32,14 @@ public:
   ~Flu_Combo_Tree();
 
   //! Publicly exposed tree widget (instance of Flu_Tree_Browser)
-  Flu_Tree_Browser tree;
+  Fl_Tree tree;
 
  protected:
-
+  
   bool _value( const char *v );
   const char* _next();
   const char* _previous();
-  void _hilight( int x, int y );
+  void _hilight( int event, int x, int y );
 
   inline static void _cb( Fl_Widget *w, void *arg )
     { ((Flu_Combo_Tree*)arg)->cb(); }
