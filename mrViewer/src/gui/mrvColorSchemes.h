4faf334a4959b72f8582376d6f9cc30b909198df
Gonzalo Garramuño <ggarra13@gmail.com>
2019-05-09 00:21:52 -0300

    Prettyfied interface some more. Added hotkeys to reset changes and changed gain to work in exposure.
    Former-commit-id: b26c60c9df09c26e94539259215be689907199c5 [formerly 7ac34ac9a675426d96df1a90486533b77f3c1201]
    Former-commit-id: 1f0df1ee215ac4d3e017ab3eca8364917b7b1f6e

--- mrViewer/src/gui/mrvColorSchemes.h
+++ mrViewer/src/gui/mrvColorSchemes.h
@@ -12,14 +12,16 @@ namespace mrv {
     class ColorSchemes
     {
     protected:
-	std::string filename;
+        std::string filename;
         std::string remove_comments( std::string line );
     public:
+        std::string name;
+
         struct Theme
         {
             Theme( std::string& n ) : name( n ) { }
             Theme( const Theme& t ) : name( t.name ),
-				      colormaps( t.colormaps ) { }
+                                      colormaps( t.colormaps ) { }
 
             std::string name;
             typedef std::map< int, int > colorMap;
@@ -33,7 +35,7 @@ namespace mrv {
         bool read_themes( const char* file );
         bool read_colors( FILE* f, Theme& scheme );
         void apply_colors( std::string name );
-	void reload_theme( std::string name );
+        void reload_theme( std::string name );
 
     };
 
