75bd59c4c4b63720a516251854f5d69e0b244f3e
Gonzalo Garramuño <ggarra13@gmail.com>
2019-05-09 00:21:52 -0300

    Prettyfied interface some more. Added hotkeys to reset changes and changed gain to work in exposure.
    Former-commit-id: b26c60c9df09c26e94539259215be689907199c5 [formerly 7ac34ac9a675426d96df1a90486533b77f3c1201]
    Former-commit-id: dbe851660aa7b14358ef503a9ae4d1fb8cbaed55 [formerly bd9c2136fab2c96e85f55ad68b45458127d3a558] [formerly 9f87545082c45a62a10ff10b106064ca7a9035a2 [formerly 2164d10fb33988cc325f4b76a3524943a94b66d2]] [formerly 2330a16d65f42a703a62c2b141d41f7a82bf5b64] [formerly 2330a16d65f42a703a62c2b141d41f7a82bf5b64 [formerly 2330a16d65f42a703a62c2b141d41f7a82bf5b64 [formerly 007411a19148b197e40b0841f63de9529f436a7a]]] [formerly 8bb28ec80a1aab9590c131c41731e8019659c210 [formerly 8bb28ec80a1aab9590c131c41731e8019659c210 [formerly 38a897a5566062b0f002a7d2d67de77a2ea872a3]] [formerly d1aea7e32998ac0a4fb6c68cc9021e7a9af082ea [formerly 8ff18b218a84990e00afc0807c115f488352588f]] [formerly 536b3f76e51f213f16836b963a7033f26c1a8ba5 [formerly d1aea7e32998ac0a4fb6c68cc9021e7a9af082ea [formerly 8ff18b218a84990e00afc0807c115f488352588f] [formerly d1aea7e32998ac0a4fb6c68cc9021e7a9af082ea [formerly 8ff18b218a84990e00afc0807c115f488352588f] [formerly e9012e5eefbb036ea171b023552c0952c43bfeff [formerly 1f0df1ee215ac4d3e017ab3eca8364917b7b1f6e]]]]]]
    Former-commit-id: 1308851602f1c6d4e55817e8705e4310b80a3c0e [formerly e3ee909f14f31956eeab2b5d16e1759adb1fccdc] [formerly 96e895044d09706077b60d5374dc2ab3baa9b9d6 [formerly 44cc8687c82e219811b5ef0241cad5d7d3daa465]] [formerly a359b5498b3850c8ed9dfda2053c8f79a5043b4d] [formerly a359b5498b3850c8ed9dfda2053c8f79a5043b4d [formerly a359b5498b3850c8ed9dfda2053c8f79a5043b4d [formerly a3e3852f482a2b7d0d17d7b516dd92913b761de9]]]
    Former-commit-id: 2a52813eb116cbfa57d288142a8d935fd4152ac7 [formerly 2a52813eb116cbfa57d288142a8d935fd4152ac7 [formerly b87c8a3ce7389e4948651fa2ca67b18980e73b02]]
    Former-commit-id: 2a52813eb116cbfa57d288142a8d935fd4152ac7
    Former-commit-id: b4869225b348a9336439f99a568030e0dbb7c99f [formerly 49bdd028d85f56cfcc9eb30c86d88cd2117adce4]
    Former-commit-id: daca080d664637e1bb60758e9427ddab493663cb [formerly 35cce1b3e392a743f465af430c8e03a475c99885]
    Former-commit-id: c49499c33be176ea4db28388ba18b484c69b8cce

--- mrViewer/src/gui/mrvSlider.h
+++ mrViewer/src/gui/mrvSlider.h
@@ -5,6 +5,7 @@
 #define mrvSlider_h
 
 #include "core/mrvRectangle.h"
+#include "gui/mrvPreferences.h"
 #include <FL/Fl_Slider.H>
 
 namespace mrv {
@@ -16,7 +17,7 @@ class Slider : public Fl_Slider
     kNORMAL = 0,
     kLOG    = 1,
     };
-    
+
     enum Ticks
     {
     TICK_ABOVE = 1,
@@ -26,30 +27,30 @@ class Slider : public Fl_Slider
     };
 
   public:
-    
+
     Slider( int x, int y, int w, int h, const char* l = 0 ) :
     Fl_Slider( x, y, w, h, l ),
     _slider_type( kNORMAL ),
     _tick_color( FL_BLACK ),
     tick_size_( 4 )
     {
-	type( FL_HORIZONTAL );
+        type( FL_HORIZONTAL );
     }
 
     bool log() const { return _slider_type & kLOG; }
 
-    
+
     inline void ticks( Ticks t ) { _ticks = t; }
     inline Ticks ticks() const { return _ticks; }
-    
+
     inline int tick_size() const { return tick_size_; }
     inline void tick_size(int i) { tick_size_ = i; }
 
     inline void tick_color( Fl_Color c ) { _tick_color = c; }
-    
+
     inline SliderType slider_type() const { return _slider_type; }
     inline void slider_type( enum SliderType x ) { _slider_type = x; }
-    
+
     virtual void draw();
     virtual int handle(int e);
 
@@ -57,7 +58,7 @@ class Slider : public Fl_Slider
     double position_value( int X, int w );
     int slider_position( double p, int w );
     void draw_ticks( const mrv::Recti& r, int min_spacing );
-    
+
     SliderType _slider_type;
     Ticks      _ticks;
     Fl_Color   _tick_color;
