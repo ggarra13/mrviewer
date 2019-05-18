0fc9d34a9ba9b43fec64ef859f5d8a1b0ab7eb21
Gonzalo Garramuño <ggarra13@gmail.com>
2019-05-17 23:33:45 -0300

    Bug fixed memory assertion in update_cache_pic. Bug fixed file chooser crashing.
    Former-commit-id: 2c67d106618ac118bfb095536f1449bc0cd4bece [formerly ac20dc273ec8a079080aaeda65ed229919e59008]
    Former-commit-id: 30d0da2ea168c554d6a7741a69872c10e4a64421 [formerly 05545034d20a9e0c43516623955b965a2c19093b] [formerly b7e865cd25208aa5f1e4cc3c459a99188e374332 [formerly 2aab0a534e2955f1d39e06e0209860ecb5073f5a]] [formerly e85822e57c90e6d94d78cd2888e473469e3d419d] [formerly e85822e57c90e6d94d78cd2888e473469e3d419d [formerly e85822e57c90e6d94d78cd2888e473469e3d419d [formerly c50a90d41dc965573a8e2ed0c482aeaee36f67c9]]] [formerly 1194cda58b59f3dde33959da107191f2bd6df98a [formerly 1194cda58b59f3dde33959da107191f2bd6df98a [formerly 7f85f77a9ed437dc641a966ec59ea61bfe9e979a]] [formerly bca1790b5744252ac27cb3a5bfe43aea98c1f310] [formerly 0bb2ef3a45c58cd716eac4b49008d33778bee107 [formerly bca1790b5744252ac27cb3a5bfe43aea98c1f310 [formerly bca1790b5744252ac27cb3a5bfe43aea98c1f310 [formerly 8911448778381c5da23f04388cb26efbab04460e]]]]]
    Former-commit-id: 030e9fed276ff313d535f66d7b71611f011f9193 [formerly 030e9fed276ff313d535f66d7b71611f011f9193 [formerly fa1306ab2902ca66647b1922d8f1a634dc032d67]] [formerly 57d910470a08d74f285bbd9beb4ff58ce888d064] [formerly 57d910470a08d74f285bbd9beb4ff58ce888d064 [formerly 57d910470a08d74f285bbd9beb4ff58ce888d064 [formerly a99f353886f33dd25b8fa93d9a86159adf7c257b]]]
    Former-commit-id: e52ef03dbeab834c288e83fb9d25d4ed98c0d3e1 [formerly e52ef03dbeab834c288e83fb9d25d4ed98c0d3e1 [formerly 4841b3bed8630f491e46b932ed2742f9a4e47d93]]
    Former-commit-id: e52ef03dbeab834c288e83fb9d25d4ed98c0d3e1
    Former-commit-id: cb1dd753ea34d70370da8d4189f31f379d26cfb4 [formerly bab2648215d196d551ddb2af7b5ea450539dfd4f]
    Former-commit-id: 9a43adadcc9b3b4cc66a9862d0136b82eca3bd99

--- mrViewer/src/gui/mrvImageView.h
+++ mrViewer/src/gui/mrvImageView.h
@@ -920,7 +920,6 @@ protected:
     // Popup menu
     ///////////////////
     Fl_Menu_Button* menu;
-    Fl_Menu_Button* dummy_menu;  // workaround fltk bug with menus
     
     // Event Timeout
     mrv::Event*  _timeout;
