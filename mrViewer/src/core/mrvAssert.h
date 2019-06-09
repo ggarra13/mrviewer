a492a6ed0332c66127256a2058d608b9bb11be84
Gonzalo Garramuño <ggarra13@gmail.com>
2019-06-09 09:16:31 -0300

    Moved assert0 to mrvAssert.h. Fixed all alpha channels in YUV formats. Fixed 16 bits alpha channel. Fixed display of MrViewer as xclass to show it in GNOME's toolbar.
    Former-commit-id: 28d1df84ec1b82871f0188771c2800394a909ccd [formerly 4e17ca8678c7ddc50e302c93451f1a2a37b4998f [formerly 5b70636608a79602b5d0cce85ec2d4d7d3c1ef28]]
    Former-commit-id: 221c3a0daaa57356f3e03940b895eb21bdff2b98 [formerly e93fef138d0e5a78cd9d2f6bc13856a4e97a3717] [formerly dc50da0e51dcef1b6ded1284706f4ba93699b629 [formerly 3d997fe9cb89ebe38658d93bebe9c358be6e7455]] [formerly 3c6c14426507d98f58fec8a2482d425c8d1eb6b0] [formerly 3c6c14426507d98f58fec8a2482d425c8d1eb6b0 [formerly 3c6c14426507d98f58fec8a2482d425c8d1eb6b0 [formerly 7ac261f3d7f44eb59799c80706e64481a86a5221]]] [formerly 0c58220a7e576051cabae7b62382e34e31a704f9 [formerly f4819afc8912915ad13d265594cab04022a0c8de] [formerly deb93f0ed09a70cd9d2bf3c7bc30df3eedabba8b [formerly 43323cb8fb1b5bb294105d4b74c6d4d083feedd5]] [formerly ffc755f7189df1ebbe204614ea54d8468b5e4f9b] [formerly ffc755f7189df1ebbe204614ea54d8468b5e4f9b [formerly ffc755f7189df1ebbe204614ea54d8468b5e4f9b [formerly e86648b132dc580d6b6e3679a31407c91446dfc9]]]] [formerly 261e2111403dca554721ed1aa6e85853a1843ae9 [formerly ca13ad74041af084f0f78093ac96cd2d7620180c] [formerly cf106edfdf8e0436ce94e909120f6b6df3a53be9 [formerly cd1c0a26efb4e9607d88674e2cd3fc980d9ee35f]] [formerly f731ba850e53c21f7badb428bf35c55ae9056d97] [formerly f731ba850e53c21f7badb428bf35c55ae9056d97 [formerly f731ba850e53c21f7badb428bf35c55ae9056d97 [formerly 2f1eb6aa07e1d910a5075889255f4bff8299abb1]]] [formerly 1c0eaa676c67235404d84a38b8c281dc0bdd327f [formerly 1c0eaa676c67235404d84a38b8c281dc0bdd327f [formerly 53fbc3d7b0572537c18302703e1c5ec666a14ed4]] [formerly a92f5da08c7e3d571f2d16b93eb2132d37b8d13b [formerly 4c611f2e1ab229b3ef57b06144db6bdf2bb5f8d1]] [formerly 7ddc0caa67b2bc96a2edb42089bf50421b88b52d [formerly a92f5da08c7e3d571f2d16b93eb2132d37b8d13b [formerly 4c611f2e1ab229b3ef57b06144db6bdf2bb5f8d1] [formerly a92f5da08c7e3d571f2d16b93eb2132d37b8d13b [formerly 4c611f2e1ab229b3ef57b06144db6bdf2bb5f8d1] [formerly 9d872e49c0c945480b2c7f8a390b134032fe948c [formerly 2fe047243313dd92f469f8f6c4dd8e2e0f233de9]]]]]]]
    Former-commit-id: a20516adb25f1b69ad43c1792c18523c015241c2 [formerly 3642e3ab5a98e2fb0a79e1b89dcc499ba95f8528] [formerly f6f2fbe4e5b2356a2a3771c66296793bc22a77e6 [formerly 5694329b166adc3c20ee1961444b9104dcef74b3]] [formerly df2b63e0fe3ec93b7070338ee7ede39025eb0ee0] [formerly df2b63e0fe3ec93b7070338ee7ede39025eb0ee0 [formerly df2b63e0fe3ec93b7070338ee7ede39025eb0ee0 [formerly 69e406e8f01014d10533c21a2ddbe8e5a04a306d]]] [formerly 6c85a50ac7a0991c978945f11ac79df75a2d1b7e [formerly fec0e4e5d1609d442420347db1d6421d82c213b4] [formerly 1a2e356b0d21de10cbb004a6c771d0bf0f814198 [formerly 71aeea2b3e8037acfcfc03fbf0905df1cf85999d]] [formerly d90f4c69a2bd6aa5ca0d6724e2c090a47feaba48] [formerly d90f4c69a2bd6aa5ca0d6724e2c090a47feaba48 [formerly d90f4c69a2bd6aa5ca0d6724e2c090a47feaba48 [formerly aef1c1e72ce0310ca97698fde69d2697977df3cc]]]]
    Former-commit-id: 893b0235724ea9b4e79918a611c420828b33d0c9 [formerly b9dc4e06709d8aab0330bbde0ad424b1e39411c5] [formerly f2ce66fc31a75b06e9e08836de4d57964e919c97 [formerly 6b8fcf736234707eaa79ddac3daa2cfb53081b2e]] [formerly 71163d01902279c7a98091f5ca84d634a744aa35] [formerly 71163d01902279c7a98091f5ca84d634a744aa35 [formerly 71163d01902279c7a98091f5ca84d634a744aa35 [formerly f2a77a0edb2119c884742785f62557e37abffbb9]]]
    Former-commit-id: 54eb39b67e4cd4ad4bffd7e9e06a49ee8454fb04 [formerly 54eb39b67e4cd4ad4bffd7e9e06a49ee8454fb04 [formerly dddbdbaab7caeeb8ce61d290c0839df6bfb19cdd]]
    Former-commit-id: 54eb39b67e4cd4ad4bffd7e9e06a49ee8454fb04
    Former-commit-id: 8c7dbbff8dcd39049daaca18a544ff1a608e5449 [formerly 0ff0d2aa3406cd5937214efb93e05fc6227fa9a1]
    Former-commit-id: dc0147f29b1e92d0361522b001ef867806a7af43 [formerly 7b853eabc789303768def83001874b281a8489af]
    Former-commit-id: c202d6c62c5bc5cd090ec2260e5b6ecf867385c7

---
+++ mrViewer/src/core/mrvAssert.h
@@ -0,0 +1,44 @@
+/*
+    mrViewer - the professional movie and flipbook playback
+    Copyright (C) 2007-2014  Gonzalo Garramuño
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file   mrvAssert.h
+ * @author gga
+ * @date   Sun Jul 15 08:41:02 2007
+ *
+ * @brief   assertion routines
+ *
+ *
+ */
+
+#ifndef mrvAssert_h
+#define mrvAssert_h
+
+
+/**
+ * assert0() equivalent, that is always enabled.
+ */
+#define assert0(cond) do {                                            \
+    if (!(cond)) {                                                       \
+        std::cerr << "Assertion " << #cond << " failed at " \
+                  << __FILE__ << ":" << __LINE__ << std::endl;           \
+        ::abort();                                                      \
+    }                                                                    \
+} while (0)
+
+
+#endif // mrvAssert_h
