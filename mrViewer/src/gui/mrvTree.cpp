4fdaf5987544e1e85f72461d6f20b230fc32d96c
Gonzalo Garramuño <ggarra13@gmail.com>
2019-06-15 21:48:14 -0300

    Removed minor memory leak from PreferencesTree. Removed memory leak from Preferences::run (directory listing). Removed memory leak from unneeded thumbnail copy.
    Former-commit-id: aef0ad95afbd4f1e34dd130f8113be5aba4e0483
    Former-commit-id: 49fdcf3e1136cc5d929c8d82320ab2fae052f2eb [formerly 8cca4487b98b32ebf5a161e92e7883034eb82119 [formerly af9d0643643f4a542c42753e9dd77e5851b206d6] [formerly 38640db51b329924b326a9461905ba6cc929ff3d [formerly fb44149960ae8217b0e88434301a30739e3769fc]]]
    Former-commit-id: 8cca4487b98b32ebf5a161e92e7883034eb82119 [formerly af9d0643643f4a542c42753e9dd77e5851b206d6]
    Former-commit-id: bcd2c53ab2d4a716a9ae12f5c2de3aa87dbf733c
    Former-commit-id: 05ae12459745b538031e24f7c793702034f03cd8

--- mrViewer/src/gui/mrvTree.cpp
+++ mrViewer/src/gui/mrvTree.cpp
@@ -1,4 +1,3 @@
-
 #include <FL/Fl_XPM_Image.H>
 #include "FLU/flu_pixmaps.h"
 #include "gui/mrvTree.h"
@@ -10,19 +9,24 @@ PreferencesTree::PreferencesTree( int x, int y, int w, int h, const char* l ) :
 Fl_Tree( x, y, w, h, l )
 {
     {
-	Fl_Pixmap closed_icon( folder_closed_xpm );
-	Fl_RGB_Image* img = new Fl_RGB_Image( &closed_icon );
-	openicon( img );
+        Fl_Pixmap closed_icon( folder_closed_xpm );
+        Fl_RGB_Image* img = new Fl_RGB_Image( &closed_icon );
+        openicon( img );
     }
 
-    
+
     {
-	Fl_Pixmap opened_icon( folder_open_xpm );
-	Fl_RGB_Image* img = new Fl_RGB_Image( &opened_icon );
-	closeicon( img );
+        Fl_Pixmap opened_icon( folder_open_xpm );
+        Fl_RGB_Image* img = new Fl_RGB_Image( &opened_icon );
+        closeicon( img );
     }
 
-    
 }
+
+    PreferencesTree::~PreferencesTree()
+    {
+        delete openicon();
+        delete closeicon();
+    }
 
 }
