9adbaa3add157eb797b50cac83ea4e11b466eeb3
Gonzalo Garramuño <ggarra13@gmail.com>
2019-07-15 11:06:51 -0300

    Bug fixed playback of movie once loop is discarded.
    Former-commit-id: 5128813bb0d090e5f2ca20de82b8691f9da72585 [formerly 6c19df7c6ac41dc65d97c39815ce8935ddd3d922]
    Former-commit-id: b8b4076c5f378ce785a65106720e2cd0f33aba08 [formerly d2a5768766f6b1c8db3ee7704dd893ef96513b82] [formerly 1d88cab8dca909cc01da8d981ac7badbd4cb60b6 [formerly 766abb1f0cdc2d1e90aeac283195c244d4767d45]] [formerly d9c513014c751827e0c5c861481f7a6fbb5f04b9 [formerly 1d88cab8dca909cc01da8d981ac7badbd4cb60b6 [formerly 766abb1f0cdc2d1e90aeac283195c244d4767d45] [formerly 7bde43cc2c57ed0359c7e0b1505fd48b17b7eb3f [formerly 4c3e9bb238ad227913d9531601ceaa43ae7617c4]]]]
    Former-commit-id: ac8922a129d5d55e78d5e751e906d1ee15e830b5 [formerly 23998dd4b3ea81b3a08809984f71bc3a8e009cff]
    Former-commit-id: ac8922a129d5d55e78d5e751e906d1ee15e830b5
    Former-commit-id: 18717168a2d9a4813d48ac08414c9e50cabe5a67
    Former-commit-id: 01f660f42d6179a18d42cb14b3c3b8bb8fe95d7f

--- mrViewer/src/core/aviImage.cpp
+++ mrViewer/src/core/aviImage.cpp
@@ -3153,7 +3153,6 @@ bool aviImage::fetch(mrv::image_type_ptr& canvas, const int64_t frame)
 
     int64_t f = handle_loops( frame );
 
-
     if ( ( got_audio || in_audio_store( f + _audio_offset ) ) &&
             in_video_store( f ) )
     {
@@ -3162,13 +3161,14 @@ bool aviImage::fetch(mrv::image_type_ptr& canvas, const int64_t frame)
         pts = frame2pts( get_audio_stream(), f );
         if ( !got_audio ) _audio_packets.jump( pts );
         _dts = _adts = f;
-        _expected = _dts + 1;
-        _expected_audio = _dts + 1;
-        //_expected = -99999;
-        //_expected_audio = -99999;
+        //_expected = _dts + 1;
+        //_expected_audio = _dts + 1;
+        _expected = -99999;
+        _expected_audio = -99999;
 
         return true;
     }
+
 
     if ( (!got_video || !got_audio || !got_subtitle) && f != _expected  )
     {
@@ -3548,16 +3548,6 @@ CMedia::DecodeStatus aviImage::decode_video( int64_t& f )
 {
     int64_t frame = f;
 
-    // Early exit if we are past loop ends
-    if ( f > _frameEnd )
-    {
-        return kDecodeLoopEnd;
-    }
-    else if ( f < _frameStart )
-    {
-        return kDecodeLoopStart;
-    }
-
     if ( !has_video() )
     {
         return audio_video_display(_audio_frame);
@@ -3626,7 +3616,6 @@ CMedia::DecodeStatus aviImage::decode_video( int64_t& f )
 
             if ( frame < _frameStart )
             {
-                assert( !_video_packets.empty() );
                 _video_packets.pop_front();
                 return kDecodeLoopStart;
             }
@@ -3645,27 +3634,21 @@ CMedia::DecodeStatus aviImage::decode_video( int64_t& f )
             }
 
 
-            assert( !_video_packets.empty() );
             _video_packets.pop_front();
             return kDecodeLoopEnd;
         }
         else if ( _video_packets.is_jump() )
         {
-            assert( !_video_packets.empty() );
             _video_packets.pop_front();
             return kDecodeOK;
         }
         else
         {
-            assert( !_video_packets.empty() );
             AVPacket& pkt = _video_packets.front();
 
-            int64_t pktframe;
-            if ( pkt.dts != AV_NOPTS_VALUE )
-            {
-                pktframe = get_frame( get_video_stream(), pkt );
-            }
-            else
+            int64_t pktframe = get_frame( get_video_stream(), pkt );
+
+            if ( pktframe == AV_NOPTS_VALUE )
             {
                 pktframe = frame;
             }
@@ -3685,14 +3668,12 @@ CMedia::DecodeStatus aviImage::decode_video( int64_t& f )
                 // if ( pktframe == frame )
                 {
                     got_video = decode_vpacket( pktframe, frame, pkt );
-                    //assert( !_video_packets.empty() );
                     _video_packets.pop_front();
                 }
                 continue;
             }
 
             got_video = decode_image( pktframe, pkt );
-            //assert( !_video_packets.empty() );
             _video_packets.pop_front();
             continue;
         }
