74af7a18d24e9866b120e3ab0ef4c98b2c7b3978
Gonzalo Garramuño <ggarra13@gmail.com>
2019-08-07 20:47:27 -0300

    Made Builds work for current tarred/git objects. Made calculate_fps work correctly. Made saving of movies with audios not add pops or noise to audio.
    Former-commit-id: c6ac204cb18faea799cacfa4e3ebdf4f01c0a269 [formerly 9bd6bbb9dfa3fc560ffadbf6793befd1175e1917]
    Former-commit-id: 520c51e08adf967f62dcba4137a0676df8910d0f [formerly cb7a7ee38f5dc0e858ddba32b36cc7f2b9a3a22d] [formerly 3f35f05d9ac85e55ea5ed8d164eabc55c4055c34 [formerly 57c819f2e20b6263dd723440c53647eb658cbe33]] [formerly d090d977b3a46403ad3c21c8784399e1938574c7 [formerly 3f35f05d9ac85e55ea5ed8d164eabc55c4055c34 [formerly 57c819f2e20b6263dd723440c53647eb658cbe33] [formerly d5c999e467a93c0933521dcfd93e76a2811c791a [formerly 5633cff424514bd75a2118b93df305499b1fba90]]]]
    Former-commit-id: b1e5547ad887d0272ed758a6e1d6292f4914d124 [formerly 464925517adc9a86c569461f6097c09e76b31321]
    Former-commit-id: b1e5547ad887d0272ed758a6e1d6292f4914d124
    Former-commit-id: bc41a3c8c37ee514df517d08c4cf3f45ab5ebd5c [formerly 9264d552ae93b4d782396deb926e40ea481fec42]
    Former-commit-id: 85d5cc7d8a1e57ebba671fce4eca72f95609b1fb

--- external/BuildLibPNG.cmake
+++ external/BuildLibPNG.cmake
@@ -1,12 +1,17 @@
+if (WIN32)
+  set( generator "NMake Makefiles" )
+else()
+  set( generator "Unix Makefiles" )
+endif()
+
 ExternalProject_Add(
   LIBPNG
-  GIT_REPOSITORY "https://github.com/glennrp/libpng.git"
-  DEPENDS ZLIB
+  URL "https://sourceforge.net/projects/libpng/files/libpng16/1.6.37/libpng-1.6.37.tar.gz/download"
+  CMAKE_GENERATOR ${generator}
   CMAKE_ARGS
   -DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
   -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
-  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
   -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
   )
