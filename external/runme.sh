d96b86cbdddd43142bc44ed858ffca23b521c039
Gonzalo Garramuño <ggarra13@gmail.com>
2019-08-07 20:47:27 -0300

    Made Builds work for current tarred/git objects. Made calculate_fps work correctly. Made saving of movies with audios not add pops or noise to audio.
    Former-commit-id: 9bd6bbb9dfa3fc560ffadbf6793befd1175e1917
    Former-commit-id: d090d977b3a46403ad3c21c8784399e1938574c7 [formerly d5c999e467a93c0933521dcfd93e76a2811c791a [formerly 5633cff424514bd75a2118b93df305499b1fba90]]
    Former-commit-id: 3f35f05d9ac85e55ea5ed8d164eabc55c4055c34 [formerly 57c819f2e20b6263dd723440c53647eb658cbe33]
    Former-commit-id: 4f91c7d446d2bb28e814a72014d109ee561674a9

--- external/runme.sh
+++ external/runme.sh
@@ -3,9 +3,12 @@
 KERNEL=`uname`
 RELEASE=`uname -r`
 installdir=$PWD/../install-$KERNEL-$RELEASE
+echo "INSTALLDIR = " $installdir
 if [[ $KERNEL == Linux ]]; then
+    copy='sudo cp'
     finaldir=/usr/local
 else
+    copy='cp'
     finaldir=/D/code/lib/vc14_Windows_$RELEASE
 fi
 
@@ -17,6 +20,6 @@ if [[ $RELEASE == 3* ]]; then
 fi
 
 export PATH=$installdir/bin:$PATH
-export LD_LIBRARY_PATH=$installdir/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=$installdir/lib:$LD_LIBRARY_PATH
 
-../mk --installdir=$installdir && echo "Install in /usr/local.  Needs root permissions" && sudo cp -r $installdir/* $finaldir
+../mk --installdir=$installdir -j 1 && echo "Install in /usr/local.  Needs root permissions" && $copy -r $installdir/* $finaldir
