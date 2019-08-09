959c9be02c0b99cae621f8225c5ffcc6d83cd866
Gonzalo Garramuño <ggarra13@gmail.com>
2019-08-08 23:10:34 -0300

    Updated OCIO, OIIO and CTL to build on Windows 64 bits.  Allowed image information to change the duration of a single image. Fixed audio saving in movie files.
    Former-commit-id: ff48cff269d734ea2364c4d1743b8883cce739d4 [formerly a8e6197dfe9dfaa9648c515c70a8eb629f6eefdb]
    Former-commit-id: 886d95d41bb3c1d7ee6eb4f2469699bcdc4ddffe [formerly c3a790805a74f8afae661f2f02558fbb002ab142] [formerly 886d95d41bb3c1d7ee6eb4f2469699bcdc4ddffe [formerly c3a790805a74f8afae661f2f02558fbb002ab142] [formerly d181c22a7d0bb125db76dba10badff94920d34d3 [formerly 9869b36b91dac56fc8b14f5e6144add26dea16a8]]]
    Former-commit-id: 078d18960e58f784d2a63e6d2fdeb01630c1398f [formerly 4cb8660ee2c2f2fdf1c429e6891066cd7f1c0b5d]
    Former-commit-id: 2dfef759c09f99fd419a6c79849c70d78643d78f [formerly 29553978539f3d5fad69fba06be4121314521122]
    Former-commit-id: 72a17ba68bdff652ba346f69884092c966c9073c

--- external/BuildOIIO.cmake
+++ external/BuildOIIO.cmake
@@ -1,7 +1,9 @@
 ExternalProject_Add(
   OIIO
-  URL "https://github.com/OpenImageIO/oiio/archive/release.zip"
-  DEPENDS OCIO OpenEXR ${LibRaw} ${LibWebP}
+  #  URL "https://github.com/OpenImageIO/oiio/archive/release.zip
+  GIT_REPOSITORY "https://github.com/OpenImageIO/oiio.git"
+  GIT_PROGRESS 1
+  DEPENDS OpenEXR ${LibRaw} ${LibWebP} OCIO
   CMAKE_ARGS
   -DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
@@ -10,4 +12,5 @@ ExternalProject_Add(
   -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
   -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
   -DBUILD_SHARED_LIBS=ON
+  -DUSE_PYTHON=OFF
   -DOIIO_BUILD_TESTS=OFF)
