6a1d3ca3b8d4899f095ff62af8bea4b414752f8e
Gonzalo Garramuño <ggarra13@gmail.com>
2019-08-10 19:04:52 -0300

    Updated docs.  Fixed playback of single pictures.  Fixed saving of audio for up to 7.1 channels with aac.  Updated mrvColorOps to new OCIO classes (not used as yet).
    Former-commit-id: 3106aaa7384143714839b9d81690d835f1f3875f [formerly 729852b91d2e6af129fd0fd1b548966232e2d038 [formerly d35b5047b9289b31900b21f943fb855f59983e04]]
    Former-commit-id: bd9aa50fee4480d01325cbe8e1c0f5bd66a9ec07 [formerly b15ec76b79ea6331cb043c10de7137fe04013054] [formerly 7decefa28149eaa9eccc4b92bbf7ac005af28427 [formerly db4d9b412744b0c37cf783dfc6f295544f7f4045] [formerly 769ad093779e6e06d9b0a2560025ef4df958fdf5 [formerly c743f15dc1c3ba27a919ee06dc6cf5ac56758568]]]
    Former-commit-id: 29f5372eb3ef706c12e49319ee98dd84f30579f3 [formerly 8a61d7d159b87e6432e2e113ac8040cf1e7ce749]
    Former-commit-id: 1a5b77bf5655c35b75859fa0042e3846d5826c6f [formerly 599bd13f3ee05cd4fd1452ba5d2508b052d6d6a6]
    Former-commit-id: 5d71e60339e694933e957ec14be51dc5f7b7ae7d

--- external/BuildOIIO.cmake
+++ external/BuildOIIO.cmake
@@ -1,16 +1,20 @@
+if(UNIX)
+  string( JOIN " " cxx_flags ${CMAKE_CXX_FLAGS} -Wno-error=deprecated-declarations  )
+endif()
+
 ExternalProject_Add(
   OIIO
-  #  URL "https://github.com/OpenImageIO/oiio/archive/release.zip
+  # URL "https://github.com/OpenImageIO/oiio/archive/release.zip"
   GIT_REPOSITORY "https://github.com/OpenImageIO/oiio.git"
   GIT_PROGRESS 1
-  DEPENDS OpenEXR ${LibRaw} ${LibWebP} OCIO
+  DEPENDS OpenEXR LIBPNG OCIO ${LibRaw} ${LibWebP}
   CMAKE_ARGS
   -DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
   -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
   -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-  -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+  -DCMAKE_CXX_FLAGS=${cxx_flags}
   -DBUILD_SHARED_LIBS=ON
   -DUSE_PYTHON=OFF
   -DOIIO_BUILD_TESTS=OFF)
