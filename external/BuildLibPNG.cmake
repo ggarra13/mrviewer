d1c3d4197fad8ff4ecdf8fbd5fbeb62dd00016c9
Gonzalo Garramuño <ggarra13@gmail.com>
2019-08-10 19:04:52 -0300

    Updated docs.  Fixed playback of single pictures.  Fixed saving of audio for up to 7.1 channels with aac.  Updated mrvColorOps to new OCIO classes (not used as yet).
    Former-commit-id: 729852b91d2e6af129fd0fd1b548966232e2d038 [formerly d35b5047b9289b31900b21f943fb855f59983e04]
    Former-commit-id: db4d9b412744b0c37cf783dfc6f295544f7f4045 [formerly db4d9b412744b0c37cf783dfc6f295544f7f4045 [formerly db4d9b412744b0c37cf783dfc6f295544f7f4045 [formerly c743f15dc1c3ba27a919ee06dc6cf5ac56758568]]]
    Former-commit-id: bd9aa50fee4480d01325cbe8e1c0f5bd66a9ec07 [formerly bd9aa50fee4480d01325cbe8e1c0f5bd66a9ec07 [formerly b15ec76b79ea6331cb043c10de7137fe04013054]]
    Former-commit-id: a538bdcd133e8ab57d7948acdecc18bf17a367b4 [formerly 5694fd4ff086d1f6026b4ad05b7d701a7b79cba1]
    Former-commit-id: 3a1ed0b052e5bc9229ce81bebe41022a7b8cface

--- external/BuildLibPNG.cmake
+++ external/BuildLibPNG.cmake
@@ -7,7 +7,7 @@ endif()
 ExternalProject_Add(
   LIBPNG
   URL "https://sourceforge.net/projects/libpng/files/libpng16/1.6.37/libpng-1.6.37.tar.gz/download"
-  CMAKE_GENERATOR ${generator}
+  CMAKE_GENERATOR "${generator}"
   CMAKE_ARGS
   -DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
