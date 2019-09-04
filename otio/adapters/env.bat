dc3a7c7447c472ce181ad8779decd8bd12728890
Gonzalo Garramuño <ggarra13@gmail.com>
2019-09-04 15:37:19 -0300

    Bug fixed aviImage.cpp and CMedia.cpp to not loop ever, as that was the reason EDLs were not working properly.
    Former-commit-id: d946768408cde6a1047d3ef7bd0f8f18f4a51f4c [formerly 96184594ea724cb5c7bc393ab1eac0db0aa135f8]
    Former-commit-id: 8dd2240dab459796af6480905eb23a65c2eb2e0a [formerly 013f9708396b385557effa483d9568b6917cde91] [formerly 9bafd8dfedf098495e78878cbd0ffa5c7bcd07af [formerly ca2337b658540d375050452622dba8d01fbba9d8]]
    Former-commit-id: a95b11e7abf8b8c3e5dfc47936a464f5b287c1ee [formerly 3652974e15839a3b186ef0b2024bc95b26d7000f]
    Former-commit-id: bfd9755d13ab3d09e2498f079e1243d1d0929116

--- otio/adapters/env.bat
+++ otio/adapters/env.bat
@@ -1,4 +1,7 @@
 REM a bat otio file for windows
-
+REM From this directory, use:
+REM
+REM > call env.bat
+REM
 set PATH=%~dp0\..\..\bin:%PATH%
 set OTIO_PLUGIN_MANIFEST_PATH=%~dp0\mrviewer.plugin_manifest.json
