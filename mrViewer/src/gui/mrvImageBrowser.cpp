17d4909d23aaed217b208fd999f005a4dcb92046
Gonzalo Garramuño <ggarra13@gmail.com>
2019-09-10 21:56:32 -0300

    Fixed OpenEXR for v2.4.  Fixed Assert to not hang on release builds. Fixed reel.py indentation (I hate python).
    Former-commit-id: b6f0b9bcf24fc67d13d29db160b7763b541dda64 [formerly d14ff53daff6e7ab37d31ade95ba0dc9752163f0]
    Former-commit-id: 561d544c05012bdfe12c159bd81cf110a0fa2694 [formerly 561d544c05012bdfe12c159bd81cf110a0fa2694 [formerly 80ccbe8558ed99dcca49047eb1c83c50e708a7d7]] [formerly 191f0c55399e64cbf9f38f03c28107c1eccb1ad2] [formerly 7172706bd385581e11001000227ba91ab6102c92 [formerly 191f0c55399e64cbf9f38f03c28107c1eccb1ad2 [formerly 191f0c55399e64cbf9f38f03c28107c1eccb1ad2 [formerly c5b63ba113a3d539a51c476b27a100e69d6b40c3]]]]
    Former-commit-id: f957c6228d54b9ccc180a5d6c8467703c9e7c1db [formerly f957c6228d54b9ccc180a5d6c8467703c9e7c1db [formerly 02fc2e9e0034052b35f7a1f7cfb0cc39a2cb485f]]
    Former-commit-id: f957c6228d54b9ccc180a5d6c8467703c9e7c1db
    Former-commit-id: 893dc39e9e0f34d9554acab3667bd1f3cb2e3f38 [formerly d0f16f9498e9f633e2290dfe401f4ba6ab1aa10c]
    Former-commit-id: 49d68013215879eddea1b38ee2a07ee3a58486d3

--- mrViewer/src/gui/mrvImageBrowser.cpp
+++ mrViewer/src/gui/mrvImageBrowser.cpp
@@ -3293,7 +3293,6 @@ void ImageBrowser::seek( const int64_t tframe )
         if (!fg) return;
 
         CMedia* img = fg->image();
-        f = reel->global_to_local( f );
         img->seek( f );
 
         mrv::media bg = view()->background();
