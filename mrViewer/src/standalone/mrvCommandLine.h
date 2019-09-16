78efb8ba4984abc92ab5a37e0aec6ffb2e158ea3
Gonzalo Garramuño <ggarra13@gmail.com>
2019-09-16 18:01:44 -0300

    Added missing flags to command-line parser. Fixed some virtual classes without virtual destructor. Fixed playback S/E points in EDL playback.  Note that playback in reverse is still broken.
    Former-commit-id: 744a118989c12c173a6e545d1b195dc593e45bfc [formerly 3a64dd5fc40b58eb05295f6c6a0178b5e4b53883]
    Former-commit-id: 0d6a59a0ea8115216fa59f22fc72dc06c3325776 [formerly 0d6a59a0ea8115216fa59f22fc72dc06c3325776 [formerly 3faaa9b036857ab9ea5e4d10f93dcb363ed50bb8]] [formerly 2ee4d155f3618bf6f3fa0e6a3d33fac17e1d7150] [formerly d7f56c57ff89c901b8640c1f3c97fb3c53643310 [formerly 2ee4d155f3618bf6f3fa0e6a3d33fac17e1d7150 [formerly 2ee4d155f3618bf6f3fa0e6a3d33fac17e1d7150 [formerly 16900b218fd2ff6fb7b4212f11dd6976d1cfe108]]]]
    Former-commit-id: a3a73576de391d666f969e843ade734218122ce5 [formerly a3a73576de391d666f969e843ade734218122ce5 [formerly 5e7364c4b37dcd0f3e0a47b1ca33541bfe23d998]]
    Former-commit-id: a3a73576de391d666f969e843ade734218122ce5
    Former-commit-id: 9f0d000cc2663cc440161a1a7cc258051381ee53 [formerly ffff0c8dc5d13a1c701fa9227812bb794dd3033d]
    Former-commit-id: 0b21225fdffc1848520d89c224b6724964723ecc

--- mrViewer/src/standalone/mrvCommandLine.h
+++ mrViewer/src/standalone/mrvCommandLine.h
@@ -19,10 +19,10 @@
  * @file   mrvCommandLine.h
  * @author gga
  * @date   Mon Aug  6 12:26:40 2007
- * 
+ *
  * @brief  Command-line parser for mrViewer.
- * 
- * 
+ *
+ *
  */
 
 #ifndef mrvCommandLine_h
@@ -38,18 +38,28 @@ typedef std::vector< std::string > stringArray;
 
   struct Options
   {
-       mrv::LoadList files;
-       mrv::LoadList stereo;
-       bool edl;
-       float gamma;
-       float gain;
-       std::string host;
-       stringArray audios;
-       unsigned short port;
-       float fps;
+      mrv::LoadList files;
+      std::string bgfile;
+      mrv::LoadList stereo;
+      bool edl;
+      bool play;
+      bool run;
+      float gamma;
+      float gain;
 
-  Options() : edl(false), gamma(1.0f), gain( 1.0f ), port( 0 ), fps( 0 )
-    {}
+      std::string stereo_input;
+      std::string stereo_output;
+
+      std::string host;
+      stringArray audios;
+      unsigned short port;
+      float fps;
+      int debug;
+
+
+      Options() : edl(false), play(false), run( false ),
+                  gamma(1.0f), gain( 1.0f ), port( 0 ), fps( 0 ), debug( 0 )
+          {}
   };
 
 
@@ -63,11 +73,9 @@ typedef std::vector< std::string > stringArray;
   // Command-line parser
   //
   void parse_command_line( const int argc, char** argv,
-			   ViewerUI* ui, 
+                           ViewerUI* ui,
                            mrv::Options& opts);
 }
 
 
 #endif // mrvCommandLine_h
-
-
