03e0181b87eac6547a4700efdb4f57f2befd0987
Gonzalo Garramuño <ggarra13@gmail.com>
2020-06-24 13:00:35 -0300

    Updated docs and upload_docs to upload docs/ directory.

--- www/upload_docs.sh
+++ www/upload_docs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 rsync -avP --exclude '*~' -e ssh . ggarra13@web.sourceforge.net:/home/project-web/mrviewer/htdocs
+
+rsync -avP --exclude '*~' -e ssh ../docs ggarra13@web.sourceforge.net:/home/project-web/mrviewer/htdocs
