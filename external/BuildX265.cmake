c706d9db5242dbc701dd55707259e50eae8a55e3
Gonzalo Garramuño <ggarra13@gmail.com>
2021-04-09 00:22:34 -0300

    Bug fixed FFmpeg, x265 and ImageMagick builds so that they work out of the box.

--- external/BuildX265.cmake
+++ external/BuildX265.cmake
@@ -7,6 +7,10 @@ if (WIN32 OR UNIX )
       HG_REPOSITORY "http://hg.videolan.org/x265"
       HG_TAG tip
       DEPENDS NASM
-      CONFIGURE_COMMAND cd ../x265/build/linux && ./multilib.sh)
+      CONFIGURE_COMMAND cd ../x265/build/linux && ./multilib.sh && cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} ../../source
+      BUILD_COMMAND   cd ../x265/build/linux && make
+      INSTALL_COMMAND cd ../x265/build/linux && make install
+      BUILD_IN_SOURCE 1
+    )
     ENDIF()
 ENDIF()
