3d678843100337da1697dca819751ae6e28dfcd0
Gonzalo Garramuño <ggarra13@gmail.com>
2021-08-09 20:27:11 -0300

    Replaced i->join with i->timed_join.

--- mrViewer/src/core/CMedia.cpp
+++ mrViewer/src/core/CMedia.cpp
@@ -829,9 +829,9 @@ void CMedia::wait_for_threads()
     for ( const auto& i : _threads )
     {
         i->interrupt();
-#ifndef OSX
-        i->join();
-#endif
+        boost::posix_time::time_duration timeout =
+            boost::posix_time::milliseconds(10000);
+        i->timed_join(timeout);
         delete i;
     }
 
