850fa5e2f949d6575c52b50a41850806f3c15001
Gonzalo Garramuño <ggarra13@gmail.com>
2021-08-09 19:01:18 -0300

    Reverted CMedia.cpp wait_for_threads to use join on all OSes but macOS.

--- mrViewer/src/core/CMedia.cpp
+++ mrViewer/src/core/CMedia.cpp
@@ -829,7 +829,9 @@ void CMedia::wait_for_threads()
     for ( const auto& i : _threads )
     {
         i->interrupt();
-        //i->join();
+#ifndef OSX
+        i->join();
+#endif
         delete i;
     }
 
