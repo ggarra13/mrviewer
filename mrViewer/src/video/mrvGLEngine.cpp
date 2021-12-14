a972589cddde5e1e81940afc2f25dced02059e72
Gonzalo Garramuño <ggarra13@gmail.com>
2021-12-14 09:55:20 -0300

    Reverted to use Output - sRGB when no 8 / 16 bit OCIO lut is specified.

--- mrViewer/src/video/mrvGLEngine.cpp
+++ mrViewer/src/video/mrvGLEngine.cpp
@@ -1680,10 +1680,8 @@ void prepare_image( CMedia* img, mrv::Recti& daw, unsigned texWidth,
 {
     glRotated( img->rot_z(), 0, 0, 1 );
     CHECK_GL;
-    glTranslated( img->x(), img->y(), 0 );
-    CHECK_GL;
-    glTranslated( double(daw.x() - img->eye_separation()),
-                  double(-daw.y()), 0 );
+    glTranslated( double(img->x() + daw.x() - img->eye_separation()),
+                  double(img->y() - daw.y()), 0 );
     CHECK_GL;
 
     glScaled( double(texWidth), double(texHeight), 1.0 );
