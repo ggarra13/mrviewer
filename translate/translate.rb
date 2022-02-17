fa5819fd58832ff99b4cf9fb3c65f868bd7ff591
Gonzalo Garramuño <ggarra13@gmail.com>
2022-02-17 00:20:03 -0300

    Fixed translations with translate.rb.

--- translate/translate.rb
+++ translate/translate.rb
@@ -1,50 +1,36 @@
-#!/usr/local/bin/ruby
+#!/usr/bin/env ruby
 # coding: utf-8
 
 
 require "google/cloud/translate/v2"
 require "fileutils"
 
+home = '/home'
+if RUBY_PLATFORM =~ /Darwin/i
+  home = '/Users'
+  cred = '/Users/gga/Downloads/translator-341205-b03ffc77569a.json'
+else
+  cred = '/home/gga/Downloads/translator-341205-98e6451016d4.json'
+end
 
 @translate = Google::Cloud::Translate::V2.new(
   project_id: "translator-341205",
-  credentials: "/home/gga/Downloads/translator-341205-98e6451016d4.json"
+  credentials: "#{cred}"
 )
 
-
-def replace( text )
-  text.gsub!(/&#39;/, "'" )
-  text.gsub!(/&gt;/, ">" )
-  text.gsub!(/&lt;/, "<" )
-  text.gsub!(/&amp;/, "&" )
-  text.gsub!(/&quot;/, '"' )
-  puts "result: #{text}."
-  return text
-end
-
-def translate( text, lang )
-  if text =~ /\//
-    menus = text.split('/')
-    if menus.size > 1
-      puts "origin: #{text} menus"
-      r = @translate.translate menus, to: lang
-      result = []
-      r.each { |m| result << m.text }
-      if menus[-1] == '%s'
-        result[-1] = '%s'
-      end
-      result = result.join('/')
-      return replace( result )
-    end
-  end
-  puts "origin: #{text}"
-  r = @translate.translate text, to: lang
-  result = r.text
+def fix( text, result, lang )
+  result.gsub!(/&#39;/, "'" )
+  result.gsub!(/&gt;/, ">" )
+  result.gsub!(/&lt;/, "<" )
+  result.gsub!(/&amp;/, "&" )
+  result.gsub!(/&quot;/, '"' )
   if text == ' UF: %<PRId64> ' or text == 'F: ' or text == 'T: ' or
       text == ' FC: ' or text == 'V-A: ' or
       text == ' ( %02<PRId64>:%02<PRId64>:%02<PRId64>  %d ms. )' or
       text == '  INF.  ' or
-      text == 'PMem: %<PRIu64>/%<PRIu64> MB  VMem: %<PRIu64>/%<PRIu64> MB'
+    text == 'PMem: %<PRIu64>/%<PRIu64> MB  VMem: %<PRIu64>/%<PRIu64> MB' or
+    text == "mrViewer    FG: %s [%d]   BG: %s [%d] (%s)" or
+    text == "mrViewer    FG: %s"
     result = text
   elsif text =~ /FPS:/
     result.sub!(/s*(FPS)./, 'FPS:')
@@ -103,21 +89,42 @@ def translate( text, lang )
       # Automatic translation returns @ || instead of @||
       #
       result = "@" + $1
-    elsif result =~ /\\s+t/
+    end
+    if result =~ /：/
+      result.gsub!(/：/, ':')
+    end
+    if result =~ /。/
+      result.gsub!(/。/, '.')
+    end
+    if result =~ /％s/
+      result.gsub!(/％s/, '%s' )
+    end
+    if result =~ /％([dg])s*/
+      result.gsub!(/％([dg])s*/, '%' + $1 + ' ' )
+    end
+    if result =~ /\\\s+"/
+      #
+      # Automatic translation returns \ " instead of \"
+      #
+      result.gsub!(/\\\s+"/, '\\"')
+    end
+    if result =~ /\\\s+t/
+      #
+      # Automatic translation returns \ t instead of \t
+      #
+      result.gsub!(/\\\s+t/, '\t')
+    end
+    if result =~ /\\\s+n/
       #
       # Automatic translation returns \ instead of \n
       #
-      result.gsub!(/\\s+t/, '\t')
-    elsif result =~ /\\s+"/
+      result.gsub!(/\\\s+n/, '\n')
+    end
+    if result =~ /\\\s+[^nt"]/
       #
       # Automatic translation returns \ instead of \n
       #
-      result.gsub!(/\\s+"/, '\"')
-    elsif result =~ /\\[^nt"]/
-      #
-      # Automatic translation returns \ instead of \n
-      #
-      result.gsub!(/\\/, '\n')
+      result.gsub!(/\\\s+/, '\n')
     end
   elsif lang == 'ko'
     if result == 'LM변환% 유'
@@ -130,7 +137,8 @@ def translate( text, lang )
   elsif lang == 'zh'
     if result =~ /闲暇/
       result.sub!(/闲暇/, 'OCIO')
-    elsif result =~ /\\”/
+    end
+    if result =~ /\\”/
       result.gsub!( /\\”/, '\"' )
     end
   end
@@ -147,32 +155,60 @@ def translate( text, lang )
       result = spaces + text
     end
   end
-  return replace(result)
+  return result
 end
 
+def replace( text )
+  puts "result: #{text}."
+  return text
+end
+
+def translate( text, lang )
+  if text =~ /\//
+    menus = text.split('/')
+    if menus.size > 1
+      puts "origin: #{text} menus"
+      r = @translate.translate menus, to: lang
+      result = []
+      r.each { |m| result << m.text }
+      if menus[-1] == '%s'
+        result[-1] = '%s'
+      end
+      result = result.join('/')
+      result = fix( text, result, lang )
+      return replace( result )
+    end
+  end
+  puts "origin: #{text}"
+  r = @translate.translate text, to: lang
+  result = r.text
+  result = fix( text, result, lang )
+  return replace( result )
+end
 
 @h = {}
+@op = nil
 
-def new_line( op, text )
+def new_line( text )
   return if @msgid.empty? or @h[@msgid]
   @h[@msgid] = 1
-  op.puts "msgid \"#{@msgid}\""
-  op.puts "msgstr \"#{text}\""
+  @op.puts "msgid \"#{@msgid}\""
+  @op.puts "msgstr \"#{text}\""
 end
 
-langs = [ 'es' ]
-for lang in [ 'de', 'fr', 'it', 'cs', 'zh', 'ja', 'ko' ]
+langs = [ 'es', 'de', 'fr', 'it', 'cs', 'ru', 'zh', 'ko' ]
+for lang in [ 'de', 'fr', 'it', 'cs', 'ru', 'zh', 'ja', 'ko', 'ru' ]
   next if langs.any? lang
   @h = {}
-  $stderr.puts "=================== Translate to #{lang} ======================x"
+  $stderr.puts "=================== Translate to #{lang} ======================"
   in_msg_id = false
   msg = ''
-  root = "/home/gga/code/applications/mrv/mrViewer/src/po"
+  root = "#{home}/gga/code/applications/mrv/mrViewer/src/po"
   fp = File.open( "#{root}/messages.pot", encoding: "utf-8")
   FileUtils.cp( "#{root}/#{lang}.po",
                 "#{root}/#{lang}.po.old" )
-  op = File.open("#{root}/#{lang}.po", "w", encoding: "utf-8")
-  op.puts <<EOF
+  @op = File.open("#{root}/#{lang}.po", "w", encoding: "utf-8")
+  @op.puts <<EOF
 msgid ""
 msgstr ""
 "Project-Id-Version: mrViewer\\n"
@@ -189,13 +225,14 @@ msgstr ""
 
 EOF
 
-  for line in fp
+  lines = fp.readlines
+  for line in lines
     if in_msg_id
       msgstr = line =~ /msgstr\s+"/
       text = line =~ /"(.*)"/
       if not text or msgstr
         r = translate( msg, lang )
-        new_line( op, r )
+        new_line( r )
         in_msg_id = false
         next
       end
@@ -213,6 +250,6 @@ EOF
     in_msg_id = true
     next
   end
-  op.close
+  @op.close
   fp.close
 end
