3135832054aa374331673e20259bb4fff486d01e
Gonzalo Garramuño <ggarra13@gmail.com>
2022-08-10 17:23:35 -0300

    Fixed the lack of assembler comping on x265 (however, not sure if it gets used).

--- SuperBuild/BuildX265.cmake
+++ SuperBuild/BuildX265.cmake
@@ -7,7 +7,7 @@ if (WIN32 OR UNIX )
       #HG_REPOSITORY "http://hg.videolan.org/x265"
       URL "http://ftp.videolan.org/pub/videolan/x265/x265_3.2.1.tar.gz"
       DEPENDS ${NASM}
-      CONFIGURE_COMMAND cd "${CMAKE_CURRENT_BINARY_DIR}/x265-prefix/src/x265/build/linux" && ./multilib.sh && cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" ../../source
+      CONFIGURE_COMMAND cd "${CMAKE_CURRENT_BINARY_DIR}/x265-prefix/src/x265/build/linux" && unset MAKEFLAGS && ./multilib.sh && cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} ../../source
       BUILD_COMMAND   cd "${CMAKE_CURRENT_BINARY_DIR}/x265-prefix/src/x265/build/linux" && make
       INSTALL_COMMAND cd "${CMAKE_CURRENT_BINARY_DIR}/x265-prefix/src/x265/build/linux" && make install
       BUILD_IN_SOURCE 1
