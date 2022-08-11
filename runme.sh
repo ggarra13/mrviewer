1e270c9c3a488b7b243ea58b8301747d21e6ed47
Gonzalo Garramuño <ggarra13@gmail.com>
2022-08-11 00:12:07 -0300

    Added main runme.sh.

---
+++ runme.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+#
+# Determine CPU architecture
+#
+KERNEL=`uname -s`
+
+if [[ $KERNEL == CYGWIN* ]]; then
+    KERNEL=Windows
+    RELEASE=(`cmd /c 'ver'`)
+    RELEASE=${RELEASE[3]%.[0-9]*}
+elif [[ $KERNEL == MINGW* ]]; then
+    RELEASE=(`cmd /c 'ver'`)
+    #RELEASE=${RELEASE[3]%.[0-9]*}
+    RELEASE=${RELEASE[3]/]/}
+    KERNEL=Windows
+else
+    RELEASE=`uname -r`
+fi
+
+
+if [[ "$RELEASE" == "" ]]; then
+    $RELEASE=`cmd.exe /c 'ver'`
+    echo $RELEASE
+    echo "Could not determine OS version"
+    exit 1
+fi
+
+export OS_32_BITS=1
+export OS_64_BITS=
+
+export fltk_dir=""
+
+if [ $KERNEL == 'Windows' ]; then
+    arch=`wmic OS get OSArchitecture`
+    if [[ $arch == *64* ]]; then
+	CMAKE_NATIVE_ARCH=64
+	export OS_64_BITS=1
+    fi
+else
+    arch=`uname -a`
+fi
+
+if [[ $arch == *64* ]]; then
+    CMAKE_NATIVE_ARCH=64
+    export OS_64_BITS=1
+else
+    if [[ $arch == *ia64* ]]; then
+	CMAKE_NATIVE_ARCH=64
+	export OS_64_BITS=1
+	unset  OS_32_BITS
+    fi
+fi
+
+BUILD=$KERNEL-$RELEASE-$CMAKE_NATIVE_ARCH
+
+export prefix=$PWD/install-$BUILD/
+export LD_FLAGS="-Wl,--copy-dt-needed-entries"
+
+mk --prefix=$prefix -DFLTK_DIR=$PWD/SuperBuild/BUILD/$BUILD/Release/tmp/FLTK-prefix/src/FLTK-build/ $@
+
+
