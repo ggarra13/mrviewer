0ff6e34d674418ad2bc45db64cd38f296c2e5eab
Gonzalo Garramuño <ggarra13@gmail.com>
2022-08-13 06:48:50 -0300

    Fixed and sped up compilation on macOS.

--- SuperBuild/BuildLibVPX.cmake
+++ SuperBuild/BuildLibVPX.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(
   GIT_REPOSITORY "https://github.com/webmproject/libvpx.git"
   #https://chromium.googlesource.com/webm/libvpx
   #GIT_TAG v1.12.0
-  CONFIGURE_COMMAND ./configure ${ARCH} --enable-static --enable-shared --prefix=${CMAKE_INSTALL_PREFIX}
+  CONFIGURE_COMMAND ./configure ${ARCH} --enable-static --prefix=${CMAKE_INSTALL_PREFIX}
   DEPENDS ${YASM}
   BUILD_IN_SOURCE 1
   )
--- SuperBuild/BuildLibWebP.cmake
+++ SuperBuild/BuildLibWebP.cmake
@@ -3,6 +3,7 @@ set( libwebp_version "1.2.4" )
 
 set( file "libwebp-${libwebp_version}.tar.gz" )
 
+
 ExternalProject_Add(
   ${LibWebP}
   URL "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/${file}"
@@ -13,6 +14,8 @@ ExternalProject_Add(
   -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
   -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
   -DBUILD_TESTING=FALSE
+  -DWEBP_BUILD_CWEBP=FALSE
+  -DWEBP_BUILD_DWEBP=FALSE
   -DWEBP_BUILD_VWEBP=FALSE
    ${MRV_EXTERNAL_ARGS}
   )
