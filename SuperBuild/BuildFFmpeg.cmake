910772663f992fc45236829920f419104a081d53
Gonzalo Garramuño <ggarra13@gmail.com>
2022-08-24 08:47:46 -0300

    Modified FFmpeg and LibVPX cmake scripts for macOS.

--- SuperBuild/BuildFFmpeg.cmake
+++ SuperBuild/BuildFFmpeg.cmake
@@ -9,9 +9,43 @@ set( ENV{LD_FLAGS} "-Wl,--copy-dt-needed-entries" )
 
 ExternalProject_Add(
   FFmpeg
-  GIT_REPOSITORY "https://git.ffmpeg.org/ffmpeg.git"
-  # GIT_TAG n4.4.2
-  CONFIGURE_COMMAND ./configure ${ARCH} --enable-pic --enable-shared --enable-gpl --enable-gray --enable-gnutls --enable-libfontconfig --enable-libfreetype --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libopus --enable-libass --enable-libvpx --enable-libx264 --enable-libwebp --enable-bzlib --enable-zlib --extra-libs=-lgnutls ${lm_lib} --extra-libs=-lass --extra-libs=-lpthread --extra-cflags=-I/usr/include/freetype2 --extra-cflags=-I/usr/include/fontconfig --extra-cflags=-I${CMAKE_PREFIX_PATH}/include/opus --extra-cflags=-I/usr/include/ --extra-ldflags=-L${CMAKE_PREFIX_PATH}/lib --extra-ldflags=-L${CMAKE_PREFIX_PATH}/lib64 --prefix=${CMAKE_INSTALL_PREFIX} --enable-libx265 --disable-vaapi
+  #GIT_REPOSITORY "https://git.ffmpeg.org/ffmpeg.git"
+  URL https://ffmpeg.org/releases/ffmpeg-5.1.tar.bz2
+  CONFIGURE_COMMAND ./configure
+  --disable-appkit
+  --disable-avfoundation
+  --disable-coreimage
+  --disable-audiotoolbox
+  --disable-vaapi
+  --enable-pic
+  --enable-shared
+  --enable-gpl
+  --enable-gray
+  --enable-gnutls
+  --enable-libfontconfig
+  --enable-libfreetype
+  --enable-libmp3lame
+  --enable-libtheora
+  --enable-libvorbis
+  --enable-libopus
+  --enable-libass
+  --enable-libvpx
+  --enable-libx264
+  --enable-libwebp
+  --enable-bzlib
+  --enable-zlib
+  --extra-libs=-lgnutls
+  ${lm_lib}
+  --extra-libs=-lass
+  --extra-libs=-lpthread
+  --extra-cflags=-I/usr/include/freetype2
+  --extra-cflags=-I/usr/include/fontconfig
+  --extra-cflags=-I${CMAKE_PREFIX_PATH}/include/opus
+  --extra-cflags=-I/usr/include/
+  --extra-ldflags=-L${CMAKE_PREFIX_PATH}/lib
+  --extra-ldflags=-L${CMAKE_PREFIX_PATH}/lib64
+  --prefix=${CMAKE_INSTALL_PREFIX}
+  --enable-libx265
   DEPENDS ${NASM} ${LibAss} ${x264} ${LIBPNG} ${LibTIFF} ${JPEGTURBO} ${LibVPX}
   ${LibOpus} ${LibWebP} ${x265}
   BUILD_IN_SOURCE 1
--- SuperBuild/BuildLibVPX.cmake
+++ SuperBuild/BuildLibVPX.cmake
@@ -1,11 +1,16 @@
 include( ExternalProject )
 
+set( LIBVPX_FLAGS --enable-shared )
+if( APPLE )
+  set( LIBVPX_FLAGS --enable-static )
+endif()
+
 ExternalProject_Add(
   LibVPX
   GIT_REPOSITORY "https://github.com/webmproject/libvpx.git"
   #https://chromium.googlesource.com/webm/libvpx
   #GIT_TAG v1.12.0
-  CONFIGURE_COMMAND ./configure ${ARCH} --enable-shared --prefix=${CMAKE_INSTALL_PREFIX}
+  CONFIGURE_COMMAND ./configure ${ARCH} ${LIBVPX_FLAGS} --prefix=${CMAKE_INSTALL_PREFIX}
   DEPENDS ${YASM}
   BUILD_IN_SOURCE 1
   )
